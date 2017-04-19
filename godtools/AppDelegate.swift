6372c4198ccdaf92625f6f413967883220fad3bb
ryancarlson <ryan.t.carlson@cru.org>
2017-04-19 11:36:52 -0400

    Setup local database stack with magical record

--- godtools/AppDelegate.swift
+++ godtools/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import MagicalRecord
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        MagicalRecord.setupCoreDataStack(withAutoMigratingSqliteStoreNamed: "godtools-5")
         return true
     }
 
@@ -39,8 +40,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
+        MagicalRecord.cleanUp()
     }
 }
 
