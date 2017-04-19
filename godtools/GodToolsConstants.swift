acc1b637b8f579dc920623c05f3cc9f731214a3d
ryancarlson <ryan.t.carlson@cru.org>
2017-04-19 10:39:52 -0400

    Add GodToolsConstants class to start tracking constants.

---
+++ godtools/GodToolsConstants.swift
@@ -0,0 +1,14 @@
+//
+//  GodToolsConstants.swift
+//  godtools
+//
+//  Created by Ryan Carlson on 4/19/17.
+//  Copyright © 2017 Cru. All rights reserved.
+//
+
+import Foundation
+
+class GodToolsConstants {
+    
+    static let kApiBase = "https://mobile-content-api-stage.cru.org"
+}
