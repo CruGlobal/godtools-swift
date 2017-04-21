f4bf139013d23bd6a96f265edf5caf8495e6379a
Pablo Marti <startpablo@gmail.com>
2017-04-21 14:52:21 -0600

    Load content of tools on add tools screen

--- godtools/ViewControllers/Platform/AddToolsViewController.swift
+++ godtools/ViewControllers/Platform/AddToolsViewController.swift
@@ -11,30 +11,43 @@ import UIKit
 protocol AddToolsViewControllerDelegate {
 }
 
-class AddToolsViewController: BaseViewController {
+class AddToolsViewController: BaseViewController, ToolsManagerDelegate {
     
     var delegate: AddToolsViewControllerDelegate?
-
+    
+    let toolsManager = ToolsManager.shared
+    
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = toolsManager
+            tableView.dataSource = toolsManager
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.registerCells()
+        self.setupStyle()
+        self.toolsManager.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - Helpers
+    
+    fileprivate func registerCells() {
+        self.tableView.register(UINib(nibName: "HomeToolTableViewCell", bundle: nil), forCellReuseIdentifier: ToolsManager.toolCellIdentifier)
     }
-    */
+    
+    fileprivate func setupStyle() {
+        self.tableView.separatorStyle = .none
+    }
+    
+    // MARK: - ToolsManagerDelegate
+    
+    func didSelectTableViewRow(cell: HomeToolTableViewCell) {
+    }
 
 }
