287fe39044ad7e566fc01dd5bf8b3f2ad930ad7d
ryancarlson <ryan.t.carlson@cru.org>
2017-04-24 15:43:19 -0400

    Add TractViewController to project

---
+++ godtools/ViewControllers/Tract/TractViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TractViewController.swift
+//  godtools
+//
+//  Created by Ryan Carlson on 4/24/17.
+//  Copyright © 2017 Cru. All rights reserved.
+//
+
+import Foundation
+
+class TractViewController: BaseViewController {
+    
+    
+    
+}
