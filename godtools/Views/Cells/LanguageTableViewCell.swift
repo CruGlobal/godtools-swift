6c0df106222b887366368dc54719903e97e9c784
Pablo Marti <startpablo@gmail.com>
2017-04-26 17:22:55 -0600

    Set style to language cell

--- godtools/Views/Cells/LanguageTableViewCell.swift
+++ godtools/Views/Cells/LanguageTableViewCell.swift
@@ -9,13 +9,37 @@
 import UIKit
 
 class LanguageTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var downloadButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func pressDownloadButton(_ sender: Any) {
+    }
+    
+    @IBAction func pressDeleteButton(_ sender: Any) {
+    }
+    
+    // MARK: - Helpers
+    
+    func languageExist(exist:Bool) {
+        if exist {
+            self.deleteButton.isHidden = false
+            self.downloadButton.isHidden = true
+        }
+        else {
+            self.deleteButton.isHidden = true
+            self.downloadButton.isHidden = false
+        }
     }
     
 }
