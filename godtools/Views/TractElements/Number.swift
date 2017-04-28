a001e7e8cbd034f4b0a74bfa94cedb09c116a96f
Pablo Marti <startpablo@gmail.com>
2017-04-28 15:13:23 -0600

    Add missing tract elements

---
+++ godtools/Views/TractElements/Number.swift
@@ -0,0 +1,27 @@
+//
+//  Number.swift
+//  godtools
+//
+//  Created by Devserker on 4/28/17.
+//  Copyright © 2017 Cru. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Number: BaseTractElement {
+    
+    let paddingConstant = CGFloat(30.0)
+    
+    override func setupView(properties: Dictionary<String, Any>) {
+        let frame = CGRect(x: 0.0, y: self.yStartPosition  + paddingConstant, width: BaseTractElement.Standards.screenWidth, height: self.height)
+        let view = UIView(frame: frame)
+        view.backgroundColor = .green
+        self.view = view
+    }
+    
+    override func yEndPosition() -> CGFloat {
+        return self.yStartPosition + self.height + paddingConstant
+    }
+
+}
--- godtools/Views/TractElements/TractRoot.swift
+++ godtools/Views/TractElements/TractRoot.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Cru. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class TractRoot: BaseTractElement {
