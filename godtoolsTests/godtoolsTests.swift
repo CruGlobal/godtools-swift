6dfeed9f53c6a84d0b0ac177e7ba5323873a5f1d
Harro <michael.harrison@cru.org>
2017-05-01 17:01:18 -0400

    adds test target and testing pods to Podfile

--- godtoolsTests/godtoolsTests.swift
+++ godtoolsTests/godtoolsTests.swift
@@ -6,30 +6,24 @@
 //  Copyright © 2017 Cru. All rights reserved.
 //
 
-import XCTest
+import Quick
+import Nimble
 
-class godtoolsTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class GodToolsSpec: QuickSpec {
+  override func spec() {
+    describe("the 'Documentation' directory") {
+      it("has everything you need to get started") {
+        let sections = Directory("Documentation").sections
+        expect(sections).to(contain("Organized Tests with Quick Examples and Example Groups"))
+        expect(sections).to(contain("Installing Quick"))
+      }
+      
+      context("if it doesn't have what you're looking for") {
+        it("needs to be updated") {
+          let you = You(awesome: true)
+          expect{you.submittedAnIssue}.toEventually(beTruthy())
         }
+      }
     }
-    
+  }
 }
