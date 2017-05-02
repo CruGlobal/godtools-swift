7587cf16abd80fe80d66566f60738b936ecedec0
Harro <michael.harrison@cru.org>
2017-05-02 10:23:31 -0400

    commented out demo code in demo unit test to avoid build failures

--- godtoolsTests/godtoolsTests.swift
+++ godtoolsTests/godtoolsTests.swift
@@ -13,15 +13,15 @@ class GodToolsSpec: QuickSpec {
   override func spec() {
     describe("the 'Documentation' directory") {
       it("has everything you need to get started") {
-        let sections = Directory("Documentation").sections
-        expect(sections).to(contain("Organized Tests with Quick Examples and Example Groups"))
-        expect(sections).to(contain("Installing Quick"))
+        //let sections = Directory("Documentation").sections
+        //expect(sections).to(contain("Organized Tests with Quick Examples and Example Groups"))
+        //expect(sections).to(contain("Installing Quick"))
       }
       
       context("if it doesn't have what you're looking for") {
         it("needs to be updated") {
-          let you = You(awesome: true)
-          expect{you.submittedAnIssue}.toEventually(beTruthy())
+          //let you = You(awesome: true)
+          //expect{you.submittedAnIssue}.toEventually(beTruthy())
         }
       }
     }
