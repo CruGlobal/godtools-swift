c4973a2cc974bf90e7fffe60630a290a3e4e3a3c
Pablo Marti <startpablo@gmail.com>
2017-05-10 09:50:10 -0600

    Fix display of call to action on view without cards

--- godtools/Views/TractElements/CallToAction.swift
+++ godtools/Views/TractElements/CallToAction.swift
@@ -17,20 +17,24 @@ class CallToAction: BaseTractElement {
     var xPosition: CGFloat {
         return Card.xMarginConstant
     }
-    var yPosition: CGFloat {
-        return self.yStartPosition + CallToAction.yMarginConstant
-    }
     override var width: CGFloat {
         return (self.parent?.width)! - self.xPosition - Card.xMarginConstant
     }
-    let minHeight = CGFloat(80.0)
+    static let minHeight = CGFloat(80.0)
     override var height: CGFloat {
         get {
-            return super.height > self.minHeight ? super.height : self.minHeight
+            return super.height > CallToAction.minHeight ? super.height : CallToAction.minHeight
         }
         set {
             super.height = newValue
         }
+    }
+    var yPosition: CGFloat {
+        var position = self.yStartPosition + CallToAction.yMarginConstant
+        if position < (self.parent?.maxHeight)! - self.height {
+            position = (self.parent?.maxHeight)! - self.height
+        }
+        return position
     }
     let buttonSizeConstant = CGFloat(22.0)
     let buttonSizeXMargin = CGFloat(8.0)
@@ -50,7 +54,7 @@ class CallToAction: BaseTractElement {
         let size = CGSize(width: self.buttonSizeConstant, height: self.buttonSizeConstant)
         let buttonFrame = CGRect(origin: origin, size: size)
         let button = UIButton(frame: buttonFrame)
-        let image = UIImage(named: "right_arrow_black")
+        let image = UIImage(named: "right_arrow_blue")
         button.setBackgroundImage(image, for: UIControlState.normal)
         self.addSubview(button)
     }
