186bb51348b8c61893a1c257e9a7fb3e193fba88
ryancarlson <ryan.t.carlson@cru.org>
2017-05-22 10:55:27 -0400

    Add logic to show light content (white) status bar on all view controllers  - status bar will be shown by default, with option to hide it  - tract view controller will opt to hide it

--- godtools/ViewControllers/BaseViewController.swift
+++ godtools/ViewControllers/BaseViewController.swift
@@ -32,6 +32,8 @@ class BaseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.displayNavigationButtons()
+        
+        navigationController?.navigationBar.barStyle = .black
     }
     
     override func viewWillAppear(_ animated: Bool) {
