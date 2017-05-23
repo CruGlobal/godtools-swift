5517f93bb3a83642e1855bc7c3211bbfec89a548
Harro <michael.harrison@cru.org>
2017-05-23 15:48:03 -0400

    adds CRUConfig pod, Config class, config sample file and ignores other config files

---
+++ godtools/Config.swift
@@ -0,0 +1,16 @@
+//
+//  Config.swift
+//  godtools
+//
+//  Created by Michael Harrison on 5/23/17.
+//  Copyright © 2017 Cru. All rights reserved.
+//
+
+import Foundation
+import CRUConfig
+
+class Config: CRUEmptyConfig {
+    let baseUrl: URL? = nil
+    let apiKey: String = ""
+    let googleAnalyticsApiKey: String = ""
+}
