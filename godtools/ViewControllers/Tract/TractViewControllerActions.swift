daf45718004c929da07c131b70d1efe919bb6f92
Pablo Marti <startpablo@gmail.com>
2017-06-07 15:03:27 -0600

    Refactor code

--- godtools/ViewControllers/Tract/TractViewControllerActions.swift
+++ godtools/ViewControllers/Tract/TractViewControllerActions.swift
@@ -18,20 +18,28 @@ extension TractViewController: MFMailComposeViewControllerDelegate {
                 return
             }
             
-            let composeVC = MFMailComposeViewController()
-            composeVC.mailComposeDelegate = self
-            composeVC.setSubject(dictionary["subject"] as! String)
-            composeVC.setMessageBody(dictionary["content"] as! String, isHTML: dictionary["html"] as! Bool)
-            self.present(composeVC, animated: true, completion: nil)
+            displayEmailCompose(dictionary: dictionary)
         } else {
-            let alert = UIAlertController(title: "GodTools", message: "error_can_not_send_email".localized, preferredStyle: UIAlertControllerStyle.alert)
-            alert.addAction(UIAlertAction(title: "ok".localized, style: UIAlertActionStyle.default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
+            displayEmailErrorMessage()
         }
     }
     
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func displayEmailCompose(dictionary: [String: Any]) {
+        let composeVC = MFMailComposeViewController()
+        composeVC.mailComposeDelegate = self
+        composeVC.setSubject(dictionary["subject"] as! String)
+        composeVC.setMessageBody(dictionary["content"] as! String, isHTML: dictionary["html"] as! Bool)
+        self.present(composeVC, animated: true, completion: nil)
+    }
+    
+    func displayEmailErrorMessage() {
+        let alert = UIAlertController(title: "GodTools", message: "error_can_not_send_email".localized, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "ok".localized, style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
