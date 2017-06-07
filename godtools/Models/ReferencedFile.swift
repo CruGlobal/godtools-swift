7df41c4021e84e4fc00f8138bdc5e29291e9985e
ryancarlson <ryan.t.carlson@cru.org>
2017-06-06 17:01:48 -0700

    Migrate ReferencedFile from CoreData to Realm model

---
+++ godtools/Models/ReferencedFile.swift
@@ -0,0 +1,15 @@
+//
+//  ReferencedFile.swift
+//  godtools
+//
+//  Created by Ryan Carlson on 6/6/17.
+//  Copyright © 2017 Cru. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class ReferencedFile: Object {
+    dynamic var filename = ""
+    let translations = List<Translation>()
+}
