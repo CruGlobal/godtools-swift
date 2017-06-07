13df2ae3bfc60c9e291b30e75bcf27211b086453
Pablo Marti <startpablo@gmail.com>
2017-06-06 18:43:49 -0600

    Present email view for sending the email dispatched from tract and alert if no email configured

--- godtools/ViewControllers/Tract/TractViewController.swift
+++ godtools/ViewControllers/Tract/TractViewController.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 import SWXMLHash
+import MessageUI
 
 class TractViewController: BaseViewController {
     
--- godtools/ViewControllers/Tract/TractViewControllerActions.swift
+++ godtools/ViewControllers/Tract/TractViewControllerActions.swift
@@ -13,15 +13,21 @@ import MessageUI
 extension TractViewController: MFMailComposeViewControllerDelegate {
     
     func sendEmail(notification: Notification) {
-        guard let dictionary = notification.userInfo as? [String: Any] else {
-            return
+        if MFMailComposeViewController.canSendMail() {
+            guard let dictionary = notification.userInfo as? [String: Any] else {
+                return
+            }
+            
+            let composeVC = MFMailComposeViewController()
+            composeVC.mailComposeDelegate = self
+            composeVC.setSubject(dictionary["subject"] as! String)
+            composeVC.setMessageBody(dictionary["content"] as! String, isHTML: dictionary["html"] as! Bool)
+            self.present(composeVC, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertController(title: "GodTools", message: "error_can_not_send_email".localized, preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "ok".localized, style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         }
-        
-        let composeVC = MFMailComposeViewController()
-        composeVC.mailComposeDelegate = self
-        composeVC.setSubject(dictionary["subject"] as! String)
-        composeVC.setMessageBody(dictionary["content"] as! String, isHTML: dictionary["html"] as! Bool)
-        self.present(composeVC, animated: true, completion: nil)
     }
     
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
