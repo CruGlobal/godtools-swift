6e0eee7f2d40d8314c6c67d5cf8b8796cf17ebbc
Pablo Marti <startpablo@gmail.com>
2017-06-09 14:34:14 -0600

    Load background for card

--- godtools/Views/TractElements/TractCard.swift
+++ godtools/Views/TractElements/TractCard.swift
@@ -151,6 +151,35 @@ class TractCard: BaseTractElement {
     
     // MARK: - UI
     
+    func setupBackground() {
+        self.backgroundColor = .clear
+        
+        guard let image = self.properties.backgroundProperties.backgroundImage else {
+            return
+        }
+        let imageView = UIImageView(image: image)
+        
+        let viewWidth = self.frame.size.width
+        let viewHeight = self.frame.size.height
+        var width = image.size.width
+        var height = image.size.height
+        let ratio = width / height
+        
+        if height > viewHeight || width > viewWidth {
+            width = viewWidth
+            height = width / ratio
+        }
+        
+        let xPosition = (viewWidth - width) / CGFloat(2.0)
+        let yPosition: CGFloat = 0.0
+        
+        imageView.frame = CGRect(x: xPosition, y: yPosition, width: width, height: height)
+        imageView.contentMode = .scaleAspectFit
+        
+        self.addSubview(imageView)
+        self.sendSubview(toBack: imageView)
+    }
+    
     func setupTransparentView() {
         if self.externalHeight >= self.internalHeight {
             return
