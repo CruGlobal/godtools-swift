38c723b7591a5c07d890109a112e24e4436c8479
ryancarlson <ryan.t.carlson@cru.org>
2017-06-21 17:32:09 -0400

    Create image using CGColor created from UIColor

--- godtools/Views/NavigationBarBackground.swift
+++ godtools/Views/NavigationBarBackground.swift
@@ -30,4 +30,23 @@ class NavigationBarBackground {
         
         return image
     }
+    
+    static func createFrom(color: UIColor) -> UIImage {
+        let color = color.cgColor
+        
+        let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: CGFloat(1.0), height: CGFloat(1.0)), false, CGFloat(2.0))
+        
+        let context = UIGraphicsGetCurrentContext()
+        
+        context?.setFillColor(color)
+        context?.fill(rect)
+        context?.saveGState()
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
 }
