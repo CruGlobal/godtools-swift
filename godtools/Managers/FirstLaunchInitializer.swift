abc5ef11d869f365c399a8746dda193a1074f34a
ryancarlson <ryan.t.carlson@cru.org>
2017-06-30 11:36:00 -0400

    Set that english and default tools (kgp,4sl,sat) should download

--- godtools/Managers/FirstLaunchInitializer.swift
+++ godtools/Managers/FirstLaunchInitializer.swift
@@ -18,9 +18,24 @@ class FirstLaunchInitializer: GTDataManager {
     
     private func initializeInitialLanguages() {
         LanguagesManager().loadInitialContentFromDisk()
+        let englishLanguage = findEntity(Language.self, byAttribute: "code", withValue: "en")
+        
+        safelyWriteToRealm {
+            englishLanguage?.shouldDownload = true
+        }
     }
     
     private func initializeInitialResources() {
         DownloadedResourceManager().loadInitialContentFromDisk()
+        setResourceShouldDownload(resourceCodes: ["kgp","sat","4sl"])
+    }
+    
+    private func setResourceShouldDownload(resourceCodes: [String]) {
+        safelyWriteToRealm {
+            for resourceCode in resourceCodes {
+                let resource = findEntity(DownloadedResource.self, byAttribute: "code", withValue: resourceCode)
+                resource?.shouldDownload = true
+            }
+        }
     }
 }
