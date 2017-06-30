9a23a9a14df7f7121f779a241330f5004de033e5
ryancarlson <ryan.t.carlson@cru.org>
2017-06-30 14:17:18 -0400

    Extract bundled zips

--- godtools/Managers/FirstLaunchInitializer.swift
+++ godtools/Managers/FirstLaunchInitializer.swift
@@ -14,6 +14,7 @@ class FirstLaunchInitializer: GTDataManager {
     func initializeAppState() {
         initializeInitialLanguages()
         initializeInitialResources()
+        extractInitialZipFiles()
     }
     
     private func initializeInitialLanguages() {
@@ -28,6 +29,31 @@ class FirstLaunchInitializer: GTDataManager {
     private func initializeInitialResources() {
         DownloadedResourceManager().loadInitialContentFromDisk()
         setResourceShouldDownload(resourceCodes: ["kgp","sat","4sl"])
+    }
+    
+    private func extractInitialZipFiles() {
+        for code in ["kgp","sat","4sl"] {
+            extractZipFile(resourceCode: code)
+        }
+    }
+    
+    private func extractZipFile(resourceCode: String) {
+        let zipImporter = TranslationZipImporter()
+        let translationsManager = TranslationsManager()
+        
+        let zipFileURL = fileURLForResource(code: resourceCode)
+        let translation = translationsManager.loadTranslation(resourceCode: resourceCode,
+                                                              languageCode: "en",
+                                                              published: true)!
+
+        let zippedData = try! Data(contentsOf: zipFileURL)
+        zipImporter.handleZippedData(zipData: zippedData, translation: translation)
+    }
+    
+    private func fileURLForResource(code: String) -> URL {
+        let pathToZip = Bundle.main.path(forResource: code, ofType: "zip")!
+        let zipFileURL = URL(fileURLWithPath: pathToZip)
+        return zipFileURL
     }
     
     private func setResourceShouldDownload(resourceCodes: [String]) {
