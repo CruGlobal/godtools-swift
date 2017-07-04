cc729f282ccd7efa6a045a9e55d99b4b7cc86a52
ryancarlson <ryan.t.carlson@cru.org>
2017-07-04 13:52:03 -0400

    Add briding header so Obj-c (boo) BA library can be used

---
+++ godtools/godtools-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  godtools-Bridging-Header.h
+//  godtools
+//
+//  Created by Ryan Carlson on 7/4/17.
+//  Copyright © 2017 Cru. All rights reserved.
+//
+
+#ifndef godtools_Bridging_Header_h
+#define godtools_Bridging_Header_h
+
+#import <Google/Analytics.h>
+
+#endif /* godtools_Bridging_Header_h */
