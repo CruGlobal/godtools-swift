89e6d0b1220c234ec1d7b1d19bb16feee0b88300
Pablo Marti <startpablo@gmail.com>
2017-07-07 14:08:34 -0600

    Fix text margin on paragraph

--- godtools/Views/TractElements/TractParagraph+UI.swift
+++ godtools/Views/TractElements/TractParagraph+UI.swift
@@ -15,6 +15,7 @@ extension TractParagraph {
         let properties = super.textStyle()
         properties.width = self.elementFrame.finalWidth()
         properties.xMargin = BaseTractElement.xMargin
+        properties.yMargin = TractParagraph.yMarginConstant
         properties.textColor = .gtWhite
         properties.textAlign = .center
         return properties
@@ -32,6 +33,7 @@ extension TractParagraph {
         let properties = super.textStyle()
         properties.width = self.elementFrame.finalWidth()
         properties.xMargin = xMargin
+        properties.yMargin = TractParagraph.yMarginConstant
         return properties
     }
     
@@ -46,8 +48,6 @@ extension TractParagraph {
         
         self.elementFrame.x = 0
         self.elementFrame.width = parentWidth()
-        self.elementFrame.yMarginTop = TractParagraph.yMarginConstant
-        self.elementFrame.yMarginBottom = TractParagraph.yMarginConstant
         self.elementFrame.xMargin = xMargin
     }
     
@@ -55,8 +55,6 @@ extension TractParagraph {
         let width = TractModal.contentWidth
         self.elementFrame.x = (self.parent!.width - width) / CGFloat(2)
         self.elementFrame.width = width
-        self.elementFrame.yMarginTop = TractParagraph.yMarginConstant
-        self.elementFrame.yMarginBottom = TractParagraph.yMarginConstant
     }
     
 }
