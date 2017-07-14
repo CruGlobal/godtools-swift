2b464c7fff2b8773f036f17ed23a6400ec9219c4
ryancarlson <ryan.t.carlson@cru.org>
2017-07-14 13:47:46 -0600

    Remove line used for testing

--- godtools/AppDelegate.swift
+++ godtools/AppDelegate.swift
@@ -80,10 +80,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             FirstLaunchInitializer().initializeAppState()
         }
         
-        self.flowController?.goToUniversalLinkedResource(DownloadedResourceManager().loadFromDisk(code: "kgp")!,
-                                                         language: languagesManager.loadFromDisk(code: "en")!,
-                                                         page: 5)
-        
         return languagesManager.loadFromRemote().then { (languages) -> Promise<DownloadedResources> in
             return DownloadedResourceManager().loadFromRemote()
             }.then { (resources) -> Promise<DownloadedResources> in
