5799d10e644e8d3df12762a432976475dfeb02df
ryancarlson <ryan.t.carlson@cru.org>
2017-12-18 16:47:23 -0500

    Test working for home screen capture

--- godtoolsUITests/godtoolsUITests.swift
+++ godtoolsUITests/godtoolsUITests.swift
@@ -12,15 +12,9 @@ class godtoolsUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
     }
     
     override func tearDown() {
@@ -28,9 +22,28 @@ class godtoolsUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testTakeScreenshots() {
+        
+        let app = XCUIApplication()
+        if app.buttons["ok"].exists {
+        app/*@START_MENU_TOKEN@*/.buttons["ok"]/*[[".buttons[\"Okay\"]",".buttons[\"ok\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        }
+        
+        if app.buttons["later"].exists {
+        app/*@START_MENU_TOKEN@*/.buttons["later"]/*[[".buttons[\"Later\"]",".buttons[\"later\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        }
+        
+        snapshot("01_HomeScreen")
+//
+//
+//        snapshot("02_LanguagesScreen")
+//
+//
+//
+//        snapshot("03_KGPLaw1")
+//        snapshot("04_4SLLaw1")
+//
+//        snapshot("05_SatisfiedPoint1")
     }
     
 }
