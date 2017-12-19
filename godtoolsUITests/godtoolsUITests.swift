541ad79c3c37f7c3bb0e8df6c0fa5d21167b8d35
ryancarlson <ryan.t.carlson@cru.org>
2017-12-18 22:18:49 -0500

    Update the test to work in all cases (almost), iPhone X needs fixing b/c we're not detecting when it's active correctly yet

--- godtoolsUITests/godtoolsUITests.swift
+++ godtoolsUITests/godtoolsUITests.swift
@@ -35,17 +35,42 @@ class godtoolsUITests: XCTestCase {
         
         snapshot("01_HomeScreen")
         
-        app.navigationBars["GodTools"].buttons["nav_languages"].tap()
+        app.navigationBars["navbar"].buttons["nav_languages"].tap()
         app/*@START_MENU_TOKEN@*/.buttons["languages_primary_btn"]/*[[".buttons[\"English\"]",".buttons[\"languages_primary_btn\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         
         snapshot("02_LanguagesScreen")
-//
-//
-//
-//        snapshot("03_KGPLaw1")
-//        snapshot("04_4SLLaw1")
-//
-//        snapshot("05_SatisfiedPoint1")
+        
+        // back to language settings screen
+        app.navigationBars["navbar"].buttons.element(boundBy: 0).tap()
+        
+        // go to select parallel language
+        app.buttons["languages_parallel_btn"].tap()
+        
+        // choose the 7th, whatever it is
+        app.tables.cells.element(boundBy: 6).tap()
+
+        // back to the home screen
+        app.navigationBars["navbar"].buttons.element(boundBy: 0).tap()
+        
+        app.tables.cells["kgp"].tap()
+        app.otherElements["Page 0"].buttons["right arrow blue"].tap()
+
+        snapshot("03_KGPLaw1")
+        
+        app.navigationBars["navbar"].buttons["nav_home"].tap()
+        
+        app.tables.cells["fourlaws"].tap()
+        app.otherElements["Page 0"].buttons["right arrow blue"].tap()
+        
+        snapshot("04_4SLLaw1")
+        
+        app.navigationBars["navbar"]/*@START_MENU_TOKEN@*/.buttons["nav_home"]/*[[".buttons[\"home\"]",".buttons[\"nav_home\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        app.tables.cells["satisfied"].tap()
+        app.otherElements["Page 0"].buttons["right arrow blue"].tap()
+        app.otherElements["Page 1"].swipeLeft()
+        
+        snapshot("05_SatisfiedPoint1")
     }
     
 }
