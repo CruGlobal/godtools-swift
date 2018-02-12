e54fdc8ac1bf9a68525f5bad60a89b1caafe4a96
ryancarlson <ryan.t.carlson@cru.org>
2018-02-12 12:46:42 -0500

    Add controlColor attribute to call-to-action
    Allows content to specify desired color for control (button). This change requires changing the button creation logic slightly to be a "system" button type because (shrug) Apple requires it that way?
    
    Default is still "GodTools Blue"

--- godtools/Views/TractElements/TractCallToAction+UI.swift
+++ godtools/Views/TractElements/TractCallToAction+UI.swift
@@ -17,9 +17,11 @@ extension TractCallToAction {
         let origin = CGPoint(x: xPosition, y: yPosition)
         let size = CGSize(width: self.buttonSizeConstant, height: self.buttonSizeConstant)
         let buttonFrame = CGRect(origin: origin, size: size)
-        let button = UIButton(frame: buttonFrame)
+        let button = UIButton(type: .system)
+        button.frame = buttonFrame
         let image = UIImage(named: "right_arrow_blue")
-        button.setBackgroundImage(image, for: UIControlState.normal)
+        button.setImage(image, for: UIControlState.normal)
+        button.tintColor = callToActionProperties().controlColor
         button.addTarget(self, action: #selector(moveToNextView), for: UIControlEvents.touchUpInside)
         self.addSubview(button)
     }
