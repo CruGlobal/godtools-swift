93cef7493c40d289f32b6db5b371c616baafdbdb
ryancarlson <ryan.t.carlson@cru.org>
2018-02-27 14:59:41 -0500

    Remove deprecated `characters` usage

--- godtools/Managers/ToolsManager.swift
+++ godtools/Managers/ToolsManager.swift
@@ -85,7 +85,7 @@ extension ToolsManager: UITableViewDelegate {
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         let resource = resources[indexPath.section]
         let primaryLanguage = LanguagesManager().loadPrimaryLanguageFromDisk()
-        if resource.localizedName(language: primaryLanguage).characters.count > 25 {
+        if resource.localizedName(language: primaryLanguage).count > 25 {
             return 133.0
         }
         
