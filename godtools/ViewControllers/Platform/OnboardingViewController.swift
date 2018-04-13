da00ad3c97ac5c349a7afe995befa3b12612d517
Greg Weiss <gergusa04@gmail.com>
2018-04-13 13:38:39 -0400

    removed add tools screen for onboarding, as per new designs, but left code in place for future use (commented out)

--- godtools/ViewControllers/Platform/OnboardingViewController.swift
+++ godtools/ViewControllers/Platform/OnboardingViewController.swift
@@ -21,34 +21,45 @@ class OnboardingViewController: BaseViewController {
         super.viewDidLoad()
         initialSetup()
         
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
-        swipeLeft.direction = .left
-        view.addGestureRecognizer(swipeLeft)
+        // This is currently not used, but left for future use when onboarding expands
         
-        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
-        swipeRight.direction = .right
-        view.addGestureRecognizer(swipeRight)
+//        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+//        swipeLeft.direction = .left
+//        view.addGestureRecognizer(swipeLeft)
+//
+//        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture))
+//        swipeRight.direction = .right
+//        view.addGestureRecognizer(swipeRight)
     }
     
     private func initialSetup() {
-        let viewWidth = view.frame.width
-        page2View.transform = CGAffineTransform(translationX: viewWidth, y: 0)
-        pageControl.transform = CGAffineTransform(scaleX: 2.0, y: 2.0);
+        // This is currently not used, but left for future use when onboarding expands
+        
+//        let viewWidth = view.frame.width
+//        page2View.transform = CGAffineTransform(translationX: viewWidth, y: 0)
+//        pageControl.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
+        
+        page2View.transform = CGAffineTransform(translationX: 0, y: 0)
         
         setupButtonLabels()
+        displayPage2()
     }
     
     private func setupButtonLabels() {
-        toolsOkayButton.setTitle("okay".localized, for: .normal)
+        // This is currently not used, but left for future use when onboarding expands
+    //    toolsOkayButton.setTitle("okay".localized, for: .normal)
+        
         languagesOkayButton.setTitle("okay".localized, for: .normal)
     }
     
     @objc fileprivate func handleGesture(gesture: UISwipeGestureRecognizer) {
-        if gesture.direction == .right {
-            displayPage1()
-        } else if gesture.direction == .left {
-            displayPage2()
-        }
+        // This is currently not used, but left for future use when onboarding expands
+        
+//        if gesture.direction == .right {
+//            displayPage1()
+//        } else if gesture.direction == .left {
+//            displayPage2()
+ //       }
     }
 
     override func didReceiveMemoryWarning() {
@@ -58,7 +69,8 @@ class OnboardingViewController: BaseViewController {
     // MARK: - Actions
     
     @IBAction func pressToolsOkayButton(_ sender: Any) {
-        displayPage2()
+        // This is currently not used, but left for future use when onboarding expands
+    //    displayPage2()
     }
 
     @IBAction func pressLanguagesOkayButton(_ sender: Any) {
@@ -67,6 +79,7 @@ class OnboardingViewController: BaseViewController {
     
     // MARK: Movement
     
+    // This is currently not used, but left for future use when onboarding expands
     fileprivate func displayPage1() {
         pageControl.currentPage -= 1
         let viewWidth = view.frame.width
