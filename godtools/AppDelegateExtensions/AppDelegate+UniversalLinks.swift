66d69d31b26de6d4617a77e02d605d107137cbd6
Greg Weiss <gergusa04@gmail.com>
2018-04-23 17:33:59 -0400

    parsing fallbacks differently and stubbing some other linkage functions

--- godtools/AppDelegateExtensions/AppDelegate+UniversalLinks.swift
+++ godtools/AppDelegateExtensions/AppDelegate+UniversalLinks.swift
@@ -6,10 +6,56 @@
 //  Copyright © 2017 Cru. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import PromiseKit
 
 extension AppDelegate {
+    
+    static let kCustomURLScheme = "godToolsCustomUrlScheme://"
+    
+    static let gtURL = URL(string: "itms-apps://itunes.apple.com/app/godtools/id542773210?ls=1&mt=8")
+    
+    static let appStoreAppID = "542773210?ls=1&mt=8"
+    
+
+    
+    class func openCustomApp() {
+        if openCustomURLScheme(customURLScheme: kCustomURLScheme) {
+            // app was opened successfully
+        } else {
+            // handle unable to open the app, perhaps redirect to the App Store
+            UIApplication.shared.openURL(URL(string: "itms://itunes.apple.com/app/id" + appStoreAppID)!)
+        }
+    }
+    
+    
+    class func openCustomURLScheme(customURLScheme: String) -> Bool {
+        guard let customURL = URL(string: customURLScheme) else { return false }
+        if UIApplication.shared.canOpenURL(customURL) {
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(customURL, options: [:], completionHandler: { (true) in
+                    // Do something here?
+                })
+            } else {
+                UIApplication.shared.openURL(customURL)
+            }
+            return true
+        }
+        
+        return false
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        let message = url.host?.removingPercentEncoding
+        let alertController = UIAlertController(title: "Incoming Message", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
+        alertController.addAction(okAction)
+        
+        window?.rootViewController?.present(alertController, animated: true, completion: nil)
+        
+        return true
+    }
     
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
         if userActivity.activityType != NSUserActivityTypeBrowsingWeb {
@@ -106,7 +152,32 @@ extension AppDelegate {
         }
         
         let languagesManager = LanguagesManager()
-        return languagesManager.loadFromDisk(code: "en")
+        var language: Language?
+        
+        switch pathParts.count {
+
+        case 5:
+           language = returnAlternateLanguage(pathParts, primary: 1, secondary: 4, manager: languagesManager)
+        case 6:
+            language = returnAlternateLanguage(pathParts, primary: 1, secondary: 5, manager: languagesManager)
+        case 7:
+            language = returnAlternateLanguage(pathParts, primary: 1, secondary: 6, manager: languagesManager)
+        default:
+            language = languagesManager.loadFromDisk(code: "en")
+        }
+        // TODO: - Find out where in the URL we are putting fallbacks
+        return language
+    }
+    
+    private func returnAlternateLanguage(_ pathParts: [String], primary: Int, secondary: Int, manager: LanguagesManager) -> Language? {
+        if let languagePrimary = manager.loadFromDisk(code: pathParts[primary])  {
+            return languagePrimary
+        } else if let languageSecondary = manager.loadFromDisk(code: pathParts[secondary]) {
+            return languageSecondary
+        } else {
+            return manager.loadFromDisk(code: "en")
+        }
+        
     }
     
     private func fallbackToOtherLanguage(url: URL)  {
