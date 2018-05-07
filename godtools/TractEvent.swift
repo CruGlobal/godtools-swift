f60316842a6b5c07bf3b4ef7f521c0f8b0b21b89
Greg Weiss <gergusa04@gmail.com>
2018-05-07 16:48:03 -0400

    trying to filter out analytics that cause stray characters

---
+++ godtools/TractEvent.swift
@@ -0,0 +1,37 @@
+//
+//  TractEvent.swift
+//  godtools
+//
+//  Created by Greg Weiss on 5/7/18.
+//  Copyright © 2018 Cru. All rights reserved.
+//
+
+import UIKit
+
+class TractEvent: BaseTractElement {
+    
+    // MARK: - Setup
+    
+    override func propertiesKind() -> TractProperties.Type {
+        return TractEventProperties.self
+    }
+    
+    override func setupView(properties: [String: Any]) {
+        super.setupView(properties: properties)
+        TractBindings.addBindings(self)
+    }
+    
+    // MARK: - Bindings
+    
+    override func elementListeners() -> [String]? {
+        let properties = eventProperties()
+        return properties.listeners == "" ? nil : properties.listeners.components(separatedBy: ",")
+    }
+    
+    // MARK: - Helpers
+    
+    func eventProperties() -> TractEventProperties {
+        return self.properties as! TractEventProperties
+    }
+    
+}
---
+++ godtools/TractEventProperties.swift
@@ -0,0 +1,24 @@
+//
+//  TractEventProperties.swift
+//  godtools
+//
+//  Created by Greg Weiss on 5/7/18.
+//  Copyright © 2018 Cru. All rights reserved.
+//
+
+import UIKit
+
+class TractEventProperties: TractProperties {
+    
+    // MARK: - XML Properties
+    
+    var subject: String = ""
+    var content: String = ""
+    var html: Bool = true
+    var listeners: String = ""
+    
+    override func defineProperties() {
+        self.properties = ["subject", "content", "html", "listeners"]
+    }
+    
+}
