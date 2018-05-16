b1fe01adff0d41a80c838e98d7d3d920578016f3
Greg Weiss <gergusa04@gmail.com>
2018-05-16 16:32:05 -0400

    added exhaustive component parser in case the languages-subtags scope increases

--- godtools/AppDelegateExtensions/AppDelegate+UniversalLinks.swift
+++ godtools/AppDelegateExtensions/AppDelegate+UniversalLinks.swift
@@ -150,7 +150,7 @@ extension AppDelegate {
         
         var languageOptions = languages.components(separatedBy: ",")
         if languageOptions.isEmpty { return tryLanguages }
-        languageOptions = addExtraGenericLanguageFallbacks(languageStrings: languageOptions)
+        languageOptions = parseForSubtagsAndDialects(languageStrings: languageOptions)
 
         tryLanguages.remove(at: 0)
         tryLanguages = languageOptions.flatMap { languagesManager.loadFromDisk(code: $0) }
@@ -196,34 +196,61 @@ extension AppDelegate {
         }
     }
     
-    // MARK: - This was a request from JesusFilm to add extra fallbacks they don't provide in the query
+    // MARK: - This was a request from JesusFilm to add extra fallbacks that aren't in the query
+    // For a language with a subtag and/or a dialect ex: "ms-pse-x-Ogan"
     
-    func addExtraGenericLanguageFallbacks(languageStrings: [String]) -> [String] {
+    func parseForSubtagsAndDialects(languageStrings: [String]) -> [String] {
+        
         var copiedLanguages: [String] = []
+        
         for language in languageStrings {
-            copiedLanguages.append(language)
-            
-            // Split out language components
+
+            // Split out language components to check for subtags
             if language.contains("-") {
                 let components = language.components(separatedBy: "-")
                 
-                // Check for specified sub-regions with a specialty?
-                if components.count > 3 {
-                    let combined = "\(components[0])-\(components[1])-\(components[2])"
-                    copiedLanguages.append(combined)
-                }
+                // This means there is one or more subtags, so we will parse and return all options
+                copiedLanguages.append(contentsOf: parseSubtagComponents(languageComponents: components))
                 
-                // Check for specified sub-regions
-                if components.count > 2 {
-                    let combined = "\(components[0])-\(components[1])"
-                    copiedLanguages.append(combined)
-                }
-                
-                // Finally add just the generic language
-                copiedLanguages.append(components[0])
+            } else {
+                copiedLanguages.append(language)
             }
         }
         return copiedLanguages
+    }
+    
+    func parseSubtagComponents(languageComponents: [String]) -> [String] {
+        
+        // We need a mutatable copy so it can be used again as it shrinks
+        var copiedComponents: [String] = languageComponents
+        
+        // This will be the returned output that gets appended to
+        var newComponents: [String] = []
+        
+        // Find out how many times to iterate
+        let count = languageComponents.count
+        
+        // No need to iterate unless there is more than one
+        guard count > 1 else { return newComponents }
+        
+        for _ in 1...count {
+            
+            // This will assemble all the parts of the language together ex: ms-pse-x-Ogan-
+            // Notice the extra "-" at the end. (addressed below...)
+            var languageName = copiedComponents.reduce("") { part1, part2  in "\(part1)\(part2)-"}
+            
+            // This removes the extra "-" character
+            languageName.removeLast()
+            
+            // This adds the assembled name to our new list, starting with full name.
+            newComponents.append(languageName)
+            
+            // This removes the last component from our copy before we iterate through again.
+            copiedComponents.removeLast()
+        }
+        
+        // Here we now have the array of ["ms-pse-x-Ogan", "ms-pse-x", "ms-pse", "ms"]
+        return newComponents
     }
     
 
