5439271fd240769f5173f5c3954be93d4da20a6f
Greg Weiss <gergusa04@gmail.com>
2018-05-18 15:40:19 -0400

    This will display a loading screen until all of the needed resources are ready for displaying

---
+++ godtools/ViewControllers/LoadingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoadingViewController.swift
+//  godtools
+//
+//  Created by Greg Weiss on 5/18/18.
+//  Copyright © 2018 Cru. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
