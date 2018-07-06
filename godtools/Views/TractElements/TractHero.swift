c0e189e973b0b904b3bf590d6a718d90500df92b
ryancarlson <ryan.t.carlson@cru.org>
2018-07-06 17:17:29 -0400

    Add a couple debug prints

--- godtools/Views/TractElements/TractHero.swift
+++ godtools/Views/TractElements/TractHero.swift
@@ -74,6 +74,8 @@ class TractHero: BaseTractElement {
     func updateHeroHeight(cards: TractCards) {
         self.heroHeight = cards.getMaxFreeHeight(hero: self)
         self.elementFrame.height = self.heroHeight
+        debugPrint(self.frame)
+        debugPrint(self.elementFrame.getFrame())
         self.frame = self.elementFrame.getFrame()
     }
 }
