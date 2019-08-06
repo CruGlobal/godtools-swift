266307bab94b64a58709c5349b46725f17536509
Dean Thibault <dean.thibault@cru.org>
2019-08-06 09:58:44 -0400

    GT-501: Handle localized language name overrides

--- godtools/Models/Language.swift
+++ godtools/Models/Language.swift
@@ -16,6 +16,7 @@ class Language: Object {
     @objc dynamic var remoteId = ""
     @objc dynamic var shouldDownload = false
     @objc dynamic var direction = ""
+    @objc dynamic var name: String?
 //    @objc dynamic var isPublished = true
     
     let translations = List<Translation>()
@@ -32,12 +33,27 @@ class Language: Object {
         return remoteId == GTSettings.shared.parallelLanguageId
     }
     
-    func localizedName() -> String {
-        guard let nameInLocale = NSLocale.current.localizedString(forIdentifier: self.code) else {
-            return self.code
+    func localizedName(locale: Locale = NSLocale.current, table: String? = nil) -> String {
+        
+        // Load localized string using language code as key
+        let nameInLocalization = Bundle.main.localizedString(forKey: code, value: nil, table: table)
+        // localizedString returns key if localized string not found. If returned string == code, then localized string not was not found
+        if nameInLocalization != code {
+            return nameInLocalization
         }
         
-        return nameInLocale
+        // Return device default localized language name, if found
+        if let localizedName = locale.localizedString(forIdentifier: self.code) {
+            return localizedName
+        }
+        
+        // Return default name supplied by API, if not nil
+        if let name = name {
+            return name
+        }
+        
+        // If no localization found, return the language code
+        return code
     }
     
     func isRightToLeft() -> Bool {
