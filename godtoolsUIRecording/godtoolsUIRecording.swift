563f9359c71500aa1096f93e03f0c762dae574fe
Dean Thibault <dean.thibault@cru.org>
2019-08-20 11:47:24 -0400

    Updated to generate all screen shots for english

--- godtoolsUIRecording/godtoolsUIRecording.swift
+++ godtoolsUIRecording/godtoolsUIRecording.swift
@@ -43,19 +43,104 @@ class godtoolsUIRecording: XCTestCase {
         
         app.navigationBars["Parallel Language"].buttons["Back"].tap()
         app.navigationBars["Language Settings"].buttons["Back"].tap()
-//        XCUIApplication().navigationBars["GodTools"].buttons["home"].tap()
         XCUIApplication()/*@START_MENU_TOKEN@*/.tables["home_table_view"].cells.containing(.staticText, identifier:"Knowing God Personally")/*[[".otherElements[\"home_my_tools_view\"].tables[\"home_table_view\"]",".cells.containing(.staticText, identifier:\"Uses hand drawn images to illustrate God's invitation to know Him personally.\")",".cells.containing(.staticText, identifier:\"Knowing God Personally\")",".tables[\"home_table_view\"]"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/.otherElements["Progress"].waitForExistence(timeout: 5)
         XCUIApplication()/*@START_MENU_TOKEN@*/.tables["home_table_view"].cells.containing(.staticText, identifier:"Knowing God Personally")/*[[".otherElements[\"home_my_tools_view\"].tables[\"home_table_view\"]",".cells.containing(.staticText, identifier:\"Uses hand drawn images to illustrate God's invitation to know Him personally.\")",".cells.containing(.staticText, identifier:\"Knowing God Personally\")",".tables[\"home_table_view\"]"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/.otherElements["Progress"].tap()
+        app.otherElements.containing(.navigationBar, identifier:"GodTools").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).buttons["right arrow blue"].tap()
         snapshot("03KnowingGod")
         
         app.navigationBars["GodTools"].buttons["home"].tap()
-        app.navigationBars["GodTools"]/*@START_MENU_TOKEN@*/.buttons["Find Tools"]/*[[".segmentedControls[\"GodTools\"].buttons[\"Find Tools\"]",".buttons[\"Find Tools\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.tables/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"Know God Personally")/*[[".cells.containing(.staticText, identifier:\"Explains how to begin a personal relationship with God.\")",".cells.containing(.staticText, identifier:\"Know God Personally\")"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.buttons["download green"].tap()
-        app.navigationBars["GodTools"]/*@START_MENU_TOKEN@*/.buttons["My Tools"]/*[[".segmentedControls[\"GodTools\"].buttons[\"My Tools\"]",".buttons[\"My Tools\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sleep(5)
+        if !XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"Know God Personally").otherElements["Progress"].waitForExistence(timeout: 5)
+        {
+            app.navigationBars["GodTools"]/*@START_MENU_TOKEN@*/.buttons["Find Tools"]/*[[".segmentedControls[\"GodTools\"].buttons[\"Find Tools\"]",".buttons[\"Find Tools\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+            app.tables/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"Know God Personally")/*[[".cells.containing(.staticText, identifier:\"Explains how to begin a personal relationship with God.\")",".cells.containing(.staticText, identifier:\"Know God Personally\")"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.buttons["download green"].tap()
+            app.navigationBars["GodTools"]/*@START_MENU_TOKEN@*/.buttons["My Tools"]/*[[".segmentedControls[\"GodTools\"].buttons[\"My Tools\"]",".buttons[\"My Tools\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+            sleep(5)
+        }
+        XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"Know God Personally").otherElements["Progress"].waitForExistence(timeout: 5)
         XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"Know God Personally").otherElements["Progress"].tap()
+        app.otherElements.containing(.navigationBar, identifier:"GodTools").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).buttons["right arrow blue"].tap()
         snapshot("04KnowGodPersonally")
 
         XCUIApplication().navigationBars["GodTools"].buttons["home"].tap()
+        sleep(5)
+
+        XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"Four Spiritual Laws").otherElements["Progress"].waitForExistence(timeout: 5)
+        XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"Four Spiritual Laws").otherElements["Progress"].tap()
+        XCUIApplication().otherElements.containing(.navigationBar, identifier:"GodTools").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).buttons["right arrow blue"].tap()
+        snapshot("05FourSpiritualLaws")
+
+        XCUIApplication().navigationBars["GodTools"].buttons["home"].tap()
+        sleep(5)
+        if !XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"Satisfied?").otherElements["Progress"].waitForExistence(timeout: 5)
+        {
+            app.navigationBars["GodTools"]/*@START_MENU_TOKEN@*/.buttons["Find Tools"]/*[[".segmentedControls[\"GodTools\"].buttons[\"Find Tools\"]",".buttons[\"Find Tools\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+            app.tables/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"Know God Personally")/*[[".cells.containing(.staticText, identifier:\"Explains how to begin a personal relationship with God.\")",".cells.containing(.staticText, identifier:\"Know God Personally\")"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.buttons["download green"].tap()
+            app.navigationBars["GodTools"]/*@START_MENU_TOKEN@*/.buttons["My Tools"]/*[[".segmentedControls[\"GodTools\"].buttons[\"My Tools\"]",".buttons[\"My Tools\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+            sleep(5)
+        }
+        XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"Satisfied?").otherElements["Progress"].waitForExistence(timeout: 5)
+        XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"Satisfied?").otherElements["Progress"].tap()
+        app.otherElements.containing(.navigationBar, identifier:"GodTools").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).buttons["right arrow blue"].tap()
+        snapshot("06Satisfied")
+
+        XCUIApplication().navigationBars["GodTools"].buttons["home"].tap()
+        sleep(5)
+        if !XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"THE FOUR").otherElements["Progress"].waitForExistence(timeout: 5)
+        {
+            app.navigationBars["GodTools"]/*@START_MENU_TOKEN@*/.buttons["Find Tools"]/*[[".segmentedControls[\"GodTools\"].buttons[\"Find Tools\"]",".buttons[\"Find Tools\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+            app.tables.cells.containing(.staticText, identifier:"THE FOUR").buttons["download green"].tap()
+            app.navigationBars["GodTools"]/*@START_MENU_TOKEN@*/.buttons["My Tools"]/*[[".segmentedControls[\"GodTools\"].buttons[\"My Tools\"]",".buttons[\"My Tools\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+            sleep(10)
+        }
+
+        XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"THE FOUR").otherElements["Progress"].waitForExistence(timeout: 5)
+        XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"THE FOUR").otherElements["Progress"].tap()
+        let element = XCUIApplication().otherElements.containing(.navigationBar, identifier:"GodTools").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        element.children(matching: .other).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element(boundBy: 1).swipeLeft()
         
+        let element2 = element.children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element(boundBy: 1)
+        element2.swipeLeft()
+        element2.swipeLeft()
+        snapshot("07TheFour")
+        
+        XCUIApplication().navigationBars["GodTools"].buttons["home"].tap()
+        sleep(5)
+        if !XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"Honor Restored").otherElements["Progress"].waitForExistence(timeout: 5)
+        {
+            app.navigationBars["GodTools"]/*@START_MENU_TOKEN@*/.buttons["Find Tools"]/*[[".segmentedControls[\"GodTools\"].buttons[\"Find Tools\"]",".buttons[\"Find Tools\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+            app.tables.cells.containing(.staticText, identifier:"Honor Restored").buttons["download green"].tap()
+            app.navigationBars["GodTools"]/*@START_MENU_TOKEN@*/.buttons["My Tools"]/*[[".segmentedControls[\"GodTools\"].buttons[\"My Tools\"]",".buttons[\"My Tools\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+            sleep(10)
+        }
+        XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"Honor Restored").otherElements["Progress"].waitForExistence(timeout: 5)
+        XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"Honor Restored").otherElements["Progress"].tap()
+        snapshot("08HonorRestored")
+        
+        XCUIApplication().navigationBars["GodTools"].buttons["home"].tap()
+        sleep(5)
+        XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"Teach Me to Share").otherElements["Progress"].waitForExistence(timeout: 5)
+        XCUIApplication().tables["home_table_view"].cells.containing(.staticText, identifier:"Teach Me to Share").otherElements["Progress"].tap()
+        snapshot("09TeachMeToShare")
+
     }
+    
+//    func testSomething() {
+//
+//        let app = XCUIApplication()
+//        let godtoolsNavigationBar = app.navigationBars["GodTools"]
+//        godtoolsNavigationBar/*@START_MENU_TOKEN@*/.buttons["Find Tools"]/*[[".segmentedControls[\"GodTools\"].buttons[\"Find Tools\"]",".buttons[\"Find Tools\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+//        app.tables/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"THE FOUR")/*[[".cells.containing(.staticText, identifier:\"Uses four symbols to simply explain the message of the Bible.\")",".cells.containing(.staticText, identifier:\"THE FOUR\")"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.buttons["download green"].tap()
+//        godtoolsNavigationBar/*@START_MENU_TOKEN@*/.buttons["My Tools"]/*[[".segmentedControls[\"GodTools\"].buttons[\"My Tools\"]",".buttons[\"My Tools\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+//        XCUIApplication().navigationBars["GodTools"].buttons["home"].tap()
+//
+//
+//        let element = XCUIApplication().otherElements.containing(.navigationBar, identifier:"GodTools").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+//        element.children(matching: .other).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element(boundBy: 1).swipeLeft()
+//
+//        let element2 = element.children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element(boundBy: 1)
+//        element2.swipeLeft()
+//        element2.swipeLeft()
+//
+//    }
 }
