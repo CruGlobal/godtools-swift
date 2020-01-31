497209093a503982dbe0f09f6d3a5707e4ece87b
Levi Eggert <levi.eggert@cru.org>
2020-01-31 09:35:46 -0500

    GT-637 Removed notification observer, not being used

--- godtools/Share/Services/DeviceLanguagePreferences.swift
+++ godtools/Share/Services/DeviceLanguagePreferences.swift
@@ -8,37 +8,14 @@
 
 import Foundation
 
-class DeviceLanguagePreferences: NSObject, LanguagePreferencesType {
+struct DeviceLanguagePreferences: LanguagePreferencesType {
     
-    override init() {
+    init() {
         
-        super.init()
-        
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(handleLocationChangedNotification(notification:)),
-            name: NSLocale.currentLocaleDidChangeNotification,
-            object: nil
-        )
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(
-            self,
-            name: NSLocale.currentLocaleDidChangeNotification,
-            object: nil
-        )
     }
     
     var isEnglish: Bool {
         let languageCode: String? = NSLocale.current.languageCode
         return languageCode == "en" || languageCode == "en_US"
-    }
-    
-   @objc func handleLocationChangedNotification(notification: Notification) {
-    
-        print("\nLocation changed")
-        print("  object: \(String(describing: notification.object))")
-        print("  userInfo: \(String(describing: notification.userInfo))")
     }
 }
