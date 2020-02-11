fd0ba614a7e4cb24a56726966830477be1ffcf3d
Levi Eggert <levi.eggert@cru.org>
2020-02-11 15:31:02 -0500

    GT-668 Changes to analytics action for home tutorial notification dismiss

--- godtools/Share/Views/OpenTutorialView/OpenTutorialViewModel.swift
+++ godtools/Share/Views/OpenTutorialView/OpenTutorialViewModel.swift
@@ -40,6 +40,6 @@ class OpenTutorialViewModel: OpenTutorialViewModelType {
         tutorialServices.disableOpenTutorialCallout()
         hidesOpenTutorial.accept(value: (hidden: true, animated: true))
         
-        analytics.recordActionForADBMobile(screenName: "home", actionName: "Close Tutorial Notification", data: ["cru.tutorial_notification_close": 1])
+        analytics.recordActionForADBMobile(screenName: "home", actionName: "Tutorial Home Tutorial Dismiss", data: ["cru.tutorial_home_tutorial_dismiss": 1])
     }
 }
