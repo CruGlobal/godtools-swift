fef12cfe0726e3686706f8e4e66655b296b4d1dc
Levi Eggert <levi.eggert@cru.org>
2020-02-26 15:39:19 -0500

    GT-685 Hiding open tutorial callout when the tutorial option is tapped from the menu

---
+++ godtools/Share/Objects/SignalValue.swift
@@ -0,0 +1,42 @@
+//
+//  SignalValue.swift
+//  godtools
+//
+//  Created by Levi Eggert on 2/26/20.
+//  Copyright © 2020 Cru. All rights reserved.
+//
+
+import Foundation
+
+class SignalValue<T> {
+    
+    typealias Handler = ((_ value: T) -> Void)
+    
+    private var observers: [String: Handler] = Dictionary()
+        
+    required init() {
+
+    }
+    
+    deinit {
+        observers.removeAll()
+    }
+    
+    func accept(value: T) {
+        notifyAllObservers(value: value)
+    }
+    
+    func addObserver(_ observer: NSObject, onObserve: @escaping Handler) {
+        observers[observer.description] = onObserve
+    }
+    
+    func removeObserver(_ observer: NSObject) {
+        observers[observer.description] = nil
+    }
+    
+    private func notifyAllObservers(value: T) {
+        for (_, onObserve) in observers {
+            onObserve(value)
+        }
+    }
+}
