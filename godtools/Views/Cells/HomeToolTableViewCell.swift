11174e61bfdfc5e2a30667d63d021a4643e4d46b
Robert Eldredge <robert.eldredge@cru.org>
2020-06-17 11:11:27 -0400

    swift file fixes and layout updates

--- godtools/Views/Cells/HomeToolTableViewCell.swift
+++ godtools/Views/Cells/HomeToolTableViewCell.swift
@@ -16,25 +16,12 @@ protocol HomeToolTableViewCellDelegate {
 class HomeToolTableViewCell: UITableViewCell {
     
     @IBOutlet weak var shadowView: UIView!
-    @IBOutlet weak var borderView: UIView!
-    @IBOutlet weak var contentTopView: UIView!
-    @IBOutlet weak var contentBottomView: UIView!
+    @IBOutlet weak var toolContentView: UIView!
     @IBOutlet weak var bannerImageView: UIImageView!
-    @IBOutlet weak var mainContentView: UIView!
-    @IBOutlet weak var downloadButton: UIButton!
-    @IBOutlet weak var greyVerticalLine: UIImageView!
-    @IBOutlet weak var titleLabel: GTLabel!
-    
-    // Now using this for description of Tract
-    @IBOutlet weak var tractDescriptionLabel: GTLabel!
-    
-    @IBOutlet weak var languageLabel: GTLabel!
-    @IBOutlet weak var titleLeadingConstraint: NSLayoutConstraint!
-    @IBOutlet weak var numberOfViewsLeadingConstraint: NSLayoutConstraint!
-    @IBInspectable var leftConstraintValue: CGFloat = 8.0
-    @IBInspectable var defaultTitleLeadingConstraint: CGFloat = 47.0
-    @IBInspectable var defaultNumberOfViewsLeadingConstraint: CGFloat = 47.0
-    @IBOutlet weak var downloadProgressView: GTProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var downloadProgressView: UIProgressView!
     
     private (set) var resource: DownloadedResource?
     private (set) var cellDelegate: HomeToolTableViewCellDelegate?
@@ -45,13 +32,6 @@ class HomeToolTableViewCell: UITableViewCell {
         super.awakeFromNib()
         setupUI()
         registerProgressViewListener()
-    }
-    
-    override func prepareForReuse() {
-        downloadButton.isHidden = false
-        greyVerticalLine.isHidden = false
-        titleLeadingConstraint.constant = defaultTitleLeadingConstraint
-        numberOfViewsLeadingConstraint.constant = defaultNumberOfViewsLeadingConstraint
     }
     
     func configure(resource: DownloadedResource,
@@ -76,10 +56,6 @@ class HomeToolTableViewCell: UITableViewCell {
         
         isAvailable = isAvailableInPrimaryLanguage
         
-        if resource.shouldDownload || resource.numberOfAvailableLanguages() == 0 {
-            setCellAsDisplayOnly()
-        }
-        
         bannerImageView.image = banner ?? #imageLiteral(resourceName: "cell_banner_placeholder")
 
         accessibilityIdentifier = resource.name
@@ -94,7 +70,7 @@ class HomeToolTableViewCell: UITableViewCell {
         
         configureParallelLanguageLabel(parallelLanguage: parallelLanguage)
 
-        tractDescriptionLabel.text = loadDescription(resource: resource)
+        categoryLabel.text = loadDescription(resource: resource)
     }
     
     private func configureParallelLanguageLabel(parallelLanguage: Language?) {
@@ -108,13 +84,6 @@ class HomeToolTableViewCell: UITableViewCell {
         } else {
             languageLabel.text = nil
         }
-    }
-    
-    private func setCellAsDisplayOnly() {
-        downloadButton.isHidden = true
-        greyVerticalLine.isHidden = true
-        titleLeadingConstraint.constant = leftConstraintValue
-        numberOfViewsLeadingConstraint.constant = leftConstraintValue
     }
     
     private func loadDescription(resource: DownloadedResource) -> String {
@@ -157,7 +126,7 @@ class HomeToolTableViewCell: UITableViewCell {
     }
     
     func setBorders() {
-        let layer = borderView.layer
+        let layer = toolContentView.layer
         layer.cornerRadius = 5.0
         layer.masksToBounds = true
         layer.borderWidth = 1.0
@@ -182,7 +151,7 @@ class HomeToolTableViewCell: UITableViewCell {
     // MARK: Present data
     
     fileprivate func displayData(resource: DownloadedResource) {
-        self.tractDescriptionLabel.text = loadDescription(resource: resource)
+        self.categoryLabel.text = loadDescription(resource: resource)
     }
     
     // MARK: Progress view listener
