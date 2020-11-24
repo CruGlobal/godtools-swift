4f51257d8571b3bd0b32023f2ba902c5fcc424b5
Levi Eggert <>
2020-11-24 16:02:11 -0500

    GT-841 parsing delay and trigger for analytics

--- godtools/Share/Services/Renderer/MobileContentXmlNodeParser/Nodes/AnalyticsEventNode.swift
+++ godtools/Share/Services/Renderer/MobileContentXmlNodeParser/Nodes/AnalyticsEventNode.swift
@@ -12,14 +12,18 @@ import SWXMLHash
 class AnalyticsEventNode: MobileContentXmlNode {
         
     let action: String?
+    let delay: String?
     let systems: [String]
+    let trigger: String?
     
     required init(xmlElement: XMLElement) {
     
         let attributes: [String: XMLAttribute] = xmlElement.allAttributes
         
         action = attributes["action"]?.text
+        delay = attributes["delay"]?.text
         systems = attributes["system"]?.text.components(separatedBy: " ") ?? []
+        trigger = attributes["trigger"]?.text
         
         super.init(xmlElement: xmlElement)
     }
