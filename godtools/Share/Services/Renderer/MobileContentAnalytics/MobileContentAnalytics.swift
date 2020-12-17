bebc5c4de13949b6ef974f9feeded32e66c7344d
Levi Eggert <>
2020-12-17 15:08:38 -0500

    GT-1003 include firebase in analytics systems dictionary

--- godtools/Share/Services/Renderer/MobileContentAnalytics/MobileContentAnalytics.swift
+++ godtools/Share/Services/Renderer/MobileContentAnalytics/MobileContentAnalytics.swift
@@ -11,30 +11,17 @@ import Foundation
 class MobileContentAnalytics {
     
     private let analyticsSystems: [String: MobileContentAnalyticsSystem]
-    
-    // TODO: We will REMOVE this once firebase is included in the xml and no longer tied to adobe from the xml. ~Levi
-    private let firebaseAnalyticsSystem: MobileContentAnalyticsSystem
-    
+        
     required init(analytics: AnalyticsContainer) {
            
-        // TODO: We will need to ADD this back in once firebase is included in the xml and no longer tied to adobe from the xml. ~Levi
-        /*
         let analyticsSystems = [
             "adobe": analytics.adobeAnalytics,
             "appsflyer": analytics.appsFlyer,
             "firebase": analytics.firebaseAnalytics,
             "snowplow": analytics.snowplowAnalytics
-        ]*/
-        
-        // TODO: We will REMOVE this once firebase is included in the xml and no longer tied to adobe from the xml. ~Levi
-        let analyticsSystems = [
-            "adobe": analytics.adobeAnalytics,
-            "appsflyer": analytics.appsFlyer,
-            "snowplow": analytics.snowplowAnalytics
         ]
-        
+ 
         self.analyticsSystems = analyticsSystems
-        self.firebaseAnalyticsSystem = analytics.firebaseAnalytics
     }
     
     func trackEvents(events: AnalyticsEventsNode) {
@@ -71,7 +58,7 @@ class MobileContentAnalytics {
             
             // TODO: We will REMOVE this once firebase is included in the xml and no longer tied to adobe from the xml. ~Levi
             if system == "adobe" {
-                firebaseAnalyticsSystem.trackAction(action: action, data: data)
+                analyticsSystems["firebase"]?.trackAction(action: action, data: data)
             }
          }
     }
