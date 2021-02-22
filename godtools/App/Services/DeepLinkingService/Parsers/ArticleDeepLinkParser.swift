37ea8621158623f8c540dc15e7b1eb8d4f6b209c
Robert Eldredge <robert.eldredge@cru.org>
2021-02-22 10:39:06 -0500

    Work on Article Parser

--- godtools/App/Services/DeepLinkingService/Parsers/ArticleDeepLinkParser.swift
+++ godtools/App/Services/DeepLinkingService/Parsers/ArticleDeepLinkParser.swift
@@ -29,13 +29,12 @@ class ArticleDeepLinkParser: DeepLinkParserType {
         //  https://godtoolsapp.com/article/aem?uri=https://www.cru.org/content/experience-fragments/shared-library/language-masters/en/how-to-know-god/what-is-christianity/does-god-answer-our-prayers-/godtools
         
         let pathComponents: [String] = url.pathComponents.filter({$0 != "/"})
-        let urlString = url.absoluteString
-
+        
         guard url.containsDeepLinkHost(deepLinkHost: .godToolsApp), pathComponents[0] == "article", pathComponents[1] == "aem"  else {
             return nil
         }
         
-        var articleURI: String? = nil
+        var articleURIFromURL: String? = nil
         
         let components: URLComponents? = URLComponents(url: url, resolvingAgainstBaseURL: false)
         let queryItems: [URLQueryItem] = components?.queryItems ?? []
@@ -45,62 +44,12 @@ class ArticleDeepLinkParser: DeepLinkParserType {
             let value: String? = queryItem.value
                         
             if key == "uri", let value = value {
-               articleURI = value
+                articleURIFromURL = value
             }
         }
         
+        guard let articleURI = articleURIFromURL else { return nil }
         
-        
-        
-        /*if pathComponents.count > 0 {
-            primaryLanguageCodeFromUrlPath = pathComponents[0]
-        }
-        
-        if pathComponents.count > 1 {
-            resourceAbbreviationFromUrlPath = pathComponents[1]
-        }
-        
-        if pathComponents.count > 2, let pageIntegerValue = Int(pathComponents[2]) {
-            pageFromUrlPath = pageIntegerValue
-        }
-        
-        var primaryLanguageCodes: [String] = Array()
-        var parallelLanguageCodes: [String] = Array()
-        var liveShareStream: String?
-        
-        let components: URLComponents? = URLComponents(url: url, resolvingAgainstBaseURL: false)
-        let queryItems: [URLQueryItem] = components?.queryItems ?? []
-        
-        for queryItem in queryItems {
-            
-            let key: String = queryItem.name
-            let value: String? = queryItem.value
-                        
-            if key == "primaryLanguage", let value = value {
-                primaryLanguageCodes = value.components(separatedBy: ",")
-            }
-            else if key == "parallelLanguage", let value = value {
-                parallelLanguageCodes = value.components(separatedBy: ",")
-            }
-            else if key == "liveShareStream" {
-                liveShareStream = value
-            }
-        }
-        
-        if let primaryLanguageCodeFromUrlPath = primaryLanguageCodeFromUrlPath {
-            primaryLanguageCodes.insert(primaryLanguageCodeFromUrlPath, at: 0)
-        }
-                
-        guard let resourceAbbreviation = resourceAbbreviationFromUrlPath, !resourceAbbreviation.isEmpty, !primaryLanguageCodes.isEmpty else {
-            return nil
-        }
-        
-        return .article(
-            resourceAbbreviation: resourceAbbreviation,
-            translationZipFile: <#T##TranslationZipFileModel#>,
-            articleAemImportData: <#T##ArticleAemImportData#>
-        )*/
-        
-        return nil
+        return .article(articleURI: articleURI)
     }
 }
