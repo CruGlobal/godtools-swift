22233f39d67679854891c8cf82db5a28cfda5708
Levi Eggert <>
2021-05-04 13:47:40 -0400

    Adding close lesson event handling

--- godtools/App/Services/Renderer/Lessons/Views/Page/LessonPageView.swift
+++ godtools/App/Services/Renderer/Lessons/Views/Page/LessonPageView.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 
+protocol LessonPageViewDelegate: class {
+    
+    func lessonPageCloseLessonTapped(lessonPage: LessonPageView)
+}
+
 class LessonPageView: MobileContentPageView {
     
     private let viewModel: LessonPageViewModelType
     private let safeArea: UIEdgeInsets
     
     private var contentView: LessonContentView?
+    
+    private weak var delegate: LessonPageViewDelegate?
     
     @IBOutlet weak private var contentContainerView: UIView!
     
@@ -62,6 +69,10 @@ class LessonPageView: MobileContentPageView {
         
     }
     
+    func setLessonPageDelegate(delegate: LessonPageViewDelegate?) {
+        self.delegate = delegate
+    }
+    
     // MARK: - MobileContentView
 
     override func renderChild(childView: MobileContentView) {
@@ -70,6 +81,15 @@ class LessonPageView: MobileContentPageView {
         
         if let contentView = childView as? LessonContentView {
             addContentView(contentView: contentView)
+        }
+    }
+    
+    override func didReceiveEvents(events: [String]) {
+        
+        for event in events {
+            if viewModel.manifestDismissListeners.contains(event) {
+                delegate?.lessonPageCloseLessonTapped(lessonPage: self)
+            }
         }
     }
 }
