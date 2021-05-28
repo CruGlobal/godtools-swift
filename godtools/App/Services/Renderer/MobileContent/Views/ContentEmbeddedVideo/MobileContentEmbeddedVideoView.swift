58c59fabc19640b3d4ea529e83818e02bcc5f2de
Robert Eldredge <robert.eldredge@cru.org>
2021-05-28 14:33:23 -0400

    change height

--- godtools/App/Services/Renderer/MobileContent/Views/ContentEmbeddedVideo/MobileContentEmbeddedVideoView.swift
+++ godtools/App/Services/Renderer/MobileContent/Views/ContentEmbeddedVideo/MobileContentEmbeddedVideoView.swift
@@ -55,7 +55,7 @@ class MobileContentEmbeddedVideoView: MobileContentView {
     // MARK: - MobileContentView
 
     override var contentStackHeightConstraintType: MobileContentStackChildViewHeightConstraintType {
-        return .equalToHeight(height: 300)
+        return .equalToHeight(height: 240)
     }
 }
 
