beb67b9fa0b0c831451c2392040205e89d21b9b7
Levi Eggert <levi.eggert@cru.org>
2021-08-17 13:24:02 -0400

    Added PageModelType protocol to MultiplatformLessonPage

--- godtools/App/Services/Renderer/Parser/Multiplatform/Models/MultiplatformLessonPage.swift
+++ godtools/App/Services/Renderer/Parser/Multiplatform/Models/MultiplatformLessonPage.swift
@@ -9,12 +9,104 @@
 import UIKit
 import GodToolsToolParser
 
-class MultiplatformLessonPage {
+class MultiplatformLessonPage: PageModelType {
     
     private let lessonPage: LessonPage
+    
+    let uuid: String = UUID().uuidString
     
     required init(lessonPage: LessonPage) {
         
         self.lessonPage = lessonPage
+    }
+    
+    var backgroundImage: String? {
+        return lessonPage.backgroundImage?.name
+    }
+    
+    var backgroundImageAlignments: [MobileContentBackgroundImageAlignment] {
+        return [.center] // TODO: Need to set this. ~Levi
+    }
+    
+    var backgroundImageScale: MobileContentBackgroundImageScale {
+        switch lessonPage.backgroundImageScaleType {
+        case .fill:
+            return .fill
+        case .fit:
+            return .fit
+        case .fillX:
+            return .fillHorizontally
+        case .fillY:
+            return .fillVertically
+        default:
+            assertionFailure("Found unsupported type, returning fill.  Ensure case is supported.")
+            return .fill
+        }
+    }
+    
+    var listeners: [String] {
+        return lessonPage.listeners.map({$0.description()})
+    }
+    
+    var textScale: MobileContentTextScale {
+        return MobileContentTextScale(doubleValue: lessonPage.textScale)
+    }
+    
+    var isHidden: Bool {
+        return lessonPage.isHidden
+    }
+    
+    var hero: HeroModelType? {
+        return nil
+    }
+    
+    var callToAction: CallToActionModelType? {
+        return nil
+    }
+    
+    func getBackgroundColor() -> MobileContentColor? {
+        return MobileContentColor(color: lessonPage.backgroundColor)
+    }
+    
+    func getCardTextColor() -> MobileContentColor? {
+        return nil
+    }
+    
+    func getPrimaryColor() -> MobileContentColor? {
+        return MobileContentColor(color: lessonPage.primaryColor)
+    }
+    
+    func getPrimaryTextColor() -> MobileContentColor? {
+        return MobileContentColor(color: lessonPage.primaryTextColor)
+    }
+    
+    func getTextColor() -> MobileContentColor? {
+        return MobileContentColor(color: lessonPage.textColor)
+    }
+}
+
+// MARK: - MobileContentRenderableModel
+
+extension MultiplatformLessonPage {
+    
+    var restrictTo: String? {
+        return nil
+    }
+    
+    var version: String? {
+        return nil
+    }
+    
+    var modelContentIsRenderable: Bool {
+        return true
+    }
+    
+    func getRenderableChildModels() -> [MobileContentRenderableModel] {
+        
+        var childModels: [MobileContentRenderableModel] = Array()
+        
+        addContentToChildModels(childModels: &childModels, content: lessonPage.content)
+        
+        return childModels
     }
 }
