c512bec7f175c2b7c5a74cd61bd77dafc09f5214
Robert Eldredge <robert.eldredge@cru.org>
2021-10-25 16:35:59 -0400

    Add comment about resizing

--- godtools/App/Share/Extensions/UIImage+ScalePresevingAspectRatio.swift
+++ godtools/App/Share/Extensions/UIImage+ScalePresevingAspectRatio.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 extension UIImage {
-    
+    //found here: https://www.advancedswift.com/resize-uiimage-no-stretching-swift/
     func scalePreservingAspectRatio(targetSize: CGSize) -> UIImage {
             // Determine the scale factor that preserves aspect ratio
             let widthRatio = targetSize.width / size.width
