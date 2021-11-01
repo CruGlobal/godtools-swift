860563ea52eb52804784365a11d3b3b8f29ffa6d
Robert Eldredge <robert.eldredge@cru.org>
2021-11-01 14:55:34 -0400

    Hide video view

--- godtools/App/Features/Onboarding/Views/OnboardingTutorialIntroView.swift
+++ godtools/App/Features/Onboarding/Views/OnboardingTutorialIntroView.swift
@@ -15,8 +15,6 @@ class OnboardingTutorialIntroView: UIView, NibBased {
     @IBOutlet weak private var titleLabel: UILabel!
     @IBOutlet weak private var videoButton: UIButton!
     @IBOutlet weak private var youTubeVideoPlayer: YTPlayerView!
-    @IBOutlet weak private var youTubeVideoPlayerLoadingView: UIView!
-    @IBOutlet weak private var youTubeVideoPlayerActivityIndicator: UIActivityIndicatorView!
     
     private var viewModel: OnboardingTutorialIntroViewModelType?
     
@@ -33,9 +31,7 @@ class OnboardingTutorialIntroView: UIView, NibBased {
     var youtubePlayerParameters: [String : Any]? {
         let playsInFullScreen = 0
         
-        return [
-            "playsinline": playsInFullScreen
-        ]
+        return ["playsinline": playsInFullScreen]
     }
     
     func configure(viewModel: OnboardingTutorialIntroViewModelType) {
@@ -47,28 +43,27 @@ class OnboardingTutorialIntroView: UIView, NibBased {
         logoImageView.image = viewModel.logoImage
         
         loadYoutubePlayerVideo(videoId: viewModel.youtubeVideoId)
+        
+        setupBinding()
     }
     
     private func setupBinding() {
         
         videoButton.addTarget(self, action: #selector(videoLinkTapped(button:)), for: .touchUpInside)
     }
-
+    
     @objc private func videoLinkTapped (button: UIButton) {
+        
+        youTubeVideoPlayer.playVideo()
         
         viewModel?.videoLinkTapped()
     }
     
     private func loadYoutubePlayerVideo(videoId: String) {
         
-        youTubeVideoPlayerActivityIndicator.startAnimating()
         youTubeVideoPlayer.delegate = self
         youTubeVideoPlayer.load(withVideoId: videoId, playerVars: youtubePlayerParameters)
-    }
-    
-    private func stopVideo() {
-        
-        youTubeVideoPlayer.stopVideo()
+        youTubeVideoPlayer.isHidden = true
     }
     
     private func recueVideo() {
@@ -86,14 +81,6 @@ extension OnboardingTutorialIntroView: YTPlayerViewDelegate {
     func playerViewDidBecomeReady(_ playerView: YTPlayerView) {
         
         print("\n ToolDetailView player view did become ready")
-        
-        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: { [weak self] in
-            self?.youTubeVideoPlayerLoadingView.alpha = 0
-        }) { [weak self] (finished: Bool) in
-            self?.youTubeVideoPlayerLoadingView.isHidden = true
-            self?.youTubeVideoPlayerLoadingView.alpha = 1
-            self?.youTubeVideoPlayerActivityIndicator.stopAnimating()
-        }
     }
     
     func playerView(_ playerView: YTPlayerView, didChangeTo state: YTPlayerState) {
