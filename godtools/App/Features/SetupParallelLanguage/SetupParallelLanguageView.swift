b1a1d0a83eac0cfc21a99b5613ba38698f00b58b
Robert Eldredge <robert.eldredge@cru.org>
2021-12-01 09:56:08 -0500

    working on layout

--- godtools/App/Features/SetupParallelLanguage/SetupParallelLanguageView.swift
+++ godtools/App/Features/SetupParallelLanguage/SetupParallelLanguageView.swift
@@ -20,8 +20,8 @@ class SetupParallelLanguageView: UIViewController {
     @IBOutlet weak private var promptLabel: UILabel!
     @IBOutlet weak private var selectLanguageButton: UIButton!
     //@IBOutlet weak private var languagePicker: UIPickerView!
-    //@IBOutlet weak private var yesButton: UIButton!
-    //@IBOutlet weak private var noButton: UIButton!
+    @IBOutlet weak private var yesButton: UIButton!
+    @IBOutlet weak private var noButton: UIButton!
     //@IBOutlet weak private var selectLanguageButton: UIButton!
     //@IBOutlet weak private var getStartedButton: UIButton!
 
@@ -44,11 +44,25 @@ class SetupParallelLanguageView: UIViewController {
         
         super.viewDidLoad()
         
+        yesButton.addTarget(self, action: #selector(handleYesTapped), for: .touchUpInside)
+        
+        noButton.addTarget(self, action: #selector(handleNoTapped), for: .touchUpInside)
+        
+        setupBinding()
         setupLayout()
+    }
+    
+    private func setupBinding() {
         
         animatedView.configure(viewModel: viewModel.animatedViewModel)
         
         promptLabel.text = viewModel.promptText
+        
+        selectLanguageButton.setTitle(viewModel.languagePickerLabelText, for: .normal)
+        
+        yesButton.setTitle(viewModel.yesButtonText, for: .normal)
+        
+        noButton.setTitle(viewModel.noButtonText, for: .normal)
     }
     
     private func setupLayout() {
@@ -58,8 +72,14 @@ class SetupParallelLanguageView: UIViewController {
             image: ImageCatalog.navClose.image,
             color: ColorPalette.gtBlue.color,
             target: self,
-            action: #selector(handleClose)
+            action: #selector(handleCloseTapped)
         )
+        
+        setupSelectLanguageButton()
+        setupBottomButtons()
+    }
+    
+    func setupSelectLanguageButton() {
         
         selectLanguageButton.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
         selectLanguageButton.layer.shadowOffset = CGSize(width: 2, height: 4)
@@ -67,9 +87,35 @@ class SetupParallelLanguageView: UIViewController {
         selectLanguageButton.layer.shadowRadius = 0.0
         selectLanguageButton.layer.masksToBounds = false
         selectLanguageButton.layer.cornerRadius = 6
+        
+        selectLanguageButton.setImageColor(color: UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1))
+        
+        selectLanguageButton.layoutIfNeeded()
+        
+        let buttonWidth = selectLanguageButton.frame.size.width
+        let imageWidth = selectLanguageButton.image(for: .normal)?.size.width ?? 0
+        
+        selectLanguageButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: buttonWidth - imageWidth - 10, bottom: 0, right: 0)
     }
     
-    @objc func handleClose(barButtonItem: UIBarButtonItem) {
+    func setupBottomButtons() {
+        
+        yesButton.layer.cornerRadius = 6
+        
+        noButton.layer.cornerRadius = 6
+        noButton.drawBorder(color: ColorPalette.gtBlue.color)
+        noButton.layer.borderWidth = 1
+    }
+    
+    @objc func handleCloseTapped(barButtonItem: UIBarButtonItem) {
         viewModel.closeButtonTapped()
+    }
+    
+    @objc func handleYesTapped(barButtonItem: UIBarButtonItem) {
+        viewModel.yesButtonTapped()
+    }
+    
+    @objc func handleNoTapped(barButtonItem: UIBarButtonItem) {
+        viewModel.noButtonTapped()
     }
 }
