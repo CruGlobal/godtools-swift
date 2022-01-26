1b2466ce0c5bc834eca555d123b902bd2a9d05b4
Levi Eggert <levi.eggert@cru.org>
2022-01-26 13:48:02 -0500

    Add MobileContentFlowRow

---
+++ godtools/App/Services/Renderer/Views/MobileContent/Views/ContentFlowRow/MobileContentFlowRowItem.swift
@@ -0,0 +1,14 @@
+//
+//  MobileContentFlowRowItem.swift
+//  godtools
+//
+//  Created by Levi Eggert on 1/25/22.
+//  Copyright © 2022 Cru. All rights reserved.
+//
+
+import Foundation
+
+protocol MobileContentFlowRowItem: MobileContentView {
+    
+    var itemWidth: MobileContentViewWidth { get }
+}
