dc5b4b11496821592ec8f6022aba8d6dbb093204
Levi Eggert <levi.eggert@cru.org>
2022-02-18 17:01:56 -0500

    Remove commented code

--- godtools/App/Services/Renderer/Views/MobileContent/Views/ContentText/MobileContentTextViewModel.swift
+++ godtools/App/Services/Renderer/Views/MobileContent/Views/ContentText/MobileContentTextViewModel.swift
@@ -164,16 +164,8 @@ class MobileContentTextViewModel: MobileContentTextViewModelType {
     
     private func getFontScale() -> CGFloat {
                 
-        let fontScale: CGFloat
-        
-        // TODO: Remove commented out code once XmlNodeRenderer is removed. ~Levi
-        //let manifestTextScale: Double = rendererPageModel.manifest.attributes.textScale.doubleValue
-        //let pageTextScale: Double = rendererPageModel.pageModel.textScale.doubleValue
-        //let textScale: Double = textModel.textScale.doubleValue
-        //fontScale = CGFloat(manifestTextScale * pageTextScale * textScale)
-        
-        fontScale = CGFloat(textModel.textScale.doubleValue)
-        
+        let fontScale = CGFloat(textModel.textScale.doubleValue)
+                        
         return fontScale
     }
     
