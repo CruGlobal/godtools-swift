cb613f3508cd346021385550c3d0cada89f491d1
Levi Eggert <levi.eggert@cru.org>
2022-02-21 15:15:50 -0500

    Remove TODO and extension

--- godtools/App/Services/Renderer/Parser/Protocol/Models/MobileContentRenderableModel.swift
+++ godtools/App/Services/Renderer/Parser/Protocol/Models/MobileContentRenderableModel.swift
@@ -8,19 +8,7 @@
 
 import Foundation
 
-// TODO: I believe this can be removed now that we're using shared Kotlin parser. ~Levi
 protocol MobileContentRenderableModel {
     
-    // Actually, this is still needed. ~Levi
     func getRenderableChildModels() -> [MobileContentRenderableModel]
-}
-
-extension MobileContentRenderableModel {
-    
-    // TODO: Should I override this here or force concrete types to provide this or can this be shared between xml nodes and multiplatform. ~Levi
-    // TODO: Let's remove this method here and force concrete types to provide this. ~Levi
-    
-    func getRenderableChildModels() -> [MobileContentRenderableModel] {
-        return Array()
-    }
 }
