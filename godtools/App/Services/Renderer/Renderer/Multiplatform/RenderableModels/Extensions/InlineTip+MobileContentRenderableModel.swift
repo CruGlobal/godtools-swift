f1fc1f493f456bb4a93cfb279942a6f1cf52a588
Levi Eggert <levi.eggert@cru.org>
2022-02-23 13:32:24 -0500

    Add InlineTip MobileContentRenderableModel extension

---
+++ godtools/App/Services/Renderer/Renderer/Multiplatform/RenderableModels/Extensions/InlineTip+MobileContentRenderableModel.swift
@@ -0,0 +1,24 @@
+//
+//  InlineTip+MobileContentRenderableModel.swift
+//  godtools
+//
+//  Created by Levi Eggert on 2/23/22.
+//  Copyright © 2022 Cru. All rights reserved.
+//
+
+import Foundation
+import GodToolsToolParser
+
+extension InlineTip: MobileContentRenderableModel {
+    
+    func getRenderableChildModels() -> [MobileContentRenderableModel] {
+        
+        var childModels: [MobileContentRenderableModel] = Array()
+        
+        if let tip = self.tip {
+            childModels.append(tip)
+        }
+        
+        return childModels
+    }
+}
