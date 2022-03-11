b3dea7a4263f7e575b4f649782258a08d098d1b2
Levi Eggert <levi.eggert@cru.org>
2022-03-11 14:48:34 -0500

    Create folders for different flows

---
+++ godtools/App/Flows/FlowStepType.swift
@@ -0,0 +1,13 @@
+//
+//  FlowStepType.swift
+//  godtools
+//
+//  Created by Levi Eggert on 3/11/22.
+//  Copyright © 2022 Cru. All rights reserved.
+//
+
+import Foundation
+
+protocol FlowStepType {
+    
+}
