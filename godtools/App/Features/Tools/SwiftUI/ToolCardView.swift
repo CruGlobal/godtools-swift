af562f3a0b6740d05d676cb553a981a2b3792caa
Rachael Skeath <rachaelblue@gmail.com>
2022-04-11 15:49:27 -0400

    dynamically size tool card based on aspect ratio

--- godtools/App/Features/Tools/SwiftUI/ToolCardView.swift
+++ godtools/App/Features/Tools/SwiftUI/ToolCardView.swift
@@ -13,26 +13,16 @@ struct ToolCardView: View {
     // MARK: - Properties
     
     @ObservedObject var viewModel: ToolCardViewModel
-    var isSpotlight: Bool = false
-    
-    private var cardHeight: CGFloat { isSpotlight ? Sizes.cardHeightSpotlight : Sizes.cardHeight }
-    private var cardWidth: CGFloat? { isSpotlight ? Sizes.cardWidthSpotlight : nil }
-    private var bannerImageHeight: CGFloat { isSpotlight ? Sizes.bannerImageHeightSpotlight : Sizes.bannerImageHeight }
-    private var titleWidth: CGFloat? {
-        if let cardWidth = cardWidth {
-            return cardWidth - (Sizes.leadingPadding * 2)
-        } else {
-            return nil
-        }
-    }
     
     // MARK: - Constants
     
     private enum Sizes {
+        static let cardAspectRatio: CGFloat = cardWidth/cardHeight
         static let cardHeight: CGFloat = 150
         static let cardHeightSpotlight: CGFloat = 240
         static let cardWidth: CGFloat = 335
         static let cardWidthSpotlight: CGFloat = 200
+        static let bannerImageAspectRatio: CGFloat = cardWidth/bannerImageHeight
         static let bannerImageHeight: CGFloat = 87
         static let bannerImageHeightSpotlight: CGFloat = 162
         static let leadingPadding: CGFloat = 15
@@ -45,14 +35,13 @@ struct ToolCardView: View {
         ZStack(alignment: .topLeading) {
             RoundedRectangle(cornerRadius: Sizes.cornerRadius, style: .circular)
                 .fill(.white)
-                .frame(width: cardWidth, height: cardHeight)
+                .aspectRatio(Sizes.cardAspectRatio, contentMode: .fit)
                 .shadow(color: .black.opacity(0.25), radius: 4, y: 2)
             
             VStack(alignment: .leading, spacing: 12) {
                 viewModel.bannerImage?
                     .resizable()
-                    .frame(width: cardWidth, height: bannerImageHeight)
-                    .clipped()
+                    .aspectRatio(Sizes.bannerImageAspectRatio, contentMode: .fit)
                     .cornerRadius(Sizes.cornerRadius, corners: [.topLeft, .topRight])
                 
                 VStack(alignment: .leading, spacing: 3) {
@@ -62,7 +51,6 @@ struct ToolCardView: View {
                     Text("Subtitle")
                         .font(.system(size: 12))
                 }
-                .frame(maxWidth: titleWidth, alignment: .leading)
                 .padding([.leading, .trailing], 15)
             }
             
@@ -77,7 +65,6 @@ struct ToolCardView_Previews: PreviewProvider {
     
     static var previews: some View {
         ToolCardView(viewModel: viewModel)
-            .previewLayout(.sizeThatFits)
             .padding()
     }
 }
