8433fd212055bb06c9d5bf7cc8f9757d57c808ed
levieggertcru <59846460+levieggertcru@users.noreply.github.com>
2022-04-21 19:14:53 -0400

    Change image constraints to bounds of superview

--- godtools/App/Services/Renderer/Views/MobileContent/Views/ContentImage/MobileContentImageView.swift
+++ godtools/App/Services/Renderer/Views/MobileContent/Views/ContentImage/MobileContentImageView.swift
@@ -13,8 +13,6 @@ class MobileContentImageView: MobileContentView {
     private let viewModel: MobileContentImageViewModelType
     
     private var imageView: UIImageView?
-    private var imageWidthConstraint: NSLayoutConstraint?
-    private var imageHeightConstraint: NSLayoutConstraint?
     private var emptyView: UIView?
     
     required init(viewModel: MobileContentImageViewModelType) {
@@ -29,12 +27,6 @@ class MobileContentImageView: MobileContentView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        layoutImageViewSizeIfNeeded()
     }
     
     private func setupLayout() {
@@ -62,7 +54,6 @@ class MobileContentImageView: MobileContentView {
         }
         
         let imageView: UIImageView = UIImageView()
-        let imageViewSize: CGSize = getImageViewSize(image: image, contentViewWidth: contentViewWidth)
         
         self.imageView = imageView
         
@@ -72,39 +63,13 @@ class MobileContentImageView: MobileContentView {
         
         addSubview(imageView)
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        
-        imageView.constrainTopToView(view: self)
-        imageView.constrainBottomToView(view: self)
-        imageView.constrainCenterHorizontallyInView(view: self)
-        imageWidthConstraint = imageView.addWidthConstraint(constant: imageViewSize.width)
-        imageHeightConstraint = imageView.addHeightConstraint(constant: imageViewSize.height)
-        
+        imageView.constrainEdgesToView(view: self)
+                
         // add image tap gesture
         let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer()
         tapGesture.addTarget(self, action: #selector(handleImageTapped))
         imageView.isUserInteractionEnabled = true
         imageView.addGestureRecognizer(tapGesture)
-    }
-    
-    private func layoutImageViewSizeIfNeeded() {
-        
-        guard let image = viewModel.image else {
-            return
-        }
-        
-        switch viewModel.imageWidth {
-        
-        case .percentageOfContainer( _):
-            
-            let imageViewSize: CGSize = getImageViewSize(image: image, contentViewWidth: viewModel.imageWidth)
-            imageWidthConstraint?.constant = imageViewSize.width
-            imageHeightConstraint?.constant = imageViewSize.height
-            
-            layoutIfNeeded()
-            
-        case .points( _):
-            break
-        }
     }
     
     private func getImageViewSize(image: UIImage, contentViewWidth: MobileContentViewWidth) -> CGSize {
