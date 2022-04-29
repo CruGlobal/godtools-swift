11f27a3999360f80a1e495180087ec5c979047ba
Rachael Skeath <rachaelblue@gmail.com>
2022-04-29 09:44:03 -0400

    create favoriting tool banner view

---
+++ godtools/App/Features/Tools/SwiftUI/FavoritingToolBannerView/FavoritingToolBannerView.swift
@@ -0,0 +1,38 @@
+//
+//  FavoritingToolBannerView.swift
+//  godtools
+//
+//  Created by Rachael Skeath on 4/20/22.
+//  Copyright © 2022 Cru. All rights reserved.
+//
+
+import SwiftUI
+
+struct FavoritingToolBannerView: View {
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            Image("nav_item_close")
+                .padding(.top, 14)
+                .padding(.trailing, 18)
+            
+            HStack {
+                Spacer()
+                
+                Text("Message")
+                    .font(.system(size: 18))
+                    .foregroundColor(ColorPalette.gtGrey.color)
+                .padding([.top, .bottom], 25)
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+struct FavoritingToolBannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoritingToolBannerView()
+            .frame(width: 400)
+            .previewLayout(.sizeThatFits)
+    }
+}
