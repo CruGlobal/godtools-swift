406f51f790e1c921b7147539e3a0cdae7a60e84f
levieggertcru <59846460+levieggertcru@users.noreply.github.com>
2022-05-06 20:34:25 -0400

    Adding protocol for ToolsMenuPageView and enum for ToolsMenuPageType

---
+++ godtools/App/Features/Tools/ToolsMenu/Models/ToolsMenuPageType.swift
@@ -0,0 +1,16 @@
+//
+//  ToolsMenuPageType.swift
+//  godtools
+//
+//  Created by Levi Eggert on 5/6/22.
+//  Copyright © 2022 Cru. All rights reserved.
+//
+
+import Foundation
+
+enum ToolsMenuPageType {
+    
+    case allTools
+    case favorites
+    case lessons
+}
---
+++ godtools/App/Features/Tools/ToolsMenu/ToolsMenuPageView.swift
@@ -0,0 +1,14 @@
+//
+//  ToolsMenuPageView.swift
+//  godtools
+//
+//  Created by Levi Eggert on 5/6/22.
+//  Copyright © 2022 Cru. All rights reserved.
+//
+
+import UIKit
+
+protocol ToolsMenuPageView: UIViewController {
+    
+    func pageViewed()
+}
