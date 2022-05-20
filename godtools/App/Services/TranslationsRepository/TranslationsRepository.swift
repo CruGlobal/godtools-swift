90e80ea34cad9fb3ed1fa9b80ae6463f262f2a1c
levieggertcru <59846460+levieggertcru@users.noreply.github.com>
2022-05-20 10:25:54 -0400

    Add TODO with JIRA task for TranslationsRepository

--- godtools/App/Services/TranslationsRepository/TranslationsRepository.swift
+++ godtools/App/Services/TranslationsRepository/TranslationsRepository.swift
@@ -10,4 +10,5 @@ import Foundation
 
 class TranslationsRepository {
         
+    // TODO: This will be done with GT-1549. The goal of this repository will be to provide tool translations from the FileCache and if none exist those translations will be downloaded and provided in a closure. ~Levi
 }
