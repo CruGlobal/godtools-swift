d2d95470dbbeb383ebb69782d6fa87d15f188e40
levieggertcru <59846460+levieggertcru@users.noreply.github.com>
2022-05-23 17:02:51 -0400

    Adding initial flow files for downloading tool translations flow

---
+++ godtools/App/Flows/DownloadToolTranslations/DownloadToolTranslationsFlow.swift
@@ -0,0 +1,24 @@
+//
+//  DownloadToolTranslationsFlow.swift
+//  godtools
+//
+//  Created by Levi Eggert on 5/23/22.
+//  Copyright © 2022 Cru. All rights reserved.
+//
+
+import UIKit
+
+class DownloadToolTranslationsFlow: Flow {
+    
+    let appDiContainer: AppDiContainer
+    let navigationController: UINavigationController = UINavigationController()
+    
+    required init(appDiContainer: AppDiContainer) {
+        
+        self.appDiContainer = appDiContainer
+    }
+    
+    func navigate(step: FlowStep) {
+        
+    }
+}
