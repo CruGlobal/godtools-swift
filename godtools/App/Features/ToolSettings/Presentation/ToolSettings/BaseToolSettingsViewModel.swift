5a0f9354722ae246374bc0d339c6ed3183859c3b
levieggertcru <59846460+levieggertcru@users.noreply.github.com>
2022-06-01 09:53:02 -0400

    no message

--- godtools/App/Features/ToolSettings/Presentation/ToolSettings/BaseToolSettingsViewModel.swift
+++ godtools/App/Features/ToolSettings/Presentation/ToolSettings/BaseToolSettingsViewModel.swift
@@ -5,11 +5,10 @@
 //  Created by Levi Eggert on 5/12/22.
 //  Copyright © 2022 Cru. All rights reserved.
 //
-
 import Foundation
 
 class BaseToolSettingsViewModel: ObservableObject {
-        
+    
     init() {}
     
     func getTopBarViewModel() -> BaseToolSettingsTopBarViewModel {
