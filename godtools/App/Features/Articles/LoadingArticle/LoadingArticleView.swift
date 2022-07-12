59996cfc06fc3c93eca4c29a1e870c763e247e97
levieggertcru <59846460+levieggertcru@users.noreply.github.com>
2022-07-12 11:16:32 -0400

    Did a little folder restructuring in the Features folder for the Menu, Languages, Articles, Tutorial

---
+++ godtools/App/Features/Articles/LoadingArticle/LoadingArticleView.swift
@@ -0,0 +1,14 @@
+//
+//  LoadingArticleView.swift
+//  godtools
+//
+//  Created by Levi Eggert on 7/12/22.
+//  Copyright © 2022 Cru. All rights reserved.
+//
+
+import UIKit
+
+class LoadingArticleView: LoadingView {
+    
+}
+
