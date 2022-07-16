35a58a343dad5376186d88eb8a6b09a196e60f4c
levieggertcru <59846460+levieggertcru@users.noreply.github.com>
2022-07-16 10:32:19 -0400

    Re-organize methods in FileCache class

--- godtools/App/Share/Common/FileCache/FileCache.swift
+++ godtools/App/Share/Common/FileCache/FileCache.swift
@@ -60,20 +60,6 @@ class FileCache<CacheLocation: FileCacheLocationType> {
             return .failure(error)
         }
     }
-
-    func getFile(location: CacheLocation) -> Result<URL, Error> {
-        
-        guard let fileUrl = location.fileUrl else {
-            return .failure(NSError(domain: errorDomain, code: -1, userInfo: [NSLocalizedDescriptionKey: "File url can't be null."]))
-        }
-        
-        switch getRootDirectory() {
-        case .success(let rootDirectory):
-            return .success(rootDirectory.appendingPathComponent(fileUrl.path))
-        case .failure(let error):
-            return .failure(error)
-        }
-    }
     
     private func createDirectoryIfNotExists(location: CacheLocation) -> Result<URL, Error> {
         
@@ -104,24 +90,26 @@ class FileCache<CacheLocation: FileCacheLocationType> {
         }
     }
     
-    func cache(location: CacheLocation, data: Data) -> Result<URL, Error> {
-        
-        switch createDirectoryIfNotExists(location: location) {
-        case .success( _):
-            break
-        case .failure(let error):
-            return .failure(error)
-        }
+    func getFileExists(location: CacheLocation) -> Result<Bool, Error> {
         
         switch getFile(location: location) {
         case .success(let url):
-            if fileManager.createFile(atPath: url.path, contents: data, attributes: nil) {
-                return .success(url)
-            }
-            else {
-                return .failure(NSError(domain: errorDomain, code: -1, userInfo: [NSLocalizedDescriptionKey: "Failed to create file at url: \(url.absoluteString)"]))
-            }
-        case.failure(let error):
+            return .success(fileManager.fileExists(atPath: url.path))
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+    
+    func getFile(location: CacheLocation) -> Result<URL, Error> {
+        
+        guard let fileUrl = location.fileUrl else {
+            return .failure(NSError(domain: errorDomain, code: -1, userInfo: [NSLocalizedDescriptionKey: "File url can't be null."]))
+        }
+        
+        switch getRootDirectory() {
+        case .success(let rootDirectory):
+            return .success(rootDirectory.appendingPathComponent(fileUrl.path))
+        case .failure(let error):
             return .failure(error)
         }
     }
@@ -162,12 +150,24 @@ class FileCache<CacheLocation: FileCacheLocationType> {
         }
     }
     
-    func fileExists(location: CacheLocation) -> Result<Bool, Error> {
+    func cache(location: CacheLocation, data: Data) -> Result<URL, Error> {
+        
+        switch createDirectoryIfNotExists(location: location) {
+        case .success( _):
+            break
+        case .failure(let error):
+            return .failure(error)
+        }
         
         switch getFile(location: location) {
         case .success(let url):
-            return .success(fileManager.fileExists(atPath: url.path))
-        case .failure(let error):
+            if fileManager.createFile(atPath: url.path, contents: data, attributes: nil) {
+                return .success(url)
+            }
+            else {
+                return .failure(NSError(domain: errorDomain, code: -1, userInfo: [NSLocalizedDescriptionKey: "Failed to create file at url: \(url.absoluteString)"]))
+            }
+        case.failure(let error):
             return .failure(error)
         }
     }
