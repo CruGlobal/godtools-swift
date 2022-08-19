34c9e3822e3ca78a300885a18b5a4ffbd7ef689d
Levi Eggert <levi.eggert@cru.org>
2022-08-19 11:26:52 -0400

    Prefix filenames with ResourceModel

---
+++ godtools/App/Share/Data/ResourcesRepository/Api/Models/ResourceModelDefaultVariantData.swift
@@ -0,0 +1,16 @@
+//
+//  ResourceModelDefaultVariantData.swift
+//  godtools
+//
+//  Created by Rachael Skeath on 7/1/22.
+//  Copyright © 2022 Cru. All rights reserved.
+//
+
+import Foundation
+
+struct ResourceModelDefaultVariantData: Codable {
+    
+    let id: String
+    let type: String
+    
+}
