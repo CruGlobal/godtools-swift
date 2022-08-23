93d00206dda9d57306c242e8556a8dd4159b0a39
Rachael Skeath <rachaelblue@gmail.com>
2022-08-23 10:31:59 -0400

    add GetSpotlightToolsUseCase

---
+++ godtools/App/Share/Domain/UseCases/GetSpotlightToolsUseCase/GetSpotlightToolsUseCase.swift
@@ -0,0 +1,13 @@
+//
+//  GetSpotlightToolsUseCase.swift
+//  godtools
+//
+//  Created by Rachael Skeath on 8/22/22.
+//  Copyright © 2022 Cru. All rights reserved.
+//
+
+import Foundation
+
+class GetSpotlightToolsUseCase {
+    
+}
