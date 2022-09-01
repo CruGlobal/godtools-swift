eaec30ff3a39cd5e62b7b611cb83d78c05897bd5
Levi Eggert <levi.eggert@cru.org>
2022-09-01 12:00:23 -0400

    Set parseRelated to true on parsing for manifestAndRelatedFiles Set parsePages and parseTips to false when parsing for manifest or manifestAndRelatedFiles

--- godtools/App/Share/Data/TranslationsRepository/ManifestParser/TranslationManifestParser.swift
+++ godtools/App/Share/Data/TranslationsRepository/ManifestParser/TranslationManifestParser.swift
@@ -20,11 +20,11 @@ class TranslationManifestParser {
         switch type {
         
         case .manifestAndRelatedFiles:
-            let parserConfig = ParserConfig().withParseRelated(enabled: false)
+            let parserConfig = ParserConfig().withParseRelated(enabled: true).withParsePages(enabled: false).withParseTips(enabled: false)
             return TranslationManifestParser(parserConfig: parserConfig, resourcesFileCache: resourcesFileCache)
         
         case .manifestOnly:
-            let parserConfig = ParserConfig().withParseRelated(enabled: false)
+            let parserConfig = ParserConfig().withParseRelated(enabled: false).withParsePages(enabled: false).withParseTips(enabled: false)
             return TranslationManifestParser(parserConfig: parserConfig, resourcesFileCache: resourcesFileCache)
         
         case .renderer:
