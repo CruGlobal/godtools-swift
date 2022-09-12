6b50eb0e7c4dac274ff4c4162f258f36f8c8c118
Rachael Skeath <rachaelblue@gmail.com>
2022-09-12 15:37:10 -0400

    implement getLessonsUseCase

--- godtools/App/Share/Domain/UseCases/GetLessonsUseCase/GetLessonsUseCase.swift
+++ godtools/App/Share/Domain/UseCases/GetLessonsUseCase/GetLessonsUseCase.swift
@@ -7,13 +7,35 @@
 //
 
 import Foundation
+import Combine
 
 class GetLessonsUseCase {
     
+    private let getLessonUseCase: GetLessonUseCase
     private let resourcesRepository: ResourcesRepository
     
-    init(resourcesRepository: ResourcesRepository) {
+    init(getLessonUseCase: GetLessonUseCase, resourcesRepository: ResourcesRepository) {
+        self.getLessonUseCase = getLessonUseCase
         self.resourcesRepository = resourcesRepository
+    }
+    
+    func getLessonsPublisher() -> AnyPublisher<[LessonDomainModel], Never> {
+        
+        return resourcesRepository.getResourcesChanged()
+            .flatMap { _ -> AnyPublisher<[LessonDomainModel], Never> in
+                
+                let lessons = self.getLessons()
+                
+                return Just(lessons)
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    private func getLessons() -> [LessonDomainModel] {
+        
+        return resourcesRepository.getAllLessons()
+            .map { getLessonUseCase.getLesson(resource: $0) }
     }
     
 }
