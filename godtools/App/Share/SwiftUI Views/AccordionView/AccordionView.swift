b96a95571144e2b0a5d72a909467fc06ac91549e
Rachael Skeath <rachaelblue@gmail.com>
2022-09-27 16:18:13 -0400

    add accordionViewModel

--- godtools/App/Share/SwiftUI Views/AccordionView/AccordionView.swift
+++ godtools/App/Share/SwiftUI Views/AccordionView/AccordionView.swift
@@ -9,23 +9,41 @@
 import SwiftUI
 
 struct AccordionView: View {
+    
+    // MARK: - Properties
+    
+    @ObservedObject var viewModel: AccordionViewModel
+    
     var body: some View {
+        
         VStack(alignment: .leading, spacing: 15) {
             
             SeparatorView()
             
-            Text("Conversation Starters")
-                .font(FontLibrary.sfProTextBold.font(size: 14))
-                .foregroundColor(ColorPalette.gtGrey.color)
+            HStack {
+                Text("Conversation Starters")
+                    .font(FontLibrary.sfProTextBold.font(size: 14))
+                    .foregroundColor(ColorPalette.gtGrey.color)
+                
+                Spacer()
+                
+                Image(viewModel.isExpanded ? "accordion_section_minus" : "accordion_section_plus")
+                    .padding(.trailing, 15)
+                
+            }
             
             SeparatorView()
         }
+        .onTapGesture {
+            viewModel.isExpanded.toggle()
+        }
+        .animation(.default, value: viewModel.isExpanded)
     }
 }
 
 struct AccordionView_Previews: PreviewProvider {
     static var previews: some View {
-        AccordionView()
+        AccordionView(viewModel: AccordionViewModel())
             .padding()
     }
 }
---
+++ godtools/App/Share/SwiftUI Views/AccordionView/AccordionViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  AccordionViewModel.swift
+//  godtools
+//
+//  Created by Rachael Skeath on 9/27/22.
+//  Copyright © 2022 Cru. All rights reserved.
+//
+
+import Foundation
+
+class AccordionViewModel: ObservableObject {
+    
+    @Published var isExpanded: Bool = false
+    
+}
