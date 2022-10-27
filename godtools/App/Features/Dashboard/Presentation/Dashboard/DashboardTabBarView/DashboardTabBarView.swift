a33ee568d96fe96a01fe8c5f444731168a16b3f3
Rachael Skeath <rachaelblue@gmail.com>
2022-10-27 11:48:10 -0400

    build out dashboardTabBarView

--- godtools/App/Features/Dashboard/Presentation/Dashboard/DashboardTabBarView/DashboardTabBarView.swift
+++ godtools/App/Features/Dashboard/Presentation/Dashboard/DashboardTabBarView/DashboardTabBarView.swift
@@ -1,61 +1,55 @@
 //
-//  DashboardView.swift
+//  DashboardTabBarView.swift
 //  godtools
 //
-//  Created by Rachael Skeath on 10/6/22.
+//  Created by Rachael Skeath on 10/27/22.
 //  Copyright © 2022 Cru. All rights reserved.
 //
 
 import SwiftUI
 
-struct DashboardView: View {
+struct DashboardTabBarView: View {
     
     @ObservedObject var viewModel: DashboardViewModel
     
-    private static let marginMultiplier: CGFloat = 15/375
-    
-    init(viewModel: DashboardViewModel) {
-        self.viewModel = viewModel
-        
-        // TODO: - find a better way to hide the default tab bar
-        UITabBar.appearance().isHidden = true
-    }
-    
     var body: some View {
-        GeometryReader { geo in
+        ZStack {
             
-            let leadingTrailingPadding = DashboardView.getMargin(for: geo.size.width)
-            
-            ZStack(alignment: .bottom) {
+            HStack {
                 
-                TabView(selection: $viewModel.selectedTab) {
-                    
-                    LessonsView(viewModel: viewModel.lessonsViewModel, leadingTrailingPadding: leadingTrailingPadding)
-                        .tag(DashboardTabTypeDomainModel.lessons)
-                    
-                    
-                    FavoritesContentView(viewModel: viewModel.favoritesViewModel, leadingTrailingPadding: leadingTrailingPadding)
-                        .tag(DashboardTabTypeDomainModel.favorites)
-                    
-                    AllToolsContentView(viewModel: viewModel.allToolsViewModel, leadingTrailingPadding: leadingTrailingPadding)
-                        .tag(DashboardTabTypeDomainModel.allTools)
-                }
+                Spacer()
                 
-                DashboardTabBarView(viewModel: viewModel)
+                DashboardTabItem(title: "Lessons", imageName: ImageCatalog.toolsMenuLessons.name, isSelected: viewModel.selectedTab == .lessons)
+                    .onTapGesture {
+                        viewModel.selectedTab = DashboardTabTypeDomainModel.lessons
+                    }
+                
+                Spacer()
+                Spacer()
+                
+                DashboardTabItem(title: "Favorites", imageName: ImageCatalog.toolsMenuFavorites.name, isSelected: viewModel.selectedTab == .favorites)
+                    .onTapGesture {
+                        viewModel.selectedTab = DashboardTabTypeDomainModel.favorites
+                    }
+                
+                Spacer()
+                Spacer()
+                
+                DashboardTabItem(title: "All Tools", imageName: ImageCatalog.toolsMenuAllTools.name, isSelected: viewModel.selectedTab == .allTools)
+                    .onTapGesture {
+                        viewModel.selectedTab = DashboardTabTypeDomainModel.allTools
+                    }
+                
+                Spacer()
             }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color.white.edgesIgnoringSafeArea(.bottom))
         }
-    }
-    
-    static func getMargin(for width: CGFloat) -> CGFloat {
-        return marginMultiplier * width
-    }
-    
-    func navigateToTab(_ tab: DashboardTabTypeDomainModel) {
-        viewModel.selectedTab = tab
     }
 }
 
-struct DashboardView_Previews: PreviewProvider {
+struct DashboardTabBarView_Previews: PreviewProvider {
     static var previews: some View {
         let appDiContainer: AppDiContainer = SwiftUIPreviewDiContainer().getAppDiContainer()
         
@@ -85,6 +79,6 @@ struct DashboardView_Previews: PreviewProvider {
             toggleToolFavoritedUseCase: appDiContainer.domainLayer.getToggleToolFavoritedUseCase()
         )
         
-        DashboardView(viewModel: viewModel)
+        DashboardTabBarView(viewModel: viewModel)
     }
 }
