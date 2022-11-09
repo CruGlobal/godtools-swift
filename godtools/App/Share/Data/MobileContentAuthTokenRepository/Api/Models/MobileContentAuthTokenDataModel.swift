fb453ca028ae70dc84099a5aa86a597267af9fb3
Rachael Skeath <rachaelblue@gmail.com>
2022-11-09 16:59:30 -0500

    parse expirationDate as Date?

--- godtools/App/Share/Data/MobileContentAuthTokenRepository/Api/Models/MobileContentAuthTokenDataModel.swift
+++ godtools/App/Share/Data/MobileContentAuthTokenRepository/Api/Models/MobileContentAuthTokenDataModel.swift
@@ -11,7 +11,7 @@ import Foundation
 struct MobileContentAuthTokenDataModel: Decodable {
     
     let token: String
-    let expirationDate: String
+    let expirationDate: Date?
     let userId: Int
     
     enum RootKeys: String, CodingKey {
@@ -36,7 +36,19 @@ struct MobileContentAuthTokenDataModel: Decodable {
         let attributesContainer = try dataContainer.nestedContainer(keyedBy: AttributesKeys.self, forKey: .attributes)
         
         token = try attributesContainer.decode(String.self, forKey: .token)
-        expirationDate = try attributesContainer.decodeIfPresent(String.self, forKey: .expiration) ?? ""
         userId = try attributesContainer.decode(Int.self, forKey: .userId)
+        
+        let expirationDateString = try attributesContainer.decodeIfPresent(String.self, forKey: .expiration) ?? ""
+        expirationDate = MobileContentAuthTokenDataModel.parseExpirationDateFromString(expirationDateString)
+    }
+    
+    private static func parseExpirationDateFromString(_ dateString: String) -> Date? {
+        let dateFormatter = ISO8601DateFormatter()
+        dateFormatter.formatOptions = [
+            .withInternetDateTime,
+            .withFractionalSeconds
+        ]
+        
+        return dateFormatter.date(from: dateString)
     }
 }
