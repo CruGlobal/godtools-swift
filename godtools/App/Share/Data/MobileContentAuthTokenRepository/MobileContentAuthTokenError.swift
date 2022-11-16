8f95262a223f19915dee93e6aa907807931fd54d
Rachael Skeath <rachaelblue@gmail.com>
2022-11-16 12:54:42 -0500

    move token errors into separate file
    (cherry picked from commit 4cc393a3f088e27bc10d08890a40d55f6baaab7e)

---
+++ godtools/App/Share/Data/MobileContentAuthTokenRepository/MobileContentAuthTokenError.swift
@@ -0,0 +1,14 @@
+//
+//  MobileContentAuthTokenError.swift
+//  godtools
+//
+//  Created by Rachael Skeath on 11/16/22.
+//  Copyright © 2022 Cru. All rights reserved.
+//
+
+import Foundation
+
+enum MobileContentAuthTokenError: Error {
+    case nilOktaToken
+    case nilAuthToken
+}
