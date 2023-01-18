922b2526beadd429618e051e03d02f27814c9029
Levi Eggert <levi.eggert@cru.org>
2023-01-18 16:12:06 -0500

    Fixed bug by replacing List with a vertical ScrollView. Also removed iOS 13 code since upgrading to minimum iOS 14.

--- godtools/App/Share/SwiftUI Views/BackwardCompatibleList.swift
+++ godtools/App/Share/SwiftUI Views/BackwardCompatibleList.swift
@@ -16,17 +16,6 @@ struct BackwardCompatibleList<Content: View>: View {
     init<T: View>(rootViewType: T.Type, @ViewBuilder content: @escaping () -> Content, refreshHandler: @escaping () -> Void) {
         self.content = content
         self.refreshHandler = refreshHandler
-        
-        /*
-         About removing the List separators:
-         - iOS 15 - use the `listRowSeparator` view modifier to hide the separators
-         - iOS 13 - list is built on UITableView, so `UITableView.appearance` works to set the separator style
-         - iOS 14 - `appearance` no longer works, and the modifier doesn't yet exist.  Solution is the AllToolsListIOS14 view.
-         */
-        if #available(iOS 14.0, *) {} else {
-            // TODO: - When we stop supporting iOS 13, get rid of this.
-            UITableView.appearance(whenContainedInInstancesOf: [UIHostingController<T>.self]).separatorStyle = .none
-        }
     }
     
     var body: some View {
@@ -34,32 +23,21 @@ struct BackwardCompatibleList<Content: View>: View {
         if #available(iOS 15.0, *) {
             // Pull to refresh is supported only in iOS 15+
             
-            List {
+            ScrollView(.vertical) {
                 content()
-                    .listRowSeparator(.hidden)
-                    
             }
-            .modifier(PlainList())
             .refreshable {
                 refreshHandler()
             }
             
-        } else if #available(iOS 14.0, *) {
+        } else {
             
-            ScrollView {
+            ScrollView(.vertical) {
                 LazyVStack(spacing: 0) {
                     content()
                 }
             }
-                                    
-        } else {
-            
-            List {
-                content()
-            }
-            .modifier(PlainList())
         }
-        
     }
 }
 
