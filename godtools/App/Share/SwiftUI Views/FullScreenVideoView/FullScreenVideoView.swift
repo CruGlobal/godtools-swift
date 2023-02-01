807d3a0ffbcf813ae22efd76c2fc650668ebd53f
Levi Eggert <levi.eggert@cru.org>
2023-02-01 09:13:55 -0500

    Add SwiftUI FullScreenVideoView for playing full screen video.  Wraps VideoView UIViewRepresentable.

---
+++ godtools/App/Share/SwiftUI Views/FullScreenVideoView/FullScreenVideoView.swift
@@ -0,0 +1,47 @@
+//
+//  FullScreenVideoView.swift
+//  godtools
+//
+//  Created by Levi Eggert on 1/31/23.
+//  Copyright © 2023 Cru. All rights reserved.
+//
+
+import SwiftUI
+
+struct FullScreenVideoView: View {
+    
+    @State private var videoPlayerState: VideoViewPlayerState = .stopped
+    
+    @ObservedObject var viewModel: FullScreenVideoViewModel
+    
+    var body: some View {
+        
+        GeometryReader { geometry in
+            
+            VStack(alignment: .center, spacing: 0) {
+                
+                Spacer()
+
+                let aspectRatio: CGSize = CGSize(width: 16, height: 9)
+                let videoWidth: CGFloat = geometry.size.width
+                let videoHeight: CGFloat = (videoWidth / aspectRatio.width) * aspectRatio.height
+                
+                VideoView(playerState: $videoPlayerState, frameSize: CGSize(width: videoWidth, height: videoHeight), videoId: viewModel.videoId, videoPlayerParameters: viewModel.videoPlayerParameters, configuration: VideoViewConfiguration(videoContainerBackgroundColor: .black, videoBackgroundColor: .black, loadingViewBackgroundColor: .black, loadingActivityIndicatorColor: .white), videoEndedClosure: {
+                    viewModel.videoEnded()
+                })
+                .frame(width: videoWidth, height: videoHeight)
+                
+                
+                Spacer()
+            }
+        }
+        .onAppear {
+            videoPlayerState = .playing
+        }
+        .onDisappear {
+            videoPlayerState = .paused
+        }
+        .statusBarHidden(true)
+        .background(Color.black)
+    }
+}
---
+++ godtools/App/Share/SwiftUI Views/FullScreenVideoView/FullScreenVideoViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  FullScreenVideoViewModel.swift
+//  godtools
+//
+//  Created by Levi Eggert on 2/1/23.
+//  Copyright © 2023 Cru. All rights reserved.
+//
+
+import Foundation
+
+class FullScreenVideoViewModel: ObservableObject {
+    
+    private let userDidCloseVideoStep: FlowStep
+    private let videoEndedStep: FlowStep
+    
+    private weak var flowDelegate: FlowDelegate?
+    
+    let videoId: String
+    let videoPlayerParameters: [String: Any]?
+    
+    init(flowDelegate: FlowDelegate, videoId: String, videoPlayerParameters: [String: Any]?, userDidCloseVideoStep: FlowStep, videoEndedStep: FlowStep) {
+        
+        self.flowDelegate = flowDelegate
+        self.userDidCloseVideoStep = userDidCloseVideoStep
+        self.videoEndedStep = videoEndedStep
+        self.videoId = videoId
+        self.videoPlayerParameters = FullScreenVideoViewModel.buildVideoPlayerParameters(fromParameters: videoPlayerParameters)
+    }
+    
+    private static func buildVideoPlayerParameters(fromParameters: [String: Any]?) -> [String: Any] {
+        
+        var videoPlayerParameters: [String: Any] = fromParameters ?? Dictionary()
+        
+        let playsInlineKey: String = Strings.YoutubePlayerParameters.playsInline.rawValue
+        let disablesFullScreen: Int = 1
+        
+        if videoPlayerParameters[playsInlineKey] == nil {
+            videoPlayerParameters[playsInlineKey] = disablesFullScreen
+        }
+        
+        return videoPlayerParameters
+    }
+}
+
+// MARK: - Inputs
+
+extension FullScreenVideoViewModel {
+    
+    @objc func closeTapped() {
+        flowDelegate?.navigate(step: userDidCloseVideoStep)
+    }
+    
+    func videoEnded() {
+        flowDelegate?.navigate(step: videoEndedStep)
+    }
+}
