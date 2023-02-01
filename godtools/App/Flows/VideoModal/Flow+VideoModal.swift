c5ca9a028f3a7a82a01cc58291f1d87d4bc503af
Levi Eggert <levi.eggert@cru.org>
2023-02-01 09:14:39 -0500

    Add Flow extension for presenting a FullScreenVideoView and update OnboardingFlow to present new SwiftUI FullScreenVideoView instead of the UIKit VideoPlayerView.

---
+++ godtools/App/Flows/VideoModal/Flow+VideoModal.swift
@@ -0,0 +1,37 @@
+//
+//  Flow+VideoModal.swift
+//  godtools
+//
+//  Created by Levi Eggert on 1/31/23.
+//  Copyright © 2023 Cru. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+extension Flow {
+    
+    func presentVideoModal(viewModel: FullScreenVideoViewModel) {
+        
+        let view = FullScreenVideoView(viewModel: viewModel)
+        
+        let hostingView = UIHostingController(rootView: view)
+        
+        _ = hostingView.addBarButtonItem(
+            to: .right,
+            image: ImageCatalog.navClose.uiImage,
+            color: .white,
+            target: viewModel,
+            action: #selector(viewModel.closeTapped)
+        )
+        
+        let modal = ModalNavigationController(rootView: hostingView, navBarColor: .black, navBarIsTranslucent: true)
+        
+        navigationController.present(modal, animated: true)
+    }
+    
+    func dismissVideoModal(animated: Bool, completion: (() -> Void)?) {
+        
+        navigationController.dismissPresented(animated: animated, completion: completion)
+    }
+}
