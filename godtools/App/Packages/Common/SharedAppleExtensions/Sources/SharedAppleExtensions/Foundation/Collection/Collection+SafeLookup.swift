2230473ce720839a8fd8211bb80af655b1fbad94
Levi Eggert <levi.eggert@cru.org>
2023-03-21 16:12:18 -0400

    Add extensions to SharedAppleExtensions swift package

---
+++ godtools/App/Packages/Common/SharedAppleExtensions/Sources/SharedAppleExtensions/Foundation/Collection/Collection+SafeLookup.swift
@@ -0,0 +1,16 @@
+//
+//  Collection+SafeLookup.swift
+//  SharedAppleExtensions
+//
+//  Created by Robert Eldredge on 2/18/22.
+//  Copyright © 2022 Cru. All rights reserved.
+//
+
+import Foundation
+
+public extension Collection where Indices.Iterator.Element == Index {
+    
+    subscript (safe index: Index) -> Iterator.Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
