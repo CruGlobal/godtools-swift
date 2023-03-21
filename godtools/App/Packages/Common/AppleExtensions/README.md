b31885f7b70d8ffe503a6e19f0e2241696ce25b6
Levi Eggert <levi.eggert@cru.org>
2023-03-21 16:11:58 -0400

    Add SharedAppleExtensions swift package in place of AppleExtensions

--- godtools/App/Packages/Common/AppleExtensions/README.md
+++
@@ -1,3 +0,0 @@
-# AppleExtensions
-
-This Swift Package will contain any extensions based from an Apple Framework (UIKit, SwiftUI, Foundation, etc.).
--- godtools/App/Packages/Common/SharedAppleExtensions/Package.swift
+++ godtools/App/Packages/Common/SharedAppleExtensions/Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "AppleExtensions",
+    name: "SharedAppleExtensions",
     platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AppleExtensions",
-            targets: ["AppleExtensions"]),
+            name: "SharedAppleExtensions",
+            targets: ["SharedAppleExtensions"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,10 +20,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "AppleExtensions",
+            name: "SharedAppleExtensions",
             dependencies: []),
         .testTarget(
-            name: "AppleExtensionsTests",
-            dependencies: ["AppleExtensions"]),
+            name: "SharedAppleExtensionsTests",
+            dependencies: ["SharedAppleExtensions"]),
     ]
 )
