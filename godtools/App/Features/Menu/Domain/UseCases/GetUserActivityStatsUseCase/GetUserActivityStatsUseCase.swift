dd752cde45768af8e2762f05e9ead3fbd4cd853e
Rachael Skeath <rachaelblue@gmail.com>
2023-03-27 15:06:23 -0400

    put image names in ImageCatalog

--- godtools/App/Features/Menu/Domain/UseCases/GetUserActivityStatsUseCase/GetUserActivityStatsUseCase.swift
+++ godtools/App/Features/Menu/Domain/UseCases/GetUserActivityStatsUseCase/GetUserActivityStatsUseCase.swift
@@ -22,42 +22,42 @@ class GetUserActivityStatsUseCase {
     func getUserActivityStats(from userActivity: UserActivity) -> [UserActivityStatDomainModel] {
         
         let toolOpensStat = UserActivityStatDomainModel(
-            iconImageName: "tool-opens",
+            iconImageName: ImageCatalog.userActivityToolOpens.name,
             text: localizationServices.stringForMainBundle(key: "account.activity.toolOpens"),
             textColor: Color(red: 5 / 255, green: 105 / 255, blue: 155 / 255),
             value: String(userActivity.toolOpens)
         )
         
         let lessonCompletionsStat = UserActivityStatDomainModel(
-            iconImageName: "lesson-completions",
+            iconImageName: ImageCatalog.userActivityLessonCompletions.name,
             text: localizationServices.stringForMainBundle(key: "account.activity.lessonCompletions"),
             textColor: Color(red: 55 / 255, green: 167 / 255, blue: 160 / 255),
             value: String(userActivity.lessonCompletions)
         )
         
         let screenSharesStat = UserActivityStatDomainModel(
-            iconImageName: "screen-shares",
+            iconImageName: ImageCatalog.userActivityScreenShares.name,
             text: localizationServices.stringForMainBundle(key: "account.activity.screenShares"),
             textColor: Color(red: 229 / 255, green: 91 / 255, blue: 54 / 255),
             value: String(userActivity.screenShares)
         )
         
         let linksSharedStat = UserActivityStatDomainModel(
-            iconImageName: "links-shared",
+            iconImageName: ImageCatalog.userActivityLinksShared.name,
             text: localizationServices.stringForMainBundle(key: "account.activity.linksShared"),
             textColor: Color(red: 47 / 255, green: 54 / 255, blue: 118 / 255),
             value: String(userActivity.linksShared)
         )
         
         let languagesUsedStat = UserActivityStatDomainModel(
-            iconImageName: "languages-used",
+            iconImageName: ImageCatalog.userActivityLanguagesUsed.name,
             text: localizationServices.stringForMainBundle(key: "account.activity.languagesUsed"),
             textColor: Color(red: 110 / 255, green: 220 / 255, blue: 80 / 255),
             value: String(userActivity.languagesUsed)
         )
         
         let sessionsStat = UserActivityStatDomainModel(
-            iconImageName: "sessions",
+            iconImageName: ImageCatalog.userActivitySessions.name,
             text: localizationServices.stringForMainBundle(key: "account.activity.sessions"),
             textColor: Color(red: 224 / 255, green: 206 / 255, blue: 38 / 255),
             value: String(userActivity.sessions)
