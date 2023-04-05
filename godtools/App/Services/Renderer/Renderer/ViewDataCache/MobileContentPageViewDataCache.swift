2ee6f6f405e34a6aafb4617638a10f6ec89f274b
Levi Eggert <levi.eggert@cru.org>
2023-04-05 13:35:44 -0400

    Adding classes for cacheing data to a dictionary keyed by the page id

---
+++ godtools/App/Services/Renderer/Renderer/ViewDataCache/MobileContentPageViewDataCache.swift
@@ -0,0 +1,36 @@
+//
+//  MobileContentPageViewDataCache.swift
+//  godtools
+//
+//  Created by Levi Eggert on 4/4/23.
+//  Copyright © 2023 Cru. All rights reserved.
+//
+
+import Foundation
+import GodToolsToolParser
+
+class MobileContentPageViewDataCache {
+    
+    private var dataCache: [String: Any] = Dictionary()
+    
+    let pageId: String
+    let pagePosition: Int32
+    
+    init(page: Page) {
+        
+        pageId = page.id
+        pagePosition = page.position
+    }
+    
+    var isEmpty: Bool {
+        return dataCache.isEmpty
+    }
+    
+    func getValue(key: String) -> Any? {
+        return dataCache[key]
+    }
+    
+    func storeValue(key: String, value: Any) {
+        dataCache[key] = value
+    }
+}
