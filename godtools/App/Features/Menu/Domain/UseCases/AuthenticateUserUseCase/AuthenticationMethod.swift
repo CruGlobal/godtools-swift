7fed8a2e178995ce97db09f7ad8a06ce4994bac2
Levi Eggert <levi.eggert@cru.org>
2023-05-01 11:43:29 -0400

    Add authentication method for each supported authentication type

---
+++ godtools/App/Features/Menu/Domain/UseCases/AuthenticateUserUseCase/AuthenticationMethod.swift
@@ -0,0 +1,16 @@
+//
+//  AuthenticationMethod.swift
+//  godtools
+//
+//  Created by Levi Eggert on 5/1/23.
+//  Copyright © 2023 Cru. All rights reserved.
+//
+
+import Foundation
+
+enum AuthenticationMethod: String {
+    
+    case apple = "apple"
+    case facebook = "facebook"
+    case google = "google"
+}
