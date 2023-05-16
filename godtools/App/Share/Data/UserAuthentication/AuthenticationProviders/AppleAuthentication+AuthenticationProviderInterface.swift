960d2ff2e3ff96687a835ae3203187141f40f14e
Rachael Skeath <rachaelblue@gmail.com>
2023-05-16 14:40:06 -0400

    add AppleAuthentication+AuthenticationProviderInterface

---
+++ godtools/App/Share/Data/UserAuthentication/AuthenticationProviders/AppleAuthentication+AuthenticationProviderInterface.swift
@@ -0,0 +1,55 @@
+//
+//  AppleAuthentication+AuthenticationProviderInterface.swift
+//  godtools
+//
+//  Created by Rachael Skeath on 5/16/23.
+//  Copyright © 2023 Cru. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SocialAuthentication
+import Combine
+
+extension AppleAuthentication: AuthenticationProviderInterface {
+    func getPersistedAccessToken() -> AuthenticationProviderAccessToken? {
+        
+        // TODO: - access token renewal will come through MobileContentAPI?
+        
+        return nil
+    }
+    
+    func authenticatePublisher(presentingViewController: UIViewController) -> AnyPublisher<AuthenticationProviderAccessToken?, Error> {
+        
+        return authenticatePublisher()
+            .map { (response: AppleAuthenticationResponse) in
+                
+                guard let idToken = response.identityToken else {
+                    return nil
+                }
+                
+                // TODO: - include name once AppleAuth is updated
+                return AuthenticationProviderAccessToken.apple(
+                    idToken: idToken,
+                    givenName: "first name",
+                    familyName: "last name"
+                )
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func renewAccessTokenPublisher() -> AnyPublisher<AuthenticationProviderAccessToken, Error> {
+        
+        // TODO: - access token renewal will come through MobileContentAPI?
+        
+        return Just(AuthenticationProviderAccessToken.apple(idToken: "", givenName: "", familyName: ""))
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+    
+    func signOutPublisher() -> AnyPublisher<Void, Error> {
+        
+        return Just(()).setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
+}
