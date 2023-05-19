eecb9490df63389e278f091cbc9b9e02e5db44aa
Levi Eggert <levi.eggert@cru.org>
2023-05-19 11:36:29 -0400

    Add getAuthUserPublisher method for AuthenticationProviderInterface Add TODO for returning AuthUserDomainModel

--- godtools/App/Share/Data/UserAuthentication/AuthenticationProviders/AppleAuthentication+AuthenticationProviderInterface.swift
+++ godtools/App/Share/Data/UserAuthentication/AuthenticationProviders/AppleAuthentication+AuthenticationProviderInterface.swift
@@ -52,4 +52,12 @@ extension AppleAuthentication: AuthenticationProviderInterface {
         return Just(()).setFailureType(to: Error.self)
             .eraseToAnyPublisher()
     }
+    
+    func getAuthUserPublisher() -> AnyPublisher<AuthUserDomainModel?, Error> {
+        
+        // TODO: Should return an AuthUserDomainModel if user info can be obtained from AppleAuthentication. Implement in GT-2027. ~Levi
+        
+        return Just(nil).setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
+    }
 }
