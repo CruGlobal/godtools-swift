251f6f78442382b6808bc1ef1750da11b5b5ae24
Levi Eggert <levi.eggert@cru.org>
2023-05-22 10:57:15 -0400

    remove .sh

--- .github/workflows/xcode_build_test.sh
+++
@@ -1,1 +0,0 @@
-xcodebuild build test -workspace godtools.xcworkspace -scheme godtools -destination 'platform=iOS Simulator,name=iPhone 14' -resultBundlePath /Users/runner/Library/Developer/Xcode/godtools/ResultBundle
