fd759a1d1a3dbc0259d6793a67955746786777ee
Levi Eggert <levi.eggert@cru.org>
2023-08-28 10:13:40 -0400

    Add AppBuildConfiguration enum

---
+++ godtools/App/AppBuild/AppBuildConfiguration.swift
@@ -0,0 +1,18 @@
+//
+//  AppBuildConfiguration.swift
+//  godtools
+//
+//  Created by Levi Eggert on 8/28/23.
+//  Copyright © 2023 Cru. All rights reserved.
+//
+
+import Foundation
+
+enum AppBuildConfiguration {
+    
+    case analyticsLogging
+    case staging
+    case production
+    case release
+    case swiftUIPreview
+}
