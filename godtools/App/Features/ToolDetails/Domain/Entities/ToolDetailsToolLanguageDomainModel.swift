6e77eaa23b2dedfbbaa6ccc8796ed851e0e1edb1
Levi Eggert <levi.eggert@cru.org>
2023-10-11 09:30:54 -0400

    Adding domain model for ToolDetails tool language in languages available list

---
+++ godtools/App/Features/ToolDetails/Domain/Entities/ToolDetailsToolLanguageDomainModel.swift
@@ -0,0 +1,14 @@
+//
+//  ToolDetailsToolLanguageDomainModel.swift
+//  godtools
+//
+//  Created by Levi Eggert on 10/10/23.
+//  Copyright © 2023 Cru. All rights reserved.
+//
+
+import Foundation
+
+struct ToolDetailsToolLanguageDomainModel {
+    
+    let languageNameTranslatedInToolLanguage: String
+}
