29a7e2108b36a72aea8fe95327cbd9de5291af2f
Rachael Skeath <rachaelblue@gmail.com>
2023-10-16 16:21:22 -0400

    fix spacing

--- godtools/App/Features/AppLanguage/Presentation/ConfirmAppLanguage/ConfirmAppLanguageView.swift
+++ godtools/App/Features/AppLanguage/Presentation/ConfirmAppLanguage/ConfirmAppLanguageView.swift
@@ -11,34 +11,61 @@ import SwiftUI
 struct ConfirmAppLanguageView: View {
     var body: some View {
         
-        VStack(spacing: 16) {
+        let horizontalPadding: CGFloat = 20
+        
+        GeometryReader { geometry in
             
-            ImageCatalog.languageSettingsLogo.image
-                .resizable()
-                .scaledToFill()
-                .frame(width: 54, height: 54)
-            
-            VStack(spacing: 30) {
+            VStack(alignment: .leading, spacing: 16) {
                 
-                Text("You have selected English as your app language. This will change the language of the whole GodTools app. Are you sure?")
-                    .font(FontLibrary.sfProTextRegular.font(size: 18))
-                    .foregroundColor(ColorPalette.gtGrey.color)
+                Spacer()
                 
-                Text("Ha seleccinado Ingles como idioma de su aplicacion. Esto cambiara toda la aplicacion GodTools. Esta seguro?")
-                    .font(FontLibrary.sfProTextRegular.font(size: 14))
-                    .foregroundColor(ColorPalette.gtGrey.color)
-            }
-//            .padding(.horizontal, 30)
-            
-            HStack {
-                GTBlueButton(title: "Change language", fontSize: 15, width: 145, height: 48) {
+                HStack {
+                    Spacer()
+                    
+                    ImageCatalog.languageSettingsLogo.image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 54, height: 54)
+                    
+                    Spacer()
+                }
+                
+                FixedVerticalSpacer(height: 10)
+                
+                Group {
+                    Text("You have selected English as your app language. This will change the language of the whole GodTools app. Are you sure?")
+                        .font(FontLibrary.sfProTextRegular.font(size: 18))
+                        .foregroundColor(ColorPalette.gtGrey.color)
+                                        
+                    Text("Ha seleccinado Ingles como idioma de su aplicacion. Esto cambiara toda la aplicacion GodTools. Esta seguro?")
+                        .font(FontLibrary.sfProTextRegular.font(size: 14))
+                        .foregroundColor(ColorPalette.gtGrey.color)
+                }
+                .padding(.horizontal, 10)
+                
+                FixedVerticalSpacer(height: 20)
+                
+                let buttonSpacing: CGFloat = 12
+
+                HStack(spacing: buttonSpacing) {
+                    
+                    let buttonWidth = (geometry.size.width - buttonSpacing - 2*horizontalPadding) / 2
+                    
+                    GTBlueButton(title: "Change language", fontSize: 15, width: buttonWidth, height: 48) {
+                        
+                    }
+                    
+                    GTWhiteButton(title: "Nevermind", fontSize: 15, width: buttonWidth, height: 48) {
+                        
+                    }
                     
                 }
                 
-                GTWhiteButton(title: "Nevermind", fontSize: 15, width: 145, height: 48) {
-                    
-                }
+                Spacer()
+                Spacer()
+                
             }
+            .padding(.horizontal, horizontalPadding)
         }
     }
 }
