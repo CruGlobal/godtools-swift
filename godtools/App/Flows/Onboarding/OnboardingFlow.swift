a9718a6ca069c7eec9147277a0fca7147e26218f
Levi Eggert <levi.eggert@cru.org>
2023-10-30 19:04:27 -0400

    Set locale for onboarding tutorial video to current app language

--- godtools/App/Flows/Onboarding/OnboardingFlow.swift
+++ godtools/App/Flows/Onboarding/OnboardingFlow.swift
@@ -13,6 +13,7 @@ import Combine
 class OnboardingFlow: Flow, ChooseAppLanguageNavigationFlow {
     
     @Published private var quickStartIsAvailable: Bool = false
+    @Published private var currentAppLanguage: AppLanguageCodeDomainModel = LanguageCodeDomainModel.english.value
     
     private var cancellables: Set<AnyCancellable> = Set()
     
@@ -53,6 +54,11 @@ class OnboardingFlow: Flow, ChooseAppLanguageNavigationFlow {
             .getAvailablePublisher(appLanguageCodeChangedPublisher: getCurrentAppLanguageUseCase.getLanguagePublisher())
             .receive(on: DispatchQueue.main)
             .assign(to: &$quickStartIsAvailable)
+        
+        getCurrentAppLanguageUseCase
+            .getLanguagePublisher()
+            .receive(on: DispatchQueue.main)
+            .assign(to: &$currentAppLanguage)
     }
     
     func getInitialView() -> UIViewController {
@@ -107,10 +113,14 @@ class OnboardingFlow: Flow, ChooseAppLanguageNavigationFlow {
     
     private func presentWatchOnboardingTutorialVideoPlayer(youtubeVideoId: String) {
         
+        let videoPlayerParameters: [String: Any] = [
+            YoutubePlayerParameters.interfaceLanguage.rawValue: currentAppLanguage
+        ]
+        
         let viewModel = FullScreenVideoViewModel(
             flowDelegate: self,
             videoId: youtubeVideoId,
-            videoPlayerParameters: nil,
+            videoPlayerParameters: videoPlayerParameters,
             userDidCloseVideoStep: .closeVideoPlayerTappedFromOnboardingTutorial,
             videoEndedStep: .videoEndedOnOnboardingTutorial
         )
