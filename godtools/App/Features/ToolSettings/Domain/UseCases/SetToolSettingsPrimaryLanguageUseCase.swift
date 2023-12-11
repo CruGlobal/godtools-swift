5126c1031910132e801cc644f6ce1fc889a312cb
Levi Eggert <levi.eggert@cru.org>
2023-12-11 13:36:07 -0500

    Add use case for setting tool settings primary language

---
+++ godtools/App/Features/ToolSettings/Domain/UseCases/SetToolSettingsPrimaryLanguageUseCase.swift
@@ -0,0 +1,17 @@
+//
+//  SetToolSettingsPrimaryLanguageUseCase.swift
+//  godtools
+//
+//  Created by Levi Eggert on 12/11/23.
+//  Copyright © 2023 Cru. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+class SetToolSettingsPrimaryLanguageUseCase {
+    
+    init() {
+        
+    }
+}
