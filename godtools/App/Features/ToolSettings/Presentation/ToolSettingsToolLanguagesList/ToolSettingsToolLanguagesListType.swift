b3cf67050af3d5fae9fa870dabeadb122b1fe63d
Levi Eggert <levi.eggert@cru.org>
2023-12-11 13:35:16 -0500

    Renamed enum and moved to Presentation/ToolSettingsToolLanguagesList

---
+++ godtools/App/Features/ToolSettings/Presentation/ToolSettingsToolLanguagesList/ToolSettingsToolLanguagesListType.swift
@@ -0,0 +1,15 @@
+//
+//  ToolSettingsToolLanguagesListType.swift
+//  godtools
+//
+//  Created by Levi Eggert on 5/18/22.
+//  Copyright © 2022 Cru. All rights reserved.
+//
+
+import Foundation
+
+enum ToolSettingsToolLanguagesListType {
+    
+    case choosePrimaryLanguage
+    case chooseParallelLanguage
+}
