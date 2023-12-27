abeaec8cc06938d460bef2e512c7e26c2834bb76
Levi Eggert <levi.eggert@cru.org>
2023-12-27 16:33:29 -0500

    Increment schema

--- godtools/App/Share/Data/RealmDatabase/Configuration/RealmDatabaseProductionConfiguration.swift
+++ godtools/App/Share/Data/RealmDatabase/Configuration/RealmDatabaseProductionConfiguration.swift
@@ -12,7 +12,7 @@ import RealmSwift
 class RealmDatabaseProductionConfiguration: RealmDatabaseConfiguration {
     
     private static let diskFileName: String = "godtools_realm"
-    private static let schemaVersion: UInt64 = 26
+    private static let schemaVersion: UInt64 = 27
     
     init() {
         
