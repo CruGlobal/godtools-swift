76e1f188e2a5a45436cc44bcef3c7e78566c27b7
Levi Eggert <levi.eggert@cru.org>
2024-01-10 09:58:02 -0500

    Expose method to setSemanticContentAttribute Add method to setLayoutDirectionPublisher, defaults to ApplicationLayout.shared

--- godtools/App/Share/Views/Navigation/NavigationControllers/AppNavigationController.swift
+++ godtools/App/Share/Views/Navigation/NavigationControllers/AppNavigationController.swift
@@ -12,13 +12,15 @@ import Combine
 
 class AppNavigationController: UINavigationController {
     
+    private var layoutDirectionPublisher: AnyPublisher<UISemanticContentAttribute, Never>
     private var cancellables: Set<AnyCancellable> = Set()
-        
+    
     let navigationBarAppearance: AppNavigationBarAppearance?
     
-    init(hidesNavigationBar: Bool = false, navigationBarAppearance: AppNavigationBarAppearance?) {
+    init(hidesNavigationBar: Bool = false, navigationBarAppearance: AppNavigationBarAppearance?, layoutDirectionPublisher: AnyPublisher<UISemanticContentAttribute, Never>? = nil) {
         
         self.navigationBarAppearance = navigationBarAppearance
+        self.layoutDirectionPublisher = layoutDirectionPublisher ?? ApplicationLayout.shared.semanticContentAttributePublisher
         
         super.init(nibName: nil, bundle: nil)
         
@@ -47,29 +49,50 @@ class AppNavigationController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ApplicationLayout.shared.semanticContentAttributePublisher
+        setLayoutDirectionPublisher(
+            layoutDirectionPublisher: layoutDirectionPublisher
+        )
+    }
+    
+    func setLayoutDirectionPublisherToApplicationLayout() {
+        
+        setLayoutDirectionPublisher(
+            layoutDirectionPublisher: ApplicationLayout.shared.semanticContentAttributePublisher
+        )
+    }
+    
+    func setLayoutDirectionPublisher(layoutDirectionPublisher: AnyPublisher<UISemanticContentAttribute, Never>) {
+        
+        self.layoutDirectionPublisher = layoutDirectionPublisher
+        
+        layoutDirectionPublisher
             .receive(on: DispatchQueue.main)
             .sink { [weak self] (semanticContentAttribute: UISemanticContentAttribute) in
                 
-                self?.view.semanticContentAttribute = semanticContentAttribute
-                self?.navigationBar.semanticContentAttribute = semanticContentAttribute
-                
-                let viewControllers: [UIViewController] = self?.viewControllers ?? Array()
-                
-                // NOTE: This forloop is here to reload SwiftUI view's that have a TabView.  It feels like a hacky solution, but it's the only solution I can come up with.  Due to a bug in iOS 16.3.1 TabView not updating correctly for right to left languages.  TabViews have to be forced in a left to right language direction and then the contents are manually created based on the language direction. ~Levi
-                
-                for viewController in viewControllers {
-                    
-                    if let dashboardHostingView = viewController as? AppHostingController<DashboardView> {
-                        let currentTab: DashboardTabTypeDomainModel = dashboardHostingView.rootView.getCurrentTab()
-                        dashboardHostingView.rootView.navigateToTab(tab: currentTab)
-                    }
-                    else if let onboardingTutorialHostingView = viewController as? AppHostingController<OnboardingTutorialView> {
-                        let currentPage: Int = onboardingTutorialHostingView.rootView.getCurrentPage()
-                        onboardingTutorialHostingView.rootView.setCurrentPage(page: currentPage)
-                    }
-                }
+                self?.setSemanticContentAttribute(semanticContentAttribute: semanticContentAttribute)
             }
             .store(in: &cancellables)
+    }
+    
+    func setSemanticContentAttribute(semanticContentAttribute: UISemanticContentAttribute) {
+        
+        view.semanticContentAttribute = semanticContentAttribute
+        navigationBar.semanticContentAttribute = semanticContentAttribute
+        
+        let viewControllers: [UIViewController] = viewControllers
+        
+        // NOTE: This forloop is here to reload SwiftUI view's that have a TabView.  It feels like a hacky solution, but it's the only solution I can come up with.  Due to a bug in iOS 16.3.1 TabView not updating correctly for right to left languages.  TabViews have to be forced in a left to right language direction and then the contents are manually created based on the language direction. ~Levi
+        
+        for viewController in viewControllers {
+            
+            if let dashboardHostingView = viewController as? AppHostingController<DashboardView> {
+                let currentTab: DashboardTabTypeDomainModel = dashboardHostingView.rootView.getCurrentTab()
+                dashboardHostingView.rootView.navigateToTab(tab: currentTab)
+            }
+            else if let onboardingTutorialHostingView = viewController as? AppHostingController<OnboardingTutorialView> {
+                let currentPage: Int = onboardingTutorialHostingView.rootView.getCurrentPage()
+                onboardingTutorialHostingView.rootView.setCurrentPage(page: currentPage)
+            }
+        }
     }
 }
