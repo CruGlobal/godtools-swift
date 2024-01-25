2b73fa2c18d6a2b219959caf2791b1aefc677738
Rachael Skeath <rachaelblue@gmail.com>
2024-01-25 15:01:05 -0500

    add note about ScenePhase

--- godtools/App/Share/Extensions/View+AppLifecycle.swift
+++ godtools/App/Share/Extensions/View+AppLifecycle.swift
@@ -10,6 +10,11 @@ import SwiftUI
 
 extension View {
     
+    // NOTE: SwiftUI supports lifecycle observation through `ScenePhase` (https://developer.apple.com/documentation/swiftui/scenephase),
+    // but ScenePhase seems to require being propagated from the App's ContentView in order to work, and not through UIViewController. (https://stackoverflow.com/questions/72620039/trigger-an-action-when-scenephase-changes-in-a-sheet)
+    
+    // TODO: - when we completely transition from UIKit to SwiftUI, reinvestigate using `ScenePhase` for background/foreground observation rather than listening for UIApplication notifications.
+    
     func onAppBackgrounded(_ closure: @escaping () -> Void) -> some View {
         self.onReceive(
             NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification),
