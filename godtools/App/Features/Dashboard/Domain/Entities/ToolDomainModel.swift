ef49362770a2eed2f5c50dea90c85bb5defc45c9
Levi Eggert <levi.eggert@cru.org>
2024-02-16 15:23:27 -0500

    Move to entities folder

--- godtools/App/Features/Dashboard/Domain/Entities/ToolDomainModel.swift
+++ godtools/App/Features/Dashboard/Domain/Entities/ToolDomainModel.swift
@@ -10,6 +10,7 @@ import Foundation
 
 struct ToolDomainModel: ToolListItemDomainModelInterface {
     
+    let interfaceStrings: ToolListItemInterfaceStringsDomainModel
     let abbreviation: String
     let bannerImageId: String
     let category: String
@@ -17,13 +18,19 @@ struct ToolDomainModel: ToolListItemDomainModelInterface {
     let dataModelId: String
     let languageIds: [String]
     let name: String
+    let isFavorited: Bool = false
+    let languageAvailability: ToolLanguageAvailabilityDomainModel
     
     // TODO: - Remove this once we're done refactoring to pass ToolDomainModels around instead of ResourceModels. See GT-2100. ~Levi
     @available(*, deprecated)
     let resource: ResourceModel
 }
 
-extension ToolDomainModel: Equatable {}
+extension ToolDomainModel: Equatable {
+    static func ==(lhs: ToolDomainModel, rhs: ToolDomainModel) -> Bool {
+        return lhs.dataModelId == rhs.dataModelId
+    }
+}
 
 extension ToolDomainModel: Identifiable {
     
