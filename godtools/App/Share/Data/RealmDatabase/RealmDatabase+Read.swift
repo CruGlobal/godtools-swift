c691b6731b558594bceab009b7a3e39d99c0d9b2
Levi Eggert <levi.eggert@cru.org>
2024-05-03 09:21:08 -0400

    Update RealmDatabase read extension to include a mapping closure within the background queue this way we can map realm objects to data models within the background queue

--- godtools/App/Share/Data/RealmDatabase/RealmDatabase+Read.swift
+++ godtools/App/Share/Data/RealmDatabase/RealmDatabase+Read.swift
@@ -10,6 +10,8 @@ import Foundation
 import RealmSwift
 import Combine
 
+// MARK: - Read Object
+
 extension RealmDatabase {
     
     func readObjectElseCreateNew<T: IdentifiableRealmObject>(realm: Realm, primaryKey: String) -> T {
@@ -58,6 +60,102 @@ extension RealmDatabase {
             self.readObjectInBackground(primaryKey: primaryKey) { (realmObject: T?) in
                 
                 promise(.success(realmObject))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Read Object With Mapping
+
+extension RealmDatabase {
+    
+    func readAndMapObjectInBackground<T: Object, U>(primaryKey: String, mapInBackgroundClosure: @escaping ((_ object: T?) -> U?), completion: @escaping ((_ mappedObject: U?) -> Void)) {
+        
+        background { realm in
+            
+            let realmObject: T? = self.readObject(realm: realm, primaryKey: primaryKey)
+            
+            let mappedObject: U? = mapInBackgroundClosure(realmObject)
+
+            completion(mappedObject)
+        }
+    }
+    
+    func readAndMapObjectPublisher<T: Object, U>(primaryKey: String, mapInBackgroundClosure: @escaping ((_ object: T?) -> U?)) -> AnyPublisher<U?, Never> {
+        
+        return Future() { promise in
+            
+            self.readAndMapObjectInBackground(primaryKey: primaryKey, mapInBackgroundClosure: mapInBackgroundClosure) { (mappedObject: U?) in
+                
+                promise(.success(mappedObject))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Read Objects
+
+extension RealmDatabase {
+    
+    func readObjects<T: Object>(realm: Realm) -> Results<T> {
+        return realm.objects(T.self)
+    }
+    
+    func readObjects<T: Object>() -> Results<T> {
+          
+        let realm: Realm = openRealm()
+        
+        return readObjects(realm: realm)
+    }
+    
+    func readObjectsInBackground<T: Object>(completion: @escaping ((_ results: Results<T>) -> Void)) {
+        
+        background { realm in
+            
+            let results: Results<T> = self.readObjects(realm: realm)
+
+            completion(results)
+        }
+    }
+    
+    func readObjectsPublisher<T: Object>() -> AnyPublisher<Results<T>, Never> {
+        
+        return Future() { promise in
+            
+            self.readObjectsInBackground() { (results: Results<T>) in
+                
+                promise(.success(results))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
+
+// MARK: - Read Objects With Mapping
+
+extension RealmDatabase {
+    
+    func readAndMapObjectsInBackground<T: Object, U>(mapInBackgroundClosure: @escaping ((_ results: Results<T>) -> [U]), completion: @escaping ((_ mappedObjects: [U]) -> Void)) {
+        
+        background { realm in
+            
+            let results: Results<T> = self.readObjects(realm: realm)
+            
+            let mappedObjects: [U] = mapInBackgroundClosure(results)
+
+            completion(mappedObjects)
+        }
+    }
+    
+    func readAndMapObjectsPublisher<T: Object, U>(mapInBackgroundClosure: @escaping ((_ results: Results<T>) -> [U])) -> AnyPublisher<[U], Never> {
+        
+        return Future() { promise in
+            
+            self.readAndMapObjectsInBackground(mapInBackgroundClosure: mapInBackgroundClosure) { (mappedObjects: [U]) in
+                
+                promise(.success(mappedObjects))
             }
         }
         .eraseToAnyPublisher()
