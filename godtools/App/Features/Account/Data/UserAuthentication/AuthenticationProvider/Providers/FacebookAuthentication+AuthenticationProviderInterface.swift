c9554c58d8dae090e2cae56f052962c959f59346
Levi Eggert <levi.eggert@cru.org>
2024-05-28 13:26:48 -0400

    Ignore user cancelled errors

--- godtools/App/Features/Account/Data/UserAuthentication/AuthenticationProvider/Providers/FacebookAuthentication+AuthenticationProviderInterface.swift
+++ godtools/App/Features/Account/Data/UserAuthentication/AuthenticationProvider/Providers/FacebookAuthentication+AuthenticationProviderInterface.swift
@@ -57,7 +57,15 @@ extension FacebookAuthentication: AuthenticationProviderInterface {
         return authenticatePublisher(from: presentingViewController)
             .flatMap({ (response: FacebookAuthenticationResponse) -> AnyPublisher<AuthenticationProviderResponse, Error> in
                 
-                return self.getResponseForPersistedData().publisher
+                if !response.isCancelled {
+                    
+                    return self.getResponseForPersistedData().publisher
+                        .eraseToAnyPublisher()
+                }
+                
+                let cancelledError: Error = NSError.errorWithDomain(domain: "", code: NSUserCancelledError, description: "Cancelled")
+                
+                return Fail(error: cancelledError)
                     .eraseToAnyPublisher()
             })
             .eraseToAnyPublisher()
