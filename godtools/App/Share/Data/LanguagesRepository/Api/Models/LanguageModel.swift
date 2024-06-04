917c0ddc99ca03fa8c4f4ca07875f53e75e9e3ba
Levi Eggert <levi.eggert@cru.org>
2024-06-04 10:07:40 -0400

    Create temp override of en-PH to fil-x-taglish

--- godtools/App/Share/Data/LanguagesRepository/Api/Models/LanguageModel.swift
+++ godtools/App/Share/Data/LanguagesRepository/Api/Models/LanguageModel.swift
@@ -45,7 +45,14 @@ struct LanguageModel: LanguageModelType, Codable {
         }
         
         // attributes
-        code = try attributesContainer?.decodeIfPresent(BCP47LanguageIdentifier.self, forKey: .code) ?? ""
+        
+        // TODO: Remove tempCode override.  This is a temporary Taglish label until production switches to the fil-x-taglish language tag.
+        var tempCode: String = try attributesContainer?.decodeIfPresent(BCP47LanguageIdentifier.self, forKey: .code) ?? ""
+        if tempCode == "en-PH" {
+            tempCode = "fil-x-taglish"
+        }
+        
+        code = tempCode//try attributesContainer?.decodeIfPresent(BCP47LanguageIdentifier.self, forKey: .code) ?? "" // TODO: Remove tempCode and replace with commented out line.
         direction = try attributesContainer?.decodeIfPresent(String.self, forKey: .direction) ?? ""
         name = try attributesContainer?.decodeIfPresent(String.self, forKey: .name) ?? ""
     }
