e58cb1475366069da1de8ab6ab5aa84e1a5102b5
Rachael Skeath <rachaelblue@gmail.com>
2024-06-03 20:46:05 -0400

    add get user tool settings repo

--- godtools/App/Features/ToolSettings/Data-DomainInterface/GetUserToolSettingsRepository.swift
+++ godtools/App/Features/ToolSettings/Data-DomainInterface/GetUserToolSettingsRepository.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+import Combine
+
+class GetUserToolSettingsRepository: GetUserToolSettingsRepositoryInterface {
+    
+    private let toolSettingsRepository: ToolSettingsRepository
+    
+    init(toolSettingsRepository: ToolSettingsRepository) {
+        self.toolSettingsRepository = toolSettingsRepository
+    }
+    
+    func getUserToolSettingsPublisher(toolId: String) -> AnyPublisher<UserToolSettingsDomainModel?, Never> {
+        
+        return toolSettingsRepository.getToolSettingsPublisher(toolId: toolId)
+            .map { toolSettingsDataModel in
+                
+                guard let toolSettingsDataModel = toolSettingsDataModel else { return nil }
+                
+                return UserToolSettingsDomainModel(
+                    toolId: toolId,
+                    primaryLanguageId: toolSettingsDataModel.primaryLanguageId,
+                    parallelLanguageId: toolSettingsDataModel.parallelLanguageId
+                )
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    
+    
+}
