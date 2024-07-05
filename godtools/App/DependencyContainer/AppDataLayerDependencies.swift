d9328989f0a0025bd250a9c6bf9ef8d413b8a37b
Levi Eggert <levi.eggert@cru.org>
2024-07-05 09:06:13 -0400

    Add LocalizationLanguageNameRepository to data layer dependencies and remove translated language name repository

--- godtools/App/DependencyContainer/AppDataLayerDependencies.swift
+++ godtools/App/DependencyContainer/AppDataLayerDependencies.swift
@@ -165,6 +165,12 @@ class AppDataLayerDependencies {
         )
     }
     
+    func getLocalizationLanguageNameRepository() -> LocalizationLanguageNameRepository {
+        return LocalizationLanguageNameRepository(
+            localizationServices: getLocalizationServices()
+        )
+    }
+    
     func getLocalizationServices() -> LocalizationServices {
         return LocalizationServices(localizableStringsFilesBundle: Bundle.main)
     }
@@ -293,28 +299,9 @@ class AppDataLayerDependencies {
         )
     }
     
-    private func getTranslatedLanguageNameCache() -> RealmTranslatedLanguageNameCache {
-        return RealmTranslatedLanguageNameCache(realmDatabase: sharedRealmDatabase)
-    }
-    
-    func getTranslatedLanguageNameRepository() -> TranslatedLanguageNameRepository {
-        return TranslatedLanguageNameRepository(
-            getTranslatedLanguageName: getTranslatedLanguageName(),
-            cache: getTranslatedLanguageNameCache()
-        )
-    }
-    
-    func getTranslatedLanguageNameRepositorySync() -> TranslatedLanguageNameRepositorySync {
-        return TranslatedLanguageNameRepositorySync(
-            realmDatabase: sharedRealmDatabase,
-            getTranslatedLanguageName: getTranslatedLanguageName(),
-            cache: getTranslatedLanguageNameCache()
-        )
-    }
-    
-    private func getTranslatedLanguageName() -> GetTranslatedLanguageName {
+    func getTranslatedLanguageName() -> GetTranslatedLanguageName {
         return GetTranslatedLanguageName(
-            localizationServices: getLocalizationServices(),
+            localizationLanguageNameRepository: getLocalizationLanguageNameRepository(),
             localeLanguageName: LocaleLanguageName(),
             localeRegionName: LocaleLanguageRegionName(),
             localeScriptName: LocaleLanguageScriptName()
@@ -344,7 +331,7 @@ class AppDataLayerDependencies {
             localizationServices: getLocalizationServices(),
             resourcesRepository: getResourcesRepository(),
             languagesRepository: getLanguagesRepository(),
-            translatedLanguageNameRepository: getTranslatedLanguageNameRepository()
+            getTranslatedLanguageName: getTranslatedLanguageName()
         )
     }
     
