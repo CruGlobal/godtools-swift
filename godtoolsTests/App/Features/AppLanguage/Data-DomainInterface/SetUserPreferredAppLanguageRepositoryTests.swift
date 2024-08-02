fb3c259b58757d82f3d786bc82de18443754604e
Rachael Skeath <rachaelblue@gmail.com>
2024-08-02 15:57:45 -0400

    add test for setting user preferred app language

---
+++ godtoolsTests/App/Features/AppLanguage/Data-DomainInterface/SetUserPreferredAppLanguageRepositoryTests.swift
@@ -0,0 +1,9 @@
+//
+//  SetUserPreferredAppLanguageRepositoryTests.swift
+//  godtoolsTests
+//
+//  Created by Rachael Skeath on 8/2/24.
+//  Copyright © 2024 Cru. All rights reserved.
+//
+
+import Foundation
