4ec00c95d0acc68bddaf553813997fd26f98c60a
Levi Eggert <levi.eggert@cru.org>
2025-01-15 17:30:06 -0500

    Re-add Firebase module

--- godtools/App/Share/Data/RemoteConfigRepository/RemoteDatabase/FirebaseRemoteConfigWrapper.swift
+++ godtools/App/Share/Data/RemoteConfigRepository/RemoteDatabase/FirebaseRemoteConfigWrapper.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 import Combine
-import FirebaseRemoteConfigInternal
+import FirebaseRemoteConfig
 
 class FirebaseRemoteConfigWrapper: RemoteConfigRemoteDatabaseInterface {
     
