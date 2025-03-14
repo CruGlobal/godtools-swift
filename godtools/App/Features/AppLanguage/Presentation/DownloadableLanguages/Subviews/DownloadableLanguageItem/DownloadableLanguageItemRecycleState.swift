1b32bb3df69b78e3252db8e849bcf6cb751f6300
Levi Eggert <levi.eggert@cru.org>
2025-03-14 09:54:26 -0400

    Add note

--- godtools/App/Features/AppLanguage/Presentation/DownloadableLanguages/Subviews/DownloadableLanguageItem/DownloadableLanguageItemRecycleState.swift
+++ godtools/App/Features/AppLanguage/Presentation/DownloadableLanguages/Subviews/DownloadableLanguageItem/DownloadableLanguageItemRecycleState.swift
@@ -9,6 +9,9 @@
 import Foundation
 
 class DownloadableLanguageItemRecycleState: ObservableObject {
+
+    // NOTE: This object is used to maintain the view state when the view is recycled so these objects will remain in memory.
+    //       Need to be careful that we don't add high memory cost references here such as images. ~Levi
     
     @Published var downloadProgress: Double?
     @Published var downloadError: Error?
