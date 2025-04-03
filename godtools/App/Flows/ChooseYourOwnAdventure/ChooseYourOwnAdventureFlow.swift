702c1519b45f672eccff5d77a1903008a1fd932e
Rachael Skeath <rachaelblue@gmail.com>
2025-04-03 16:11:31 -0400

    set nav title to empty string

--- godtools/App/Flows/ChooseYourOwnAdventure/ChooseYourOwnAdventureFlow.swift
+++ godtools/App/Flows/ChooseYourOwnAdventure/ChooseYourOwnAdventureFlow.swift
@@ -162,6 +162,8 @@ extension ChooseYourOwnAdventureFlow {
                     navigationBar.setTitleView(
                         titleView: self?.getNewLanguageSelectorView(view: chooseYourOwnAdventureView, viewModel: viewModel, navBarLayoutDirection: navBarLayoutDirection)
                     )
+                } else {
+                    navigationBar.setTitle(title: "")
                 }
             }
             .store(in: &cancellables)
