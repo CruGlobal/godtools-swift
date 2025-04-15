e69cf24a750a3a8b9939391e62c2d0147f98b157
Levi Eggert <levi.eggert@cru.org>
2025-04-15 14:58:32 -0400

    Update some new line auto formatting in SwiftUI View

--- godtools/App/Features/OptInNotification/Presentation/OptInNotification/OptInNotificationView.swift
+++ godtools/App/Features/OptInNotification/Presentation/OptInNotification/OptInNotificationView.swift
@@ -10,6 +10,7 @@ import Foundation
 import SwiftUI
 
 struct OptInNotificationView: View {
+    
     @ObservedObject private var viewModel: OptInNotificationViewModel
 
     init(viewModel: OptInNotificationViewModel) {
@@ -19,59 +20,44 @@ struct OptInNotificationView: View {
     var body: some View {
 
         VStack {
+            
             Image("notification_graphic").resizable()
                 .scaledToFit()
-                .padding(
-                    EdgeInsets(
-                        top: 5,
-                        leading: 5,
-                        bottom: 0,
-                        trailing: 5
-                    )
-                ).overlay(
+                .padding(EdgeInsets(top: 5, leading: 5, bottom: 0, trailing: 5))
+                .overlay(
                     Rectangle()
                         .frame(height: 2)
-                        .foregroundColor(
-                            Color(
-                                uiColor: ColorPalette.gtBlue
-                                    .uiColor
-                            )
-                        ),
+                        .foregroundColor(Color(uiColor: ColorPalette.gtBlue.uiColor)),
                     alignment: .bottom
                 )
+            
             Text(viewModel.title)
-                .foregroundColor(
-                    Color(uiColor: ColorPalette.gtBlue.uiColor)
-                ).font(
-                    FontLibrary.sfProTextRegular.font(size: 30)
-                )
+                .foregroundColor(Color(uiColor: ColorPalette.gtBlue.uiColor))
+                .font(FontLibrary.sfProTextRegular.font(size: 30))
                 .fontWeight(.bold)
-                .padding(
-                    .vertical,
-                    10
-                ).minimumScaleFactor(0.5).lineLimit(1)
+                .padding(.vertical, 10)
+                .minimumScaleFactor(0.5)
+                .lineLimit(1)
 
-            Text(
-                viewModel.body
-            ).font(FontLibrary.sfProTextRegular.font(size: 18))
+            Text(viewModel.body)
+                .font(FontLibrary.sfProTextRegular.font(size: 18))
                 .foregroundStyle(ColorPalette.gtGrey.color)
-                .multilineTextAlignment(.center).padding(
-                    .bottom,
-                    12
-                )
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 12)
 
             Button(action: {
+                
                 viewModel.allowNotificationsTapped()
 
             }) {
                 RoundedRectangle(cornerRadius: 5).fill(
                     Color(uiColor: ColorPalette.gtBlue.uiColor)
                 )
-            }.frame(height: 45).overlay(
+            }
+            .frame(height: 45)
+            .overlay(
                 Text(viewModel.allowNotificationsActionTitle)
-                    .foregroundColor(
-                        .white
-                    )
+                    .foregroundColor(.white)
             )
 
             Button(action: {
@@ -85,30 +71,20 @@ struct OptInNotificationView: View {
                         )
                     )
             }
-            .frame(height: 40).padding(
-                .bottom,
-                50
-            )
-        }.padding(.horizontal, 20).background(
-            RoundedRectangle(
-                cornerRadius: 10
-            ).stroke(
-                Color(uiColor: ColorPalette.gtBlue.uiColor),
-                lineWidth: 8
-            )
+            .frame(height: 40)
+            .padding(.bottom, 50)
+            
+        }
+        .padding(.horizontal, 20)
+        .background(
+            RoundedRectangle(cornerRadius: 10).stroke(Color(uiColor: ColorPalette.gtBlue.uiColor), lineWidth: 8)
             .overlay(
-                RoundedRectangle(cornerRadius: 10).foregroundStyle(
-                    .white
-                )
+                RoundedRectangle(cornerRadius: 10).foregroundStyle(.white)
             )
         )
-        .padding(.horizontal, UIScreen.main.bounds.width * 0.05).environment(
-            \.layoutDirection,
-            ApplicationLayout.shared.layoutDirection
-        )
-
+        .padding(.horizontal, UIScreen.main.bounds.width * 0.05)
+        .environment(\.layoutDirection, ApplicationLayout.shared.layoutDirection)
     }
-
 }
 
 // MARK: - Preview
@@ -117,8 +93,7 @@ struct OptInNotificationView_Preview: PreviewProvider {
 
     static func getOptInNotificationViewModel() -> OptInNotificationViewModel {
 
-        let appDiContainer: AppDiContainer = SwiftUIPreviewDiContainer()
-            .getAppDiContainer()
+        let appDiContainer: AppDiContainer = SwiftUIPreviewDiContainer().getAppDiContainer()
 
         let viewModel = OptInNotificationViewModel(
 
@@ -138,11 +113,9 @@ struct OptInNotificationView_Preview: PreviewProvider {
             checkNotificationStatusUseCase: appDiContainer.feature
                 .optInNotification.domainLayer
                 .getCheckNotificationStatusUseCase(),
-
             getCurrentAppLanguageUseCase: appDiContainer.feature.appLanguage
                 .domainLayer.getCurrentAppLanguageUseCase(),
             flowDelegate: MockFlowDelegate()
-
         )
 
         return viewModel
@@ -151,8 +124,7 @@ struct OptInNotificationView_Preview: PreviewProvider {
     static var previews: some View {
 
         OptInNotificationView(
-            viewModel:
-                OptInNotificationView_Preview.getOptInNotificationViewModel()
+            viewModel: OptInNotificationView_Preview.getOptInNotificationViewModel()
         )
     }
 }
