33e7e9a612cf0dafd651eb6e12eaab021fcef7e2
Levi Eggert <levi.eggert@cru.org>
2025-06-25 13:32:59 -0400

    Reference not needed here

--- godtools/App/GodToolsAppDelegate.swift
+++ godtools/App/GodToolsAppDelegate.swift
@@ -12,9 +12,7 @@ import SocialAuthentication
 class GodToolsAppDelegate: NSObject, UIApplicationDelegate {
         
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-                     
-        let application: UIApplication = UIApplication.shared
-        
+                             
         ConfigureFacebookOnAppLaunch.configure(
             application: application,
             launchOptions: launchOptions,
