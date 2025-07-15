71cd8cd629da9162f92a71c19aa145a5f0eff957
Levi Eggert <levi.eggert@cru.org>
2025-07-15 16:15:24 -0400

    Moved to SwiftTests

--- godtoolsTests/App/Share/Data-DomainInterface/Supporting/GetTranslatedPercentage/GetTranslatedPercentageTests.swift
+++ godtoolsTests/App/Share/Data-DomainInterface/Supporting/GetTranslatedPercentage/GetTranslatedPercentageTests.swift
@@ -2,124 +2,58 @@
 //  GetTranslatedPercentageTests.swift
 //  godtools
 //
-//  Created by Levi Eggert on 3/18/25.
+//  Created by Levi Eggert on 7/15/25.
 //  Copyright © 2025 Cru. All rights reserved.
 //
 
-import Foundation
+import Testing
 @testable import godtools
-import Quick
-import Nimble
 
-class GetTranslatedPercentageTests: QuickSpec {
+struct GetTranslatedPercentageTests {
     
-    override class func spec() {
+    struct TestArgument {
+        let percentageValue: Double
+        let expectedValue: String
+    }
+    
+    private static let englishLanguage = LanguageCodeDomainModel.english.rawValue
+    
+    @Test(
+        "When user is viewing a percentage value in english, the percentage should round down to the nearest integer value.",
+        arguments: [
+            TestArgument(percentageValue: 0.2449, expectedValue: "24%"),
+            TestArgument(percentageValue: 0.001, expectedValue: "0%")
+        ]
+    )
+    func testPercentageIsRoundedDown(argument: TestArgument) {
                 
-        let englishLanguage = LanguageCodeDomainModel.english.rawValue
+        let getTranslatedPercentage = GetTranslatedPercentage()
         
-        describe("User is viewing a percentage value in the english language.") {
-            
-            let percentageValue: Double = 0.2449
-            let expectedValue: String = "24%"
-            
-            context("When the percentage value is \(percentageValue)") {
+        let percentageString = getTranslatedPercentage.getTranslatedPercentage(
+            percentValue: argument.percentageValue,
+            translateInLanguage: Self.englishLanguage
+        )
+                            
+        #expect(percentageString == argument.expectedValue)
+    }
+    
+    @Test(
+        "When user is viewing a percentage value in english, the percentage should round up to the nearest integer value.",
+        arguments: [
+            TestArgument(percentageValue: 0.0051, expectedValue: "1%"),
+            TestArgument(percentageValue: 0.625000000001, expectedValue: "63%"),
+            TestArgument(percentageValue: 0.8599911111111111, expectedValue: "86%")
+        ]
+    )
+    func testPercentageIsRoundedUp(argument: TestArgument) {
                 
-                it("The percentage should round down to the nearest integer value.") {
-                    
-                    let getTranslatedPercentage = GetTranslatedPercentage()
-                    
-                    let percentageString = getTranslatedPercentage.getTranslatedPercentage(
-                        percentValue: percentageValue,
-                        translateInLanguage: englishLanguage
-                    )
-                                        
-                    expect(percentageString).to(equal(expectedValue))
-                }
-            }
-        }
+        let getTranslatedPercentage = GetTranslatedPercentage()
         
-        describe("User is viewing a percentage value in the english language.") {
-            
-            let percentageValue: Double = 0.001
-            let expectedValue: String = "0%"
-            
-            context("When the percentage value is \(percentageValue)") {
-                
-                it("The percentage should round down to the nearest integer value.") {
-                    
-                    let getTranslatedPercentage = GetTranslatedPercentage()
-                    
-                    let percentageString = getTranslatedPercentage.getTranslatedPercentage(
-                        percentValue: percentageValue,
-                        translateInLanguage: englishLanguage
-                    )
-                                        
-                    expect(percentageString).to(equal(expectedValue))
-                }
-            }
-        }
-        
-        describe("User is viewing a percentage value in the english language.") {
-            
-            let percentageValue: Double = 0.0051
-            let expectedValue: String = "1%"
-            
-            context("When the percentage value is \(percentageValue)") {
-                
-                it("The percentage should round up to the nearest integer value.") {
-                    
-                    let getTranslatedPercentage = GetTranslatedPercentage()
-                    
-                    let percentageString = getTranslatedPercentage.getTranslatedPercentage(
-                        percentValue: percentageValue,
-                        translateInLanguage: englishLanguage
-                    )
-                                        
-                    expect(percentageString).to(equal(expectedValue))
-                }
-            }
-        }
-        
-        describe("User is viewing a percentage value in the english language.") {
-            
-            let percentageValue: Double = 0.625000000001
-            let expectedValue: String = "63%"
-            
-            context("When the percentage value is \(percentageValue)") {
-                
-                it("The percentage should round up to the nearest integer value.") {
-                    
-                    let getTranslatedPercentage = GetTranslatedPercentage()
-                    
-                    let percentageString = getTranslatedPercentage.getTranslatedPercentage(
-                        percentValue: percentageValue,
-                        translateInLanguage: englishLanguage
-                    )
-                                        
-                    expect(percentageString).to(equal(expectedValue))
-                }
-            }
-        }
-        
-        describe("User is viewing a percentage value in the english language.") {
-            
-            let percentageValue: Double = 0.8599911111111111
-            let expectedValue: String = "86%"
-            
-            context("When the percentage value is \(percentageValue)") {
-                
-                it("The percentage should round up to the nearest integer value.") {
-                    
-                    let getTranslatedPercentage = GetTranslatedPercentage()
-                    
-                    let percentageString = getTranslatedPercentage.getTranslatedPercentage(
-                        percentValue: percentageValue,
-                        translateInLanguage: englishLanguage
-                    )
-                                        
-                    expect(percentageString).to(equal(expectedValue))
-                }
-            }
-        }
+        let percentageString = getTranslatedPercentage.getTranslatedPercentage(
+            percentValue: argument.percentageValue,
+            translateInLanguage: Self.englishLanguage
+        )
+                            
+        #expect(percentageString == argument.expectedValue)
     }
 }
