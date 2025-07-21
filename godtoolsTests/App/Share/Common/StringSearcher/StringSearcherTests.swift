6ed9cfa8c1efe512560cd4be8e7838a7f734f5ef
Rachael Skeath <rachaelblue@gmail.com>
2025-07-21 16:08:12 -0400

    string searchable test

---
+++ godtoolsTests/App/Share/Common/StringSearcher/StringSearcherTests.swift
@@ -0,0 +1,58 @@
+//
+//  StringSearcherTests.swift
+//  godtoolsTests
+//
+//  Created by Rachael Skeath on 7/21/25.
+//  Copyright © 2025 Cru. All rights reserved.
+//
+
+import Testing
+@testable import godtools
+import Combine
+
+struct StringSearcherTests {
+    
+    struct TestArgument {
+        let searchTerm: String
+        let expectedSearchResults: [MockStringSearchable]
+    }
+    
+    struct MockStringSearchable: StringSearchable, Equatable {
+        let searchableStrings: [String]
+    }
+    
+    private static let englishSearchable = MockStringSearchable(searchableStrings: ["English", "English"])
+    private static let albanianSearchable = MockStringSearchable(searchableStrings: ["Albanian", "shqip"])
+    private static let croatianSearchable = MockStringSearchable(searchableStrings: ["Croatian", "hrvatski"])
+    private static let englishBahrainSearchable = MockStringSearchable(searchableStrings: ["English (Bahrain)", "English (Bahrain)"])
+    private static let frenchSearchable = MockStringSearchable(searchableStrings: ["French", "français"])
+    private static let spanishSearchable = MockStringSearchable(searchableStrings: ["Spanish", "español"])
+    
+    @Test(
+                """
+                Given: A list of StringSearchable items, where each StringSearchable contains a list of searchable strings
+                When: The StringSearchable list is searched with a search term string
+                Then: StringSearcher should return a list of StringSearchables that have a searchable string matching the search term.
+                """,
+                arguments: [
+                    TestArgument(searchTerm: "English", expectedSearchResults: [englishSearchable, englishBahrainSearchable]),
+                    TestArgument(searchTerm: "Bangla", expectedSearchResults: []),
+                    TestArgument(searchTerm: "SH", expectedSearchResults: [englishSearchable, albanianSearchable, englishBahrainSearchable, spanishSearchable]),
+                    TestArgument(searchTerm: "french", expectedSearchResults: [frenchSearchable]),
+                    TestArgument(searchTerm: "ESPAÑOL", expectedSearchResults: [spanishSearchable]),
+                    TestArgument(searchTerm: "ai", expectedSearchResults: [englishBahrainSearchable, frenchSearchable])
+                ]
+    )
+    func stringSearcherTest(argument: TestArgument) {
+        
+        let stringSearcher: StringSearcher = StringSearcher()
+        
+        let searchableLanguages: [MockStringSearchable] = [
+            StringSearcherTests.englishSearchable, StringSearcherTests.albanianSearchable, StringSearcherTests.croatianSearchable, StringSearcherTests.englishBahrainSearchable, StringSearcherTests.frenchSearchable, StringSearcherTests.spanishSearchable
+        ]
+        
+        let searchResults = stringSearcher.search(for: argument.searchTerm, in: searchableLanguages)
+        
+        #expect(searchResults == argument.expectedSearchResults)
+    }
+}
