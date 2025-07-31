3222c09bbd59c64b7d01f9234c2cb93c81993127
Levi Eggert <levi.eggert@cru.org>
2025-07-31 09:19:06 -0400

    Add tool names for opening a specific tool in ui tests

---
+++ godtoolsUITests/Share/Strings/ToolNames.swift
@@ -0,0 +1,14 @@
+//
+//  ToolNames.swift
+//  godtools
+//
+//  Created by Levi Eggert on 7/31/25.
+//  Copyright © 2025 Cru. All rights reserved.
+//
+
+struct ToolNames {
+    struct English {
+        static let fourSpiritualLaws: String = "four spiritual laws"
+        static let teachMeToShare: String = "teach me to share"
+    }
+}
