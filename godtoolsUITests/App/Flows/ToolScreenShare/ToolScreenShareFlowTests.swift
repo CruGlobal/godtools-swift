15fe68c6eb133d30ae594f60ec0e1e06f66886e2
Levi Eggert <levi.eggert@cru.org>
2025-08-05 08:13:39 -0400

    Add accessibility to tool screen share continue, generate qr code, and share link buttons Create separate button for share link

--- godtoolsUITests/App/Flows/ToolScreenShare/ToolScreenShareFlowTests.swift
+++ godtoolsUITests/App/Flows/ToolScreenShare/ToolScreenShareFlowTests.swift
@@ -37,10 +37,36 @@ class ToolScreenShareFlowTests: BaseFlowTests {
         XCTAssertTrue(shareScreenButton.exists)
         
         shareScreenButton.tap()
+        
+        assertIfScreenDoesNotExist(screenAccessibility: .toolScreenShareTutorial)
+    }
+    
+    private func getContinueButtonFromToolScreenShareTutorial() -> XCUIElement {
+        app.queryButton(buttonAccessibility: .continueForward, waitForExistence: 1)
     }
     
     func testInitialScreenIsToolScreenShare() {
         
         launchAppToToolScreenShare()
+    }
+    
+    func testNavigationThroughToolScreenShareWithContinueButtonLandsOnLastPageWithGenerateQRCodeAndShareLinkButtons() {
+        
+        launchAppToToolScreenShare()
+        
+        var continueButton: XCUIElement = getContinueButtonFromToolScreenShareTutorial()
+        
+        while continueButton.exists {
+            continueButton.tap()
+            continueButton = getContinueButtonFromToolScreenShareTutorial()
+        }
+        
+        let generateQRCodeButton = app.queryButton(buttonAccessibility: .generateQRCode, waitForExistence: 1)
+        
+        let shareLinkButton = app.queryButton(buttonAccessibility: .shareLink, waitForExistence: 1)
+        
+        XCTAssertTrue(generateQRCodeButton.exists)
+        
+        XCTAssertTrue(shareLinkButton.exists)
     }
 }
