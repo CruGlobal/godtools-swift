8d05c1f00feda690a435680612262b5fa279e52f
Levi Eggert <levi.eggert@cru.org>
2025-08-07 07:52:10 -0400

    Add constants

--- godtools/App/Share/Data/DeepLinkingService/GodToolsManifest/GodToolsDeepLinkingManifest.swift
+++ godtools/App/Share/Data/DeepLinkingService/GodToolsManifest/GodToolsDeepLinkingManifest.swift
@@ -10,86 +10,92 @@ import Foundation
 
 class GodToolsDeepLinkingManifest: DeepLinkingManifestInterface {
     
+    private static let schemeGodTools: String = "godtools"
+    private static let schemeHttps: String = "https"
+    private static let hostGodTools: String = "org.cru.godtools"
+    private static let hostGodToolsApp: String = "godtoolsapp.com"
+    private static let hostKnowGod: String = "knowgod.com"
+    
     let parserManifests: [DeepLinkingParserManifestInterface]
     
     init() {
         
         parserManifests = [
             DeepLinkingParserManifestUrl(
-                scheme: "godtools",
-                host: "org.cru.godtools",
+                scheme: Self.schemeGodTools,
+                host: Self.hostGodTools,
                 path: "dashboard",
                 parserClass: DashboardPathDeepLinkParser.self
             ),
             DeepLinkingParserManifestUrl(
-                scheme: "https",
-                host: "godtoolsapp.com",
+                scheme: Self.schemeHttps,
+                host: Self.hostGodToolsApp,
                 path: "deeplink/dashboard",
                 parserClass: DashboardPathDeepLinkParser.self
             ),
             DeepLinkingParserManifestUrl(
-                scheme: "godtools",
-                host: "org.cru.godtools",
+                scheme: Self.schemeGodTools,
+                host: Self.hostGodTools,
                 path: "tool",
                 parserClass: ToolPathDeepLinkParser.self
             ),
             DeepLinkingParserManifestUrl(
-                scheme: "https",
-                host: "godtoolsapp.com",
+                scheme: Self.schemeHttps,
+                host: Self.hostGodToolsApp,
                 path: "deeplink/tool",
                 parserClass: ToolPathDeepLinkParser.self
             ),
             DeepLinkingParserManifestUrl(
-                scheme: "https",
-                host: "godtoolsapp.com",
+                scheme: Self.schemeHttps,
+                host: Self.hostGodToolsApp,
                 path: "article/aem",
                 parserClass: ArticleAemPathDeepLinkParser.self
             ),
             DeepLinkingParserManifestUrl(
-                scheme: "https",
-                host: "knowgod.com",
+                scheme: Self.schemeHttps,
+                host: Self.hostKnowGod,
                 path: nil,
                 parserClass: KnowGodDeepLinkParser.self
             ),
             DeepLinkingParserManifestUrl(
-                scheme: "godtools",
-                host: "knowgod.com",
+                scheme: Self.schemeGodTools,
+                host: Self.hostKnowGod,
                 path: nil,
                 parserClass: KnowGodDeepLinkParser.self
             ),
             DeepLinkingParserManifestUrl(
-                scheme: "https",
-                host: "knowgod.com",
+                scheme: Self.schemeHttps,
+                host: Self.hostKnowGod,
                 path: nil,
                 parserClass: LegacyKnowGodDeepLinkParser.self
             ),
             DeepLinkingParserManifestUrl(
-                scheme: "godtools",
-                host: "knowgod.com",
+                scheme: Self.schemeGodTools,
+                host: Self.hostKnowGod,
                 path: nil,
                 parserClass: LegacyKnowGodDeepLinkParser.self
             ),
             DeepLinkingParserManifestUrl(
-                scheme: "https",
-                host: "godtoolsapp.com",
+                scheme: Self.schemeHttps,
+                host: Self.hostGodToolsApp,
                 path: "lessons",
                 parserClass: GodToolsAppLessonsPathDeepLinkParser.self
             ),
             DeepLinkingParserManifestUrl(
-                scheme: "godtools",
-                host: "org.cru.godtools",
+                scheme: Self.schemeGodTools,
+                host: Self.hostGodTools,
                 path: "settings/language",
                 parserClass: LanguageSettingsDeepLinkParser.self
             ),
             DeepLinkingParserManifestUrl(
-                scheme: "https",
-                host: "godtoolsapp.com",
+                scheme: Self.schemeHttps,
+                host: Self.hostGodToolsApp,
                 path: "deeplink/settings/language",
                 parserClass: LanguageSettingsDeepLinkParser.self
             ),
             DeepLinkingParserManifestUrl(
-                scheme: "godtools",
-                host: "org.cru.godtools",
+                scheme: Self.schemeGodTools,
+                host: Self.hostGodTools,
                 path: "ui_tests",
                 parserClass: UITestsDeepLinkParser.self
             )
