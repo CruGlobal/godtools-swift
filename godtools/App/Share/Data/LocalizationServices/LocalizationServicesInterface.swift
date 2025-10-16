8d7ae5a8006ede50c2c16d164c811187a7d8e02e
Levi Eggert <levi.eggert@cru.org>
2025-10-16 17:06:56 -0400

    Add LocalizationServicesInterface

---
+++ godtools/App/Share/Data/LocalizationServices/LocalizationServicesInterface.swift
@@ -0,0 +1,22 @@
+//
+//  LocalizationServicesInterface.swift
+//  godtools
+//
+//  Created by Levi Eggert on 10/16/25.
+//  Copyright © 2025 Cru. All rights reserved.
+//
+
+import Foundation
+import LocalizationServices
+
+protocol LocalizationServicesInterface {
+    
+    func stringForEnglish(key: String) -> String
+    func stringForSystemElseEnglish(key: String) -> String
+    func stringForLocaleElseEnglish(localeIdentifier: String?, key: String) -> String
+    func stringForLocaleElseSystemElseEnglish(localeIdentifier: String?, key: String) -> String
+}
+
+extension LocalizationServices: LocalizationServicesInterface {
+    
+}
