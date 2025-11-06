f706e5f0f03a26a102bbb79275bca25d3e431e05
Levi Eggert <levi.eggert@cru.org>
2025-11-06 09:55:18 -0500

    Add AttachmentsCache and subclass SwiftElseRealmPersistence

---
+++ godtools/App/Share/Data/AttachmentsRepository/Cache/AttachmentsCache.swift
@@ -0,0 +1,52 @@
+//
+//  AttachmentsCache.swift
+//  godtools
+//
+//  Created by Levi Eggert on 11/6/25.
+//  Copyright © 2025 Cru. All rights reserved.
+//
+
+import Foundation
+
+class AttachmentsCache: SwiftElseRealmPersistence<AttachmentDataModel, AttachmentCodable, RealmAttachment> {
+    
+    private let realmDatabase: RealmDatabase
+    
+    init(realmDatabase: RealmDatabase) {
+        
+        self.realmDatabase = realmDatabase
+        
+        super.init(
+            realmDatabase: realmDatabase,
+            realmDataModelMapping: RealmAttachmentDataModelMapping()
+        )
+    }
+    
+    @available(iOS 17, *)
+    override func getAnySwiftPersistence(swiftDatabase: SwiftDatabase) -> (any RepositorySyncPersistence<AttachmentDataModel, AttachmentCodable>)? {
+        return getSwiftPersistence(swiftDatabase: swiftDatabase)
+    }
+    
+    @available(iOS 17, *)
+    private func getSwiftPersistence() -> SwiftRepositorySyncPersistence<AttachmentDataModel, AttachmentCodable, SwiftAttachment>? {
+        
+        guard let swiftDatabase = super.getSwiftDatabase() else {
+            return nil
+        }
+        
+        return getSwiftPersistence(swiftDatabase: swiftDatabase)
+    }
+    
+    @available(iOS 17, *)
+    private func getSwiftPersistence(swiftDatabase: SwiftDatabase) -> SwiftRepositorySyncPersistence<AttachmentDataModel, AttachmentCodable, SwiftAttachment>? {
+        
+        guard let swiftDatabase = super.getSwiftDatabase() else {
+            return nil
+        }
+        
+        return SwiftRepositorySyncPersistence(
+            swiftDatabase: swiftDatabase,
+            dataModelMapping: SwiftAttachmentDataModelMapping()
+        )
+    }
+}
