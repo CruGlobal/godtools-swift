315b802340121f3e83a8419854147c1add305dcf
Levi Eggert <levi.eggert@cru.org>
2025-11-24 07:09:11 -0500

    Begin to add tests

---
+++ godtoolsTests/UnitTests/App/Share/Data/TranslationsRepository/Cache/TranslationsCacheTests.swift
@@ -0,0 +1,20 @@
+//
+//  TranslationsCacheTests.swift
+//  godtools
+//
+//  Created by Levi Eggert on 11/23/25.
+//  Copyright © 2025 Cru. All rights reserved.
+//
+
+import Testing
+@testable import godtools
+import Foundation
+
+struct TranslationsCacheTests {
+    
+}
+
+
+extension TranslationsCacheTests {
+    
+}
