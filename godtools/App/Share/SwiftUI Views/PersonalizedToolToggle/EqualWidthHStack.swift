88d9d143171c64e1aabeb2afe2055a17ef5dbf3e
Rachael Skeath <rachaelblue@gmail.com>
2026-01-06 14:55:17 -0500

    remove height calculation

--- godtools/App/Share/SwiftUI Views/PersonalizedToolToggle/EqualWidthHStack.swift
+++ godtools/App/Share/SwiftUI Views/PersonalizedToolToggle/EqualWidthHStack.swift
@@ -14,27 +14,25 @@ struct EqualWidthHStack: Layout {
     var spacing: CGFloat = 0
 
     func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
-        
+
         guard !subviews.isEmpty else { return .zero }
 
         let maxWidth = subviews.map { $0.sizeThatFits(.unspecified).width }.max() ?? 0
-        let maxHeight = subviews.map { $0.sizeThatFits(.unspecified).height }.max() ?? 0
+        let height = subviews.first?.sizeThatFits(.unspecified).height ?? 0
         let totalWidth = maxWidth * CGFloat(subviews.count) + spacing * CGFloat(subviews.count - 1)
 
-        return CGSize(width: totalWidth, height: maxHeight)
+        return CGSize(width: totalWidth, height: height)
     }
 
     func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
-        
+
         guard !subviews.isEmpty else { return }
 
         let maxWidth = subviews.map { $0.sizeThatFits(.unspecified).width }.max() ?? 0
-        let maxHeight = subviews.map { $0.sizeThatFits(.unspecified).height }.max() ?? 0
 
         var x = bounds.minX
         for subview in subviews {
-            let proposal = ProposedViewSize(width: maxWidth, height: maxHeight)
-            subview.place(at: CGPoint(x: x, y: bounds.minY), proposal: proposal)
+            subview.place(at: CGPoint(x: x, y: bounds.minY), proposal: ProposedViewSize(width: maxWidth, height: nil))
             x += maxWidth + spacing
         }
     }
