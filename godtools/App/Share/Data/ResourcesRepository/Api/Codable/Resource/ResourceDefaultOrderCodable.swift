2c6dcbccb0b24cc0425cd973fb6d0b062a0e2b0a
Levi Eggert <levi.eggert@cru.org>
2026-01-12 09:10:39 -0500

    Add codable for scores and default orders

---
+++ godtools/App/Share/Data/ResourcesRepository/Api/Codable/Resource/ResourceDefaultOrderCodable.swift
@@ -0,0 +1,20 @@
+//
+//  ResourceDefaultOrderCodable.swift
+//  godtools
+//
+//  Created by Levi Eggert on 1/12/26.
+//  Copyright © 2026 Cru. All rights reserved.
+//
+
+import Foundation
+
+struct ResourceDefaultOrderCodable: Codable {
+    
+    let id: String
+    let type: String
+    
+    enum RootKeys: String, CodingKey {
+        case id = "id"
+        case type = "type"
+    }
+}
