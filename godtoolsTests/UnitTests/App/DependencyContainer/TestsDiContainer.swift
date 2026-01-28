46a2d79b70843f03c14763e91078ee1a5ca5f9cd
Levi Eggert <levi.eggert@cru.org>
2026-01-28 14:53:06 -0500

    Files no longer needed

--- godtoolsTests/UnitTests/App/DependencyContainer/TestsDiContainer.swift
+++ godtoolsTests/UnitTests/App/DependencyContainer/TestsDiContainer.swift
@@ -19,61 +19,38 @@ class TestsDiContainer: AppDiContainer {
         super.init(appConfig: testsAppConfig)
     }
     
-    convenience init(realmFileName: String? = nil, addRealmObjects: [IdentifiableRealmObject] = Array()) throws {
+    convenience init(realmFileName: String, addRealmObjects: [IdentifiableRealmObject] = Array()) throws {
         
-        let legacyRealmDatabase: LegacyRealmDatabase
-        let realmDatabase: RealmDatabase
+        let fileUrl = URL(fileURLWithPath: RLMRealmPathForFile(realmFileName), isDirectory: false)
         
-        if let realmFileName = realmFileName, !realmFileName.isEmpty {
-            
-            let fileUrl = URL(fileURLWithPath: RLMRealmPathForFile(realmFileName), isDirectory: false)
-            
-            if FileManager.default.getFilePathExists(url: fileUrl) {
-                try FileManager.default.removeUrl(url: fileUrl)
+        if FileManager.default.getFilePathExists(url: fileUrl) {
+            try FileManager.default.removeUrl(url: fileUrl)
+        }
+        
+        let config = Realm.Configuration(
+            fileURL: fileUrl,
+            schemaVersion: 1,
+            migrationBlock: { (_, _) in
+                
             }
-            
-            let config = Realm.Configuration(
-                fileURL: fileUrl,
-                schemaVersion: 1,
-                migrationBlock: { (_, _) in
-                    
-                }
-            )
-            
-            legacyRealmDatabase = LegacyRealmDatabase(config: config)
-            
-            realmDatabase = RealmDatabase(databaseConfig: RealmDatabaseConfig(config: config))
-            
-            let realm: Realm = try realmDatabase.openRealm()
-                        
-            try realmDatabase.write.realm(
-                realm: realm,
-                writeClosure: { (realm: Realm) in
-                    return WriteRealmObjects(
-                        deleteObjects: nil,
-                        addObjects: addRealmObjects
-                    )
-                },
-                updatePolicy: .modified
-            )
-        }
-        else {
-            
-            let realmDatabaseId: String = UUID().uuidString
-            let realmSchemaVersion: UInt64 = 1
-            
-            legacyRealmDatabase = try MockLegacyRealmDatabase().createInMemoryDatabase(
-                addObjects: addRealmObjects,
-                identifier: realmDatabaseId,
-                schemaVersion: realmSchemaVersion
-            )
-            
-            realmDatabase = try MockRealmDatabase().createInMemoryDatabase(
-                addObjects: addRealmObjects,
-                identifier: realmDatabaseId,
-                schemaVersion: realmSchemaVersion
-            )
-        }
+        )
+        
+        let legacyRealmDatabase = LegacyRealmDatabase(config: config)
+        
+        let realmDatabase = RealmDatabase(databaseConfig: RealmDatabaseConfig(config: config))
+        
+        let realm: Realm = try realmDatabase.openRealm()
+        
+        try realmDatabase.write.realm(
+            realm: realm,
+            writeClosure: { (realm: Realm) in
+                return WriteRealmObjects(
+                    deleteObjects: nil,
+                    addObjects: addRealmObjects
+                )
+            },
+            updatePolicy: .modified
+        )
         
         let appConfig = TestsAppConfig(
             legacyRealmDatabase: legacyRealmDatabase,
