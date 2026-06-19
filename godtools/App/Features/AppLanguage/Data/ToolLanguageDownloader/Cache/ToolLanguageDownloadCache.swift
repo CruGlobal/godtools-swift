b8de6aafd50a1cbe7929b14e813bb09f196df56c
Levi Eggert <levi.eggert@cru.org>
2026-06-19 10:49:29 -0400

    Add fetching for downloaded languages

--- godtools/App/Features/AppLanguage/Data/ToolLanguageDownloader/Cache/ToolLanguageDownloadCache.swift
+++ godtools/App/Features/AppLanguage/Data/ToolLanguageDownloader/Cache/ToolLanguageDownloadCache.swift
@@ -8,8 +8,15 @@
 
 import Foundation
 import RepositorySync
+import RealmSwift
 
 final class ToolLanguageDownloadCache {
+    
+    enum DownloadState {
+        case all
+        case complete
+        case incomplete
+    }
     
     let persistence: any Persistence<ToolLanguageDownloadDataModel, ToolLanguageDownloadDataModel>
     
@@ -30,5 +37,77 @@ final class ToolLanguageDownloadCache {
     
     private func getRealmPersistence() -> RealmRepositorySyncPersistence<ToolLanguageDownloadDataModel, ToolLanguageDownloadDataModel, RealmToolLanguageDownload>? {
         return persistence as? RealmRepositorySyncPersistence<ToolLanguageDownloadDataModel, ToolLanguageDownloadDataModel, RealmToolLanguageDownload>
+    }
+}
+
+// MARK: - Predicates
+
+extension ToolLanguageDownloadCache {
+ 
+    @available(iOS 17.4, *)
+    private func getDownloadCompletePredicate() -> Predicate<SwiftToolLanguageDownload> {
+     
+        let filter = #Predicate<SwiftToolLanguageDownload> { object in
+            object.downloadProgress >= 1
+        }
+        
+        return filter
+    }
+    
+    @available(iOS 17.4, *)
+    private func getIncompleteDownloadPredicate() -> Predicate<SwiftToolLanguageDownload> {
+     
+        let filter = #Predicate<SwiftToolLanguageDownload> { object in
+            object.downloadProgress < 1
+        }
+        
+        return filter
+    }
+}
+
+extension ToolLanguageDownloadCache {
+    
+    func getDownloads(state: DownloadState) async throws -> [ToolLanguageDownloadDataModel] {
+        
+        guard state != .all else {
+            return try await persistence.getDataModels()
+        }
+        
+        if #available(iOS 17.4, *), let swiftPersistence = getSwiftPersistence() {
+            
+            let filter: Predicate<SwiftToolLanguageDownload> = state == .complete ? getDownloadCompletePredicate() : getIncompleteDownloadPredicate()
+            
+            let query = SwiftDatabaseQuery<SwiftToolLanguageDownload>.filter(
+                filter: filter
+            )
+            
+            let context = swiftPersistence.database.openContext()
+            
+            let objects: [SwiftToolLanguageDownload] = try swiftPersistence.database.read.objects(context: context, query: query)
+            
+            return objects.map {
+                $0.toModel()
+            }
+        }
+        else if let realmPersistence = getRealmPersistence() {
+            
+            let realm = try realmPersistence.database.openRealm()
+            
+            let results: Results<RealmToolLanguageDownload> = realm.objects(RealmToolLanguageDownload.self).where {
+                
+                if state == .complete {
+                    $0.downloadProgress >= 1
+                }
+                else {
+                    $0.downloadProgress < 1
+                }
+            }
+            
+            return results.map {
+                $0.toModel()
+            }
+        }
+        
+        return Array()
     }
 }
