e38069a810ff1afeed8b92be125a809a0bd9d381
Levi Eggert <levi.eggert@cru.org>
2026-07-02 15:05:25 -0400

    Remove combine from EvaluateLessonUseCase

--- godtools/App/Features/LessonEvaluation/Presentation/LessonEvaluation/LessonEvaluationViewModel.swift
+++ godtools/App/Features/LessonEvaluation/Presentation/LessonEvaluation/LessonEvaluationViewModel.swift
@@ -11,9 +11,7 @@ import Combine
 
 @MainActor
 final class LessonEvaluationViewModel: ObservableObject {
-    
-    private static var backgroundCancellables: Set<AnyCancellable> = Set()
-        
+            
     private let stepEmitter: FlowStepEmitter
     private let lessonId: String
     private let lessonLanguage: AppLanguageDomainModel
@@ -139,13 +137,10 @@ extension LessonEvaluationViewModel {
             pageIndexReached: pageIndexReached
         )
         
-        evaluateLessonUseCase
-            .execute(lessonId: lessonId, feedback: feedback, lessonLanguage: lessonLanguage)
-            .receive(on: DispatchQueue.main)
-            .sink(receiveValue: { _ in
-                
-            })
-            .store(in: &Self.backgroundCancellables)
+        Task {
+            try await evaluateLessonUseCase
+                .execute(lessonId: lessonId, feedback: feedback, lessonLanguage: lessonLanguage)
+        }
         
         stepEmitter.emit(step: AppFlowStep.sendFeedbackTappedFromLessonEvaluation)
     }
