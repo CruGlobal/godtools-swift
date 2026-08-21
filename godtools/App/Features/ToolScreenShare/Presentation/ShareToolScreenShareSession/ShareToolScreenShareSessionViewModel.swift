4308cc247771c7237fb7066efd330730dd52586c
Levi Eggert <levi.eggert@cru.org>
2026-08-21 11:42:24 -0400

    hoist out properties

--- godtools/App/Features/ToolScreenShare/Presentation/ShareToolScreenShareSession/ShareToolScreenShareSessionViewModel.swift
+++ godtools/App/Features/ToolScreenShare/Presentation/ShareToolScreenShareSession/ShareToolScreenShareSessionViewModel.swift
@@ -47,18 +47,19 @@ extension ShareToolScreenShareSessionViewModel {
     
     func pageViewed() {
         
+        let analyticsProperties = AnalyticsProperties(
+            screenName: "",
+            siteSection: "",
+            siteSubSection: "",
+            appLanguage: nil,
+            contentLanguage: nil,
+            secondaryContentLanguage: nil
+        )
         let trackActionAnalyticsUseCase: TrackActionAnalyticsUseCase = self.trackActionAnalyticsUseCase
         
         Task.detached {
             await trackActionAnalyticsUseCase.execute(
-                properties: AnalyticsProperties(
-                    screenName: "",
-                    siteSection: "",
-                    siteSubSection: "",
-                    appLanguage: nil,
-                    contentLanguage: nil,
-                    secondaryContentLanguage: nil
-                ),
+                properties: analyticsProperties,
                 actionName: AnalyticsConstants.ActionNames.shareScreenEngaged,
                 data: [
                     AnalyticsConstants.Keys.shareScreenEngagedCountKey: 1
