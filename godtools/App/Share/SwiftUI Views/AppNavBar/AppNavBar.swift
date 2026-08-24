c217f0f26d406e74880e85e51614e1dbd6685c23
Levi Eggert <levi.eggert@cru.org>
2026-08-24 16:43:01 -0400

    Add app navbar

---
+++ godtools/App/Share/SwiftUI Views/AppNavBar/AppNavBar.swift
@@ -0,0 +1,38 @@
+//
+//  AppNavBar.swift
+//  godtools
+//
+//  Created by Levi Eggert on 8/19/26.
+//  Copyright © 2026 Cru. All rights reserved.
+//
+
+import SwiftUI
+
+extension View {
+    
+    func navBar<Content: ToolbarContent>(
+        title: String?,
+        backTapped: (() -> Void)?,
+        @ToolbarContentBuilder toolbarContent: () -> Content = { AppToolbarItem?.none }
+    ) -> some View {
+        
+        navigationTitle(title ?? "")
+            .navigationBarBackButtonHidden(true)
+            .toolbar {
+                
+                if backTapped != nil {
+                    
+                    AppToolbarItem(
+                        placement: AppToolbarItem.leadingPlacement,
+                        viewType: .image(value: ImageCatalog.navBack.image),
+                        accessibilityId: AccessibilityStrings.Button.back.id,
+                        tappedClosure: {
+                            backTapped?()
+                        }
+                    )
+                }
+                
+                toolbarContent()
+            }
+    }
+}
---
+++ godtools/App/Share/SwiftUI Views/AppNavBar/AppToolbarItem.swift
@@ -0,0 +1,81 @@
+//
+//  AppToolbarItem.swift
+//  godtools
+//
+//  Created by Levi Eggert on 8/19/26.
+//  Copyright © 2026 Cru. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppToolbarItem: ToolbarContent {
+    
+    enum ViewType: Sendable {
+        case image(value: Image)
+        case imageName(value: String)
+        case text(value: String, color: Color)
+    }
+    
+    private let placement: ToolbarItemPlacement
+    private let viewType: ViewType
+    private let accessibilityId: String?
+    private let tappedClosure: (() -> Void)?
+    
+    init(
+        placement: ToolbarItemPlacement,
+        viewType: ViewType,
+        accessibilityId: String?,
+        tappedClosure: (() -> Void)?
+    ) {
+        
+        self.placement = placement
+        self.viewType = viewType
+        self.accessibilityId = accessibilityId
+        self.tappedClosure = tappedClosure
+    }
+    
+    var body: some ToolbarContent {
+        
+        ToolbarItem(placement: placement) {
+            
+            Button(action: {
+                
+                tappedClosure?()
+            }) {
+                
+                switch viewType {
+                case .image(let value):
+                    value
+                case .imageName(let value):
+                    Image(value)
+                case .text(let value, let color):
+                    Text(value)
+                        .foregroundColor(color)
+                }
+            }
+            .accessibilityIdentifier(accessibilityId ?? "")
+            .buttonStyle(.borderless)
+        }
+        .ifAvailableSharedBackgroundVisibility(.hidden)
+    }
+}
+
+extension AppToolbarItem {
+    static var leadingPlacement: ToolbarItemPlacement {
+        
+        if #available(iOS 17.0, *) {
+            return .topBarLeading
+        }
+        
+        return .navigationBarLeading
+    }
+    
+    static var trailingPlacement: ToolbarItemPlacement {
+        
+        if #available(iOS 17.0, *) {
+            return .topBarTrailing
+        }
+        
+        return .navigationBarTrailing
+    }
+}
