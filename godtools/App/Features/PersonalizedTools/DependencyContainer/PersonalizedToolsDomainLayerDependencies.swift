5937698ccd7af5ad4657271b69ed2f61d8fd92db
levieggertcru <59846460+levieggertcru@users.noreply.github.com>
2026-09-03 15:30:34 -0400

    Add to domain layer dependencies

--- godtools/App/Features/PersonalizedTools/DependencyContainer/PersonalizedToolsDomainLayerDependencies.swift
+++ godtools/App/Features/PersonalizedTools/DependencyContainer/PersonalizedToolsDomainLayerDependencies.swift
@@ -18,13 +18,14 @@ final class PersonalizedToolsDomainLayerDependencies: Sendable {
         self.core = core
         self.dataLayer = dataLayer
     }
-    
-    func getMapLanguageToPersonalizedLessonFilterLanguage() -> MapLanguageToPersonalizedLessonFilterLanguage {
-        return MapLanguageToPersonalizedLessonFilterLanguage(
-            getTranslatedLanguageName: core.domainLayer.supporting.getTranslatedLanguageName()
+
+    func getLocalizationSettingsConfirmationStringsUseCase() -> GetLocalizationSettingsConfirmationStringsUseCase {
+
+        return GetLocalizationSettingsConfirmationStringsUseCase(
+            localizationServices: core.dataLayer.getLocalizationServices()
         )
     }
-    
+
     func getLocalizationSettingsCountryListUseCase() -> GetLocalizationSettingsCountryListUseCase {
 
         return GetLocalizationSettingsCountryListUseCase(
@@ -33,27 +34,32 @@ final class PersonalizedToolsDomainLayerDependencies: Sendable {
         )
     }
 
-    func getLocalizationSettingsConfirmationStringsUseCase() -> GetLocalizationSettingsConfirmationStringsUseCase {
-
-        return GetLocalizationSettingsConfirmationStringsUseCase(
-            localizationServices: core.dataLayer.getLocalizationServices()
-        )
-    }
-    
     func getLocalizationSettingsStringsUseCase() -> GetLocalizationSettingsStringsUseCase {
 
         return GetLocalizationSettingsStringsUseCase(
             localizationServices: core.dataLayer.getLocalizationServices()
         )
     }
-    
+
     func getLocalizationSettingsUseCase() -> GetLocalizationSettingsUseCase {
 
         return GetLocalizationSettingsUseCase(
             userLocalizationSettingsRepository: dataLayer.getUserLocalizationSettingsRepository()
         )
     }
-    
+
+    func getMapLanguageToPersonalizedLessonFilterLanguage() -> MapLanguageToPersonalizedLessonFilterLanguage {
+        return MapLanguageToPersonalizedLessonFilterLanguage(
+            getTranslatedLanguageName: core.domainLayer.supporting.getTranslatedLanguageName()
+        )
+    }
+
+    func getMapLanguageToPersonalizedToolFilterLanguage() -> MapLanguageToPersonalizedToolFilterLanguage {
+        return MapLanguageToPersonalizedToolFilterLanguage(
+            getTranslatedLanguageName: core.domainLayer.supporting.getTranslatedLanguageName()
+        )
+    }
+
     func getPersonalizedLessonFilterLanguagesStringsUseCase() -> GetPersonalizedLessonFilterLanguagesStringsUseCase {
 
         return GetPersonalizedLessonFilterLanguagesStringsUseCase(
@@ -70,40 +76,6 @@ final class PersonalizedToolsDomainLayerDependencies: Sendable {
         )
     }
 
-    func getUserPersonalizedLessonFilterLanguageUseCase() -> GetUserPersonalizedLessonFilterLanguageUseCase {
-
-        return GetUserPersonalizedLessonFilterLanguageUseCase(
-            languagesRepository: core.dataLayer.getLanguagesRepository(),
-            mapLanguageToPersonalizedLessonFilterLanguage: getMapLanguageToPersonalizedLessonFilterLanguage()
-        )
-    }
-
-    func getSearchPersonalizedLessonFilterLanguagesUseCase() -> SearchPersonalizedLessonFilterLanguagesUseCase {
-
-        return SearchPersonalizedLessonFilterLanguagesUseCase(
-            stringSearcher: StringSearcher()
-        )
-    }
-
-    func getSetUserPersonalizedLessonFilterLanguageUseCase() -> SetUserPersonalizedLessonFilterLanguageUseCase {
-
-        return SetUserPersonalizedLessonFilterLanguageUseCase()
-    }
-
-    func getSearchCountriesInLocalizationSettingsCountriesListUseCase() -> SearchCountriesInLocalizationSettingsCountriesListUseCase {
-        
-        return SearchCountriesInLocalizationSettingsCountriesListUseCase(
-            stringSearcher: StringSearcher()
-        )
-    }
-
-    func getSetLocalizationSettingsUseCase() -> SetLocalizationSettingsUseCase {
-
-        return SetLocalizationSettingsUseCase(
-            userLocalizationSettingsRepository: dataLayer.getUserLocalizationSettingsRepository()
-        )
-    }
-
     func getPersonalizedLessonsUseCase() -> GetPersonalizedLessonsUseCase {
 
         return GetPersonalizedLessonsUseCase(
@@ -116,6 +88,22 @@ final class PersonalizedToolsDomainLayerDependencies: Sendable {
         )
     }
 
+    func getPersonalizedToolFilterLanguagesStringsUseCase() -> GetPersonalizedToolFilterLanguagesStringsUseCase {
+
+        return GetPersonalizedToolFilterLanguagesStringsUseCase(
+            localizationServices: core.dataLayer.getLocalizationServices()
+        )
+    }
+
+    func getPersonalizedToolFilterLanguagesUseCase() -> GetPersonalizedToolFilterLanguagesUseCase {
+
+        return GetPersonalizedToolFilterLanguagesUseCase(
+            resourcesRepository: core.dataLayer.getResourcesRepository(),
+            languagesRepository: core.dataLayer.getLanguagesRepository(),
+            mapLanguageToPersonalizedToolFilterLanguage: getMapLanguageToPersonalizedToolFilterLanguage()
+        )
+    }
+
     func getPersonalizedToolsUseCase() -> GetPersonalizedToolsUseCase {
 
         return GetPersonalizedToolsUseCase(
@@ -124,6 +112,60 @@ final class PersonalizedToolsDomainLayerDependencies: Sendable {
             getLanguageElseAppLanguage: core.domainLayer.supporting.getLanguageElseAppLanguage(),
             getToolsListItems: core.domainLayer.supporting.getToolsListItems(),
             localizationServices: core.dataLayer.getLocalizationServices()
+        )
+    }
+
+    func getSearchCountriesInLocalizationSettingsCountriesListUseCase() -> SearchCountriesInLocalizationSettingsCountriesListUseCase {
+        
+        return SearchCountriesInLocalizationSettingsCountriesListUseCase(
+            stringSearcher: StringSearcher()
+        )
+    }
+
+    func getSearchPersonalizedLessonFilterLanguagesUseCase() -> SearchPersonalizedLessonFilterLanguagesUseCase {
+
+        return SearchPersonalizedLessonFilterLanguagesUseCase(
+            stringSearcher: StringSearcher()
+        )
+    }
+
+    func getSearchPersonalizedToolFilterLanguagesUseCase() -> SearchPersonalizedToolFilterLanguagesUseCase {
+
+        return SearchPersonalizedToolFilterLanguagesUseCase(
+            stringSearcher: StringSearcher()
+        )
+    }
+
+    func getSetLocalizationSettingsUseCase() -> SetLocalizationSettingsUseCase {
+
+        return SetLocalizationSettingsUseCase(
+            userLocalizationSettingsRepository: dataLayer.getUserLocalizationSettingsRepository()
+        )
+    }
+
+    func getSetUserPersonalizedLessonFilterLanguageUseCase() -> SetUserPersonalizedLessonFilterLanguageUseCase {
+
+        return SetUserPersonalizedLessonFilterLanguageUseCase()
+    }
+
+    func getSetUserPersonalizedToolFilterLanguageUseCase() -> SetUserPersonalizedToolFilterLanguageUseCase {
+
+        return SetUserPersonalizedToolFilterLanguageUseCase()
+    }
+
+    func getUserPersonalizedLessonFilterLanguageUseCase() -> GetUserPersonalizedLessonFilterLanguageUseCase {
+
+        return GetUserPersonalizedLessonFilterLanguageUseCase(
+            languagesRepository: core.dataLayer.getLanguagesRepository(),
+            mapLanguageToPersonalizedLessonFilterLanguage: getMapLanguageToPersonalizedLessonFilterLanguage()
+        )
+    }
+
+    func getUserPersonalizedToolFilterLanguageUseCase() -> GetUserPersonalizedToolFilterLanguageUseCase {
+
+        return GetUserPersonalizedToolFilterLanguageUseCase(
+            languagesRepository: core.dataLayer.getLanguagesRepository(),
+            mapLanguageToPersonalizedToolFilterLanguage: getMapLanguageToPersonalizedToolFilterLanguage()
         )
     }
 }
